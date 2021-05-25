e9855d965c616fa0597c543842bdcb931b107c2b
Danilo Uehara <daniloyuno@hotmail.com>
2021-05-25 19:15:18 -0300

    exercicios 1 ao 6

--- src/challenges.js
+++ src/challenges.js
@@ -7,33 +7,83 @@ function compareTrue(valor1, valor2) {
 
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  let result = (base * height) / 2;
+  
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(string) {
+  let newArray=[];
+  let aux=0;
+  //for para preencher o array com algo, para ele não ficar como não idenficado 
+  for (let index = 0; index < string.length; index+=1) {
+    newArray[aux]=""; 
+    if (string[index]==" ") {
+      aux+=1;        
+    }        
+  }
+  aux=0;
+  for (let index = 0; index < string.length; index+=1) {   
+    if (string[index]==" ") {      
+      aux+=1;        
+    }    
+    else{
+      newArray[aux]=newArray[aux] + string[index];
+    }
+  }
+  return newArray;
 }
+
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(array=[]) {
+  let concatValue="";  
+  for (let index = array.length-1; index >= 0; index-=1) {
+    if (index == 0 || index == array.length-1) {
+      concatValue=concatValue + " " + array[index];
+      if (index == 0) {
+        concatValue=concatValue;
+      }
+      else {
+        concatValue=concatValue + ",";
+      }
+    }    
+  }
+  return concatValue;
+  
 }
+
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins,ties) {
+  let result= (3*wins) + (1*ties);
+  return result;
 }
 
+
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+function highestCount(num=[]) {
+  let highValue=0;
+  let count=0;
+  for (let index = 0; index < num.length; index+=1) {
+    if (highValue < num[index]){
+      highValue = num[index];
+    }    
+  }
+  for (let index = 0; index < num.length; index++) {
+    if (highValue == num[index]){
+      count+=1;
+    }    
+  }
+  return count;
 }
+
 
 // Desafio 7
 function catAndMouse() {
-  // seu código aqui
+
+  
 }
 
 // Desafio 8
