f08ea7510a866ed87a5055c7d6ec4b72d398ce10
Rafael Janovicci <rjanovicci@hotmail.com>
2021-05-25 16:21:04 -0300

    exercício 5

--- src/challenges.js
+++ src/challenges.js
@@ -30,9 +30,9 @@ function concatName(arrayDeStrings) {
 };
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
-}
+function footballPoints(wins, ties) {
+  return (wins * 3) + (ties); 
+};
 
 // Desafio 6
 function highestCount() {
