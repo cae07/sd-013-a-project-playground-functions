09455ecc257b264a971ac847e2698e83392149b9
Guilherme Couto da Silva <gui.couto90@yahoo.com.br>
2021-05-25 18:32:11 -0300

    Conclusao do exercicio 4,6,8 e 9

--- src/challenges.js
+++ src/challenges.js
@@ -22,8 +22,14 @@ function splitSentence(frase) {
 
 
 // Desafio 4
-function concatName() {
-
+function concatName(arrayPalavras) {
+  let primeira = arrayPalavras[0];
+  let segundo = arrayPalavras[0];
+  for(let index = 0; index < arrayPalavras.length; index += 1){
+    segundo = arrayPalavras[index];
+  }
+  let fullName = segundo +','+ ' ' + primeira;
+  return fullName;
 }
 
 
@@ -34,8 +40,21 @@ function footballPoints(wins, ties) {
 }
 
 // Desafio 6
-function highestCount() {
-  
+function highestCount(arrayNum) {
+  let vezesMaior=0;
+  let maior = arrayNum[0];
+
+  for(let index = 0; index < arrayNum.length; index += 1){
+      if(maior < arrayNum[index]){
+       maior = arrayNum[index];
+      }
+  }
+  for(let index = 0; index < arrayNum.length; index += 1){
+    if(maior === arrayNum[index]){
+      vezesMaior +=1;
+    }
+  }
+  return vezesMaior;
 }
 
 
@@ -52,20 +71,80 @@ function catAndMouse(mouse, cat1, cat2) {
   }
 }
 
-console.log(catAndMouse(1,22,22));
-
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(arrayNum) {
+  let teste;
+  for(let index = 0; index < arrayNum.length; index +=1){
+    if(arrayNum[index]%3==0 && arrayNum[index]%5==0){
+      arrayNum.splice(index, 1, 'fizzBuzz');
+    }else if(arrayNum[index]%3==0){
+      arrayNum.splice(index, 1, 'fizz');
+    }else if(arrayNum[index]%5==0){
+      arrayNum.splice(index, 1, 'buzz');
+  }else{
+      arrayNum.splice(index, 1, 'bug!');
+  }
+  
+  }
+  return arrayNum;
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(frase) {
+ for(let index = 0; index < frase.length; index +=1){
+   if(frase.includes('a') ){
+   frase = frase.replace('a',1);
+   }
+
+   if(frase.includes('e')){
+   frase = frase.replace('e',2);
+   }
+
+   if(frase.includes('i')){
+  frase = frase.replace('i',3);
+  }
+
+  if(frase.includes('o')){
+  frase = frase.replace('o',4);
+  }
+
+  if(frase.includes('u')){
+  frase = frase.replace('u',5);
+  }
+ }
+ let resultado = frase;
+ 
+return resultado;
 }
-function decode() {
-  // seu código aqui
-}
+
+console.log(encode('hi there!'));
+
+function decode(frase) {
+  for(let index = 0; index < frase.length; index +=1){
+    if(frase.includes(1) ){
+    frase = frase.replace(1,'a');
+    }
+ 
+    if(frase.includes(2)){
+    frase = frase.replace(2,'e');
+    }
+ 
+    if(frase.includes(3)){
+   frase = frase.replace(3,'i');
+   }
+ 
+   if(frase.includes(4)){
+   frase = frase.replace(4,'o');
+   }
+ 
+   if(frase.includes(5)){
+   frase = frase.replace(5,'u');
+   }
+  }
+  let resultado = frase;
+  
+ return resultado;
+ }
 
 module.exports = {
   calcArea,
@@ -78,4 +157,4 @@ module.exports = {
   footballPoints,
   highestCount,
   splitSentence,
-};
+}
