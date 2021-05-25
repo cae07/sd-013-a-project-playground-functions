9b0561ccdcf546e07e1a8f7e81efba2b68d7d0d4
Gabriel Biasoli <bielbiasoli33@icloud.com>
2021-05-25 15:05:20 -0300

    Desafio 2 finalizado

--- src/challenges.js
+++ src/challenges.js
@@ -5,12 +5,13 @@ function compareTrue(p1, p2) {
   } else {
     return false;
   }
-
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  let area = 0;
+  area = (base * height) / 2;
+  return area;
 }
 
 // Desafio 3
