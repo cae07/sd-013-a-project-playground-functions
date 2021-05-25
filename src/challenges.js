94522625d5ee445f806b8bd43353b02446784914
Jean V  Pacheco <jeanvitorp@gmail.com>
2021-05-25 18:43:45 -0300

    Adiciona desafio 9

--- src/challenges.js
+++ src/challenges.js
@@ -83,11 +83,47 @@ return result;
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(codificar) {
+  let codificarSplit = codificar.split("");
+for (let index = 0; index < codificarSplit.length; index += 1){
+  if (codificarSplit[index] == 'a'){
+    codificarSplit[index] = '1';
+  }
+  else if (codificarSplit[index] == 'e'){
+    codificarSplit[index] = '2';
+  }
+  else if (codificarSplit[index] == 'i'){
+    codificarSplit[index] = '3';
+  }
+  else if (codificarSplit[index] == 'o'){
+    codificarSplit[index] = '4';
+  }
+  else if (codificarSplit[index] == 'u'){
+    codificarSplit[index] = '5';
+  }
 }
-function decode() {
-  // seu código aqui
+  return codificarSplit.join('');
+}
+function decode(decodificar) {
+  let decodificarSplit = decodificar.split("");
+  for (let index = 0; index < decodificarSplit.length; index += 1){
+    if (decodificarSplit[index] == '1'){
+      decodificarSplit[index] = 'a';
+    }
+    else if (decodificarSplit[index] == '2'){
+      decodificarSplit[index] = 'e';
+    }
+    else if (decodificarSplit[index] == '3'){
+      decodificarSplit[index] = 'i';
+    }
+    else if (decodificarSplit[index] == '4'){
+      decodificarSplit[index] = 'o';
+    }
+    else if (decodificarSplit[index] == '5'){
+      decodificarSplit[index] = 'u';
+    }
+  }
+    return decodificarSplit.join('');
 }
 
 module.exports = {
