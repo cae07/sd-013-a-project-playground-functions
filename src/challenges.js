64415cd7ce553e98687f4061a0b87ef2a1d18784
rafa-araujo <rafacarlosaraujo@gmail.com>
2021-05-25 20:56:53 -0300

    Resolução exercício 4

--- src/challenges.js
+++ src/challenges.js
@@ -10,54 +10,36 @@ function compareTrue(n1, n2) {
 // Desafio 2
 function calcArea(base, height) {
   let area = base * height / 2;
-  
   return area
 };
 
-/*
-3 - Crie uma função que divida a frase
-Escreva uma função com o nome splitSentence, a qual receberá uma string e retornará uma array de strings separadas por cada espaço na string original.
-
-Exemplo: se a função receber a string "go Trybe", o retorno deverá ser ['go', 'Trybe'].
-
-O que será verificado:
-
-Retorne o valor ['go', 'Trybe'] se a função receber a string 'go Trybe'
-
-Retorne o valor ['vamo', 'que', 'vamo']. se a função receber a string 'vamo que vamo'
-
-Retorne o valor ['foguete'] se a função receber a string 'foguete'
-*/
 // Desafio 3
-//stringExemplo = "João da Silva Oliveira";
-//resultado = stringExemplo.split(" ", 3);
-//console.log(resultado);
-
 function splitSentence(frase) {
   let split = frase.split(" ");
   return split
 };
 
-
-
-
-
-
-
-
-
-
-
-
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(array) {
+  let concat = array[array.length - 1] + ', ' + array[0];
+  return concat;
 }
+console.log(concatName(['Rafael', 'Carlos', 'Araujo']));
 
 // Desafio 5
 function footballPoints() {
   // seu código aqui
 }
+
+
+
+
+
+
+
+
+
+
 
 // Desafio 6
 function highestCount() {
