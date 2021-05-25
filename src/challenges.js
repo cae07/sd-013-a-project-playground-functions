becbc47135dd4e8f9c8fcf65d1770ab143557008
icaroMelo <exemployt@gmail.com>
2021-05-25 19:49:39 -0300

    alguns ajustes

--- src/challenges.js
+++ src/challenges.js
@@ -1,5 +1,4 @@
 // Desafio 1
-// corrigir--->
 function compareTrue(value, value1) {
 
   if(value ===true && value1 === true ){
@@ -10,7 +9,6 @@ function compareTrue(value, value1) {
 
 }
 console.log(compareTrue(true, true));
-// <---corrigir
 
 // Desafio 2
 
@@ -18,34 +16,36 @@ function calcArea(base, height) {
   let area = (base*height)/2;
   return area;
 }
-console.log(calcArea(51, 1));
-
-// console.log("vamo que vamo".split(" ").join(", "));
+// console.log(calcArea(51, 1));
 
 // Desafio 3
+
 function splitSentence(string) {
   let split = string.split(" "); 
   return split;
 }
-console.log(splitSentence("go Trybe"));
+// console.log(splitSentence("go Trybe"));
 
 // Desafio 4
+
 function concatName(string1) {
   let concat = string1[string1.length-1] + ", " + string1[0];
   return concat;
 }
-console.log(concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']));
+// console.log(concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']));
 
 // Desafio 5
+
 function footballPoints(wins, ties) {
   let winspoints = 3*wins;
   let tiespoints = 1*ties;
   let points = winspoints + tiespoints;
   return points;
 }
-console.log(footballPoints(14,8));
+// console.log(footballPoints(14,8));
 
 // Desafio 6
+
 function highestCount(array1) {
   let nmaior = 0;
   let value  = 0;
@@ -66,12 +66,11 @@ function highestCount(array1) {
 
   return value;
 }
-console.log(highestCount([0, 0, 0]));
+// console.log(highestCount([0, 0, 0]));
 
 // Desafio 7
 function catAndMouse() {
-  
-  
+
 }
 
 // Desafio 8
