1c1de1ec3bb61dd2168846881bacbb2a6d7f7545
Leandro Liduvino <leandro.augusto460@gmail.com>
2021-05-25 14:53:49 -0300

    terceiro requisito

--- src/challenges.js
+++ src/challenges.js
@@ -16,8 +16,9 @@ function calcArea(base, height) {
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(phrase) {
+  let splitedPhrase = phrase.split(' ');
+  return splitedPhrase;
 }
 
 // Desafio 4
