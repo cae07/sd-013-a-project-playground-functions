805cf67e606bcb98707cd5fad81491451487c616
deyvid-dev <deyvidscm@hotmail.com>
2021-05-25 19:24:23 -0300

    Concluindo desafio 7

--- src/challenges.js
+++ src/challenges.js
@@ -34,18 +34,25 @@ function footballPoints(wins, ties) {
 
 // Desafio 6
 function highestCount(array) {
-  
+  let cont = 0;
+  let numberHigh = 0;
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  if(Math.abs((mouse - cat1)) > Math.abs((mouse - cat2))){
+    return "cat2";
+  } else if(Math.abs((mouse - cat1)) < Math.abs((mouse - cat2))){
+    return "cat1"
+  } else {
+    return "os gatos trombam e o rato foge";
+  }
 }
 
 // Desafio 8
-function fizzBuzz(i) {
+function fizzBuzz(array) {
 
-  for (let i = 1; i <= i.length; i++){
+  for (let i = 1; i <= [array.length]; i++){
     if(i % 3 === 0 && i % 5 === 0){
       console.log("fizzBuzz");
     } else if(i % 3 === 0){
