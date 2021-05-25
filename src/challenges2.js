728d0e4f73e71c2daa6b9cc8b73cc2b1fb3dd133
Gabriel Biasoli <bielbiasoli33@icloud.com>
2021-05-25 19:09:09 -0300

    Desafio 11 finalizado

--- src/challenges2.js
+++ src/challenges2.js
@@ -14,12 +14,46 @@ function techList(arrayTech, name) {
     return (arrayTech);
   }
 }
-console.log(techList([], "Lucas"));
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(arrayPhoneNumber) {
+  if (arrayPhoneNumber.length !== 11) {
+    return "Array com tamanho incorreto.";
+  };
+  let countTimes = 0;
+  for (let i = 0; i < arrayPhoneNumber.length; i += 1) {
+    for (let i2 = 0; i2 < arrayPhoneNumber.length; i2 += 1) {
+      if (arrayPhoneNumber[i] == arrayPhoneNumber[i2]) {
+        countTimes += 1;
+      }
+    }
+    if ((countTimes >= 3) || (arrayPhoneNumber[i] > 9) || (arrayPhoneNumber[i] < 0)) {
+      return "não é possível gerar um número de telefone com esses valores"
+    }
+    countTimes = 0;
+  }
+  let phoneNumber = "";
+  for (let i3 = 0; i3 < arrayPhoneNumber.length; i3 += 1) {
+    if (phoneNumber.length == 0) {
+      phoneNumber += "(";
+    }
+    if (phoneNumber.length == 3) {
+      phoneNumber += ")";
+    }
+    if (phoneNumber.length == 4) {
+      phoneNumber += " ";
+    }
+    if (phoneNumber.length == 10) {
+      phoneNumber += "-";
+    }
+
+    phoneNumber += arrayPhoneNumber[i3];
+
+  }
+  return phoneNumber;
+
 }
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, -9, 0, 1]));
 
 // Desafio 12
 function triangleCheck() {
