9d505629670369040906191f41e3a3623b5b4def
Erick Santos <erickosantos.dev@gmail.com>
2021-05-25 14:30:11 -0300

    11 - Crie uma função de Número de Telefone

--- src/challenges2.js
+++ src/challenges2.js
@@ -20,8 +20,55 @@ function techList(arrayTech, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
+function generatePhoneNumber(number) {
   // seu código aqui
+  
+  if (number.length != 11) {
+    return "Array com tamanho incorreto.";
+  } else {
+    let quantNum = []
+    let quantNum3 = false;
+    let foraIntervalo = false;  // Verificar se algum número está fora do intervalo 0 - 9
+
+    // Criando array quantNum inicializado em 0 para cada item no tamanho do array informado
+    for (let i = 0; i < number.length; i++) {
+      quantNum[i] = 0;
+    }
+
+    for (let i = 0; i < number.length; i++) {
+
+      // Verificar número fora do intervalo 0 - 9
+      if((number[i] < 0) || (number[i] > 9)) {
+        foraIntervalo = true;
+      }
+
+      // Comparação entre 
+      for (let j = 0; j < number.length; j++) {
+        if (number[i] == number[j]) {
+          quantNum[i] += 1;
+        }
+      }
+    }
+
+    // Verificação se um número repete mais de 3x 
+    for (let i = 0; i < number.length; i++) {
+      if (quantNum[i] >= 3) {
+        quantNum3 = true;
+      }
+    }
+
+    if (foraIntervalo || quantNum3) {
+      return "não é possível gerar um número de telefone com esses valores";
+    } else {
+      number = number.join('');
+      let ddd = number.slice(0,2);
+      let parte1 = number.slice(2,7);
+      let parte2  = number.slice(7,11);
+      return `(${ddd}) ${parte1}-${parte2}`;
+    }
+
+  }
+  
 }
 
 // Desafio 12
