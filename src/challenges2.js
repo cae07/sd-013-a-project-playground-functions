2d42beb34846f1e8925334ead8db739b09688587
Rogerio P. Da Silva <rogeriop1990cv@gmail.com>
2021-05-25 18:35:03 -0300

    Requisito 10 - Ok

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,14 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(listaDeTecnologias, nome) {
+  let listaDeTech = [];
+  if (listaDeTecnologias.length <= 0) {
+    return 'Vazio!';
+  }
+
+  for (let tecnologia of listaDeTecnologias.sort()) {
+    listaDeTech.push({ name: nome, tech: tecnologia });
+  }
+  return listaDeTech;
 }
 
 // Desafio 11
