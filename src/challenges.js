bf396fa9e55e5c86d7eadce2268e35c57ae0733e
Murilo Rainho <murilorainho01@gmail.com>
2021-05-25 16:22:44 -0300

    Exercícios do 1 ao 8 realizados

--- src/challenges.js
+++ src/challenges.js
@@ -1,42 +1,92 @@
 // Desafio 1
-function compareTrue() {
-  
+function compareTrue(a, b) {
+  if (a === true && b === true) {
+    return true
+  } else {
+    return false
+  }
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  let result = (base * height) / 2
+  return result
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(phrase) {
+  phrase = phrase.split(" ")
+  return phrase
 }
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(arrayStrings) {
+  let first = arrayStrings[0]
+  let last = arrayStrings[arrayStrings.length - 1]
+  let result = `${last}, ${first}`
+  return result
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  let result = (wins * 3) + ties
+  return result
 }
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+function highestCount(arrayNumber) {
+  let highestTimes = 1
+  let highestNumber = arrayNumber[0]
+  for (let index = 1; index < arrayNumber.length; index += 1) {
+    if (arrayNumber[index] > highestNumber) {
+      highestTimes = 1
+      highestNumber = arrayNumber[index]
+    } else if (arrayNumber[index] == highestNumber) {
+      highestTimes += 1
+    }
+  }
+  return highestTimes
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  let result;
+  cat1 -= mouse
+  if (cat1 < 0) {
+    cat1 *= (-1)
+  }
+  cat2 -= mouse
+  if (cat2 < 0) {
+    cat2 *= (-1)
+  }
+  if (cat1 == cat2) {
+    result = "os gatos trombam e o rato foge"
+  } else if (cat1 > cat2) {
+    result = "cat2"
+  } else if (cat2 > cat1) {
+    result = "cat1"
+  }
+  return result
 }
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(arrayNumber) {
+  let result = []
+  for (let index = 0; index < arrayNumber.length; index += 1) {
+    if (arrayNumber[index] % 3 == 0 && arrayNumber[index] % 5 != 0) {
+      result.push("fizz")
+    } else if (arrayNumber[index] % 3 != 0 && arrayNumber[index] % 5 == 0) {
+      result.push("buzz")
+    } else if (arrayNumber[index] % 3 == 0 && arrayNumber[index] % 5 == 0) {
+      result.push("fizzBuzz")
+    } else {
+      result.push("bug!")
+    }
+  }
+  return result
 }
+
+console.log(fizzBuzz([2, 15, 7, 9, 45]));
 
 // Desafio 9
 function encode() {
