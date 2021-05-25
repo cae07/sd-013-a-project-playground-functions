36837d6c228afbba4896a18cf0270b0490eac41c
Diego Vilela <vilelaphp@gmail.com>
2021-05-25 18:56:15 -0300

    challenge 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,23 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(techArray, name) {
+    if (!techArray.length) return 'Vazio!';
+
+    let objList = [];
+
+    for (let element of techArray) {
+        objList.push({
+            tech: element,
+            name: name
+        });
+    }
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions#the_arrow_function_expression_%3E
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Conditional_Operator
+    objList.sort((a, b) => (a.tech > b.tech) ? 1 : -1);
+
+    return objList;
 }
+//console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], 'diego'));
 
 // Desafio 11
 function generatePhoneNumber() {
