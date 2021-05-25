f16df6ea854b62b911a96316d1b971842acf80f5
V. Dionysio <vini.dionysio@gmail.com>
2021-05-25 18:46:54 -0300

    Solving challenges 10 and 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,11 +1,79 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(arrTech, strName) {
+
+  let arrObj = [];
+
+  if (arrTech.length == 0) {
+    return "Vazio!";
+  }
+  else {
+    //order arrTech
+    let aux = '';
+    for (let i = arrTech.length - 1; i >= 0; i -= 1) {
+      for (let j = 0; j < i; j += 1) {
+        if (arrTech[j] > arrTech[j + 1]) {
+          aux = arrTech[j];
+          arrTech[j] = arrTech[j+1];
+          arrTech[j+1] = aux;
+        }
+      }
+    }
+
+    for (let i = 0; i < arrTech.length; i += 1) {
+      arrObj.push({
+        "tech": '',
+        'name': strName
+      });
+    }
+    console.log(arrTech);
+    
+    for (let key in arrTech) {
+      arrObj[key].tech = arrTech[key];
+    }
+  }
+  return arrObj;
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(arr) {
+  /*
+  Documentação utilizada para criação da constante
+  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/some
+  */
+  const invalid = (element) => element < 0 || element > 9 === true;
+  let strOut = '';
+
+  //Checkar se algum elemento repete mais que 3x
+  let count = []
+  const bigger3 = (element) => element >= 3 === true;
+
+  for (let i = 0; i < arr.length; i += 1) {
+    count.push(0);
+    for (let j = 0; j < arr.length; j += 1){
+      if (arr[i] == arr[j]) {
+        count[i] += 1;
+      }
+    }
+  }
+
+  if (arr.length != 11) {
+    return 'Array com tamanho incorreto.';
+  }
+  else if (arr.some(invalid) || count.some(bigger3)) {
+    return 'não é possível gerar um número de telefone com esses valores'
+  }
+  else {
+    for (let i = 0; i < 11; i += 1) {
+      arr[i] = arr[i].toString();
+    }
+    for (let i = 0; i < 11; i += 1) {
+      if (i == 0) {strOut += '(' + arr[i]}
+      else if (i == 1) {strOut +=  arr[i] + ') '}
+      else if (i == 6) {strOut += arr[i] + '-'}
+      else {strOut += arr[i]}
+    }
+  }
+  return strOut;
 }
 
 // Desafio 12
