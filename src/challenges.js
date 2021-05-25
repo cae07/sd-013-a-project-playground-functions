5ae3400446ebebd92de6faca52e963fd4874f310
Caroline Boaventura <caroline.boaventura08@gmail.com>
2021-05-25 16:08:55 -0300

    Resolve o requisito 3

--- src/challenges.js
+++ src/challenges.js
@@ -16,8 +16,31 @@ function calcArea(base, height) {
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(string) {
+  let array = [];             // array vazia, criada para receber as palavras dividas pelo espaço da string original. Vai receber via .push.
+  let letter = "";            //variável vazia, que vai receber as letras da string, para serem jogadas no array.
+
+
+  // O for vai varrer a string original, o index começando na primeira letra e indo até a última, subindo de 1 em 1:
+  for (let index = 0; index < string.length; index += 1) {
+
+    // Se a letra do índice segundo o for for diferente de vazio (" ") E(&&) o índice segundo o for for diferente do índice da última letra, pois nesse caso ele vai para a segunda condição ---> depois da última letra não tem espaço vazio, então a condição anterior não se aplicaria:
+
+    if ((string[index] != " ") && (index != string.length - 1)) {
+      letter = letter + string[index];      // a variável letter recebe a letra referente ao índice do for (sem estar vazia, ou seja, vai receber o conteúdo já existe + o conteúdo novo)
+    } 
+      //A última letra também tem que entrar na variável letter, da mesma forma que o if anterior, por isso criei esse else if, ou seja, se o index for o da última letra (string.length - 1), ela entra dentro da variável letter da mesma forma e com um push envia a variável letter para o array ---> esse passo será realizado somente uma vez, na última letra da string original. 
+      else if (index == string.length - 1) {
+        letter = letter + string[index];
+        array.push(letter);
+      } 
+        // após finalizar uma palavra, o push a envia pra o array e a variável letter é "esvaziada" para que o processo seja iniciado na próxima palavra após o espaço.  
+        else {    
+          array.push(letter);
+          letter = "";
+        } 
+  }
+  return (array);
 }
 
 // Desafio 4
