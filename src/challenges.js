d66e1586b1f9811bb1b3b359b903ee5de292dd02
Julia Baptista <baptista.julia@yahoo.com.br>
2021-05-25 16:30:57 -0300

    5. Crie uma funcao que calcule a quantidade de pontos no futebol

--- src/challenges.js
+++ src/challenges.js
@@ -29,8 +29,9 @@ function concatName(list) {
 
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  let points = (wins * 3) + ties;
+  return points;
 }
 
 // Desafio 6
