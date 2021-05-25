a1d3da3fe15a848b34a83f3e442cba8be63d444b
Erick Santos <erickosantos.dev@gmail.com>
2021-05-25 00:41:53 -0300

    9 - Crie uma função que Codifique e Decodifique

--- src/challenges.js
+++ src/challenges.js
@@ -91,11 +91,23 @@ function fizzBuzz(numeros) {
 }
 
 // Desafio 9
-function encode() {
+function encode(str) {
   // seu código aqui
+  str = str.replace(/a/g, "1");
+  str = str.replace(/e/g, "2");
+  str = str.replace(/i/g, "3");
+  str = str.replace(/o/g, "4");
+  str = str.replace(/u/g, "5");
+  return str;
 }
-function decode() {
+function decode(str) {
   // seu código aqui
+  str = str.replace(/1/g, "a");
+  str = str.replace(/2/g, "e");
+  str = str.replace(/3/g, "i");
+  str = str.replace(/4/g, "o");
+  str = str.replace(/5/g, "u");
+  return str;
 }
 
 module.exports = {
