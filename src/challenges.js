439207c0a2ef928baf278a4f8a163b72aa5de46b
Debora Teodorico <debora.steodorico@gmail.com>
2021-05-25 17:27:21 -0300

    Adicionado a questão 3

--- src/challenges.js
+++ src/challenges.js
@@ -10,8 +10,9 @@ function calcArea(base, height) {
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(string) {
+  let array = string.split(' ');
+  return array;
 }
 
 // Desafio 4
