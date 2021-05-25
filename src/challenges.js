b81379f3369890c5b5e8e27ec20912f9d08199be
Adriana Barreto <adri92barreto123@gmail.com>
2021-05-25 18:20:26 -0300

    Tenta resolver primeira questao

--- src/challenges.js
+++ src/challenges.js
@@ -1,8 +1,17 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
-  
+function compareTrue(valor1, valor2) {
+  if(valor1 == true && valor2 == true){
+    return true;
+  } else {
+    return false;
+  }
 }
+
+console.log(compareTrue(true, true))
+
+
+
+
 
 // Desafio 2
 function calcArea() {
