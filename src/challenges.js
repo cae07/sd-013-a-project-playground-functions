1b9b62969ab9a7971337844e773b90b0dba08f61
Amaury Queiroz Jr <amaury.tqj@gmail.com>
2021-05-25 18:08:27 -0300

    resolvendo requesito 4

--- src/challenges.js
+++ src/challenges.js
@@ -3,27 +3,27 @@ function compareTrue(value1, value2) {
   if (value1 === true && value2 === true) {
     return true;
   } else {
-    return false;
+    return false
   }
 }
 
 // Desafio 2
 function calcArea(base, height) {
   let res = (base * height) / 2;
-
   return res;
 }
 
 // Desafio 3
-function splitSentence(str) {
- let myString = str.split(' ');
-  
- return myString;
+/* Source: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/split */
+function splitSentence(myArray) {
+  const myFrase = myArray.split(' ');
+  return myFrase;
 }
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(myArray) {
+  let result = `${myArray[myArray.length - 1]}, ${myArray[0]}`;
+  return result;
 }
 
 // Desafio 5
