4a64d69404698cee463cc078a1dd6c463482f542
Felipe Lima <felipe_lima10@icloud.com>
2021-05-25 17:01:32 -0300

    8 - Crie uma função FizzBuzz

--- src/challenges.js
+++ src/challenges.js
@@ -90,17 +90,26 @@ function catAndMouse(mouse, cat1, cat2) {
   }else{
     answer = "os gatos trombam e o rato foge"
   }
-
   return(answer)
 }
-
-
-
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(array) {
+  let number = '';
+  let answer = [];
+  for (let index = 0; array.length > index; index+=1) {
+    number = array[index]
+    if (number%5 == 0 && number%3 == 0) {
+      answer.push("fizzBuzz")
+    }else if(number%3 == 0){
+      answer.push("fizz")
+    }else if(number%5 == 0){
+      answer.push("buzz")
+    }else{
+      answer.push("bug!")
+    }
+  }
+  return(answer)
 }
-
 // Desafio 9
 function encode() {
   // seu código aqui
