4f6afd79b628e1e4dc87b9b565dce534ef4b2ead
Matheus Duarte de Freitas <freickup@gmail.com>
2021-05-25 20:16:07 -0300

    tomando uma coça do lint parte 4

--- src/challenges.js
+++ src/challenges.js
@@ -1,10 +1,8 @@
 // Desafio 1
 function compareTrue(a, b) {
   if (a === true && b === true) {
-    return true;
-  } else {
-    return false;
-  }
+  return true;
+  } return false;
 }
 
 // Desafio 2
