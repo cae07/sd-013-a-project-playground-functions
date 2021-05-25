a9ff80dd64e3be10af43105caad313c468e785cc
Luiza Antiques <luiza.antiques@gmail.com>
2021-05-25 14:59:21 -0300

    Conclui requisito 3

--- src/challenges.js
+++ src/challenges.js
@@ -14,8 +14,10 @@ function calcArea(base, height) {
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(string) {
+  let meuArray = string.split(' ');
+
+  return meuArray;
 }
 
 // Desafio 4
