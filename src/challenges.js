0c90c2eb5b910dbd4a9be297fdd0f77f44c4c6ad
IgorFellows <igor.fpp96@gmail.com>
2021-05-25 19:29:36 -0300

    Adiciona a função 7

--- src/challenges.js
+++ src/challenges.js
@@ -38,7 +38,7 @@ for(let index = 0; index < numberArray.length; index +=1) {
     highestNumber = numberArray[index];
     contador = 1;
 
-  } else if (numberArray[index] === highestNumber) {
+  } else if(numberArray[index] === maiorNumber) {
     contador += 1;
   }
   
@@ -48,8 +48,17 @@ return contador
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  let gato1 = Math.abs(cat1 - mouse);
+  let gato2 = Math.abs(cat2 - mouse);
+
+  if (gato1 > gato2) {
+    return 'cat2'
+  } else if (gato2 > gato1) {
+    return 'cat1'
+  } else {
+    return 'os gatos trombam e o rato foge'
+  }
 }
 
 // Desafio 8
