73aaeb321598336f2d3450a47229dc25c9fbf828
Thiago <thiagoc.carboneri@gmail.com>
2021-05-25 18:13:22 -0300

    resolução do exercicio 7

--- src/challenges.js
+++ src/challenges.js
@@ -51,9 +51,17 @@ return maxcount;
 console.log(maxcount); 
 }
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
-}
+function catAndMouse(mouse, cat1,cat2) {
+  let dist1 = Math.abs(cat1-mouse);
+  let dist2 = Math.abs(cat2-mouse);
+  if (dist1>dist2){
+      return 'cat2';
+  } else if (dist2>dist1) {;
+      return 'cat1';
+  } else { 
+      return "os gatos trombam e o rato foge";
+  }
+}     
 
 // Desafio 8
 function fizzBuzz() {
