1a7e63242a62014d82568993a5ebd86a04f995fd
Icaro Ferreira <icaro.iky@gmail.com>
2021-05-25 14:57:56 -0300

    ex 1 e 2

--- src/challenges.js
+++ src/challenges.js
@@ -1,13 +1,16 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
-  
-}
+function compareTrue(value1, value2) {
+  if (value1 === true && value2 === true) {
+    return true;
+  } else {
+    return false;
+  };
+};
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
-}
+function calcArea(base, height) {
+  return (base * height) / 2;
+};
 
 // Desafio 3
 function splitSentence() {
