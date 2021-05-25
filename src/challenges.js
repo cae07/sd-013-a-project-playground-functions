b03db669c72bd84036b1873a5d69a999b2648ff4
Matheus <matheus.castro.macedo.xi@gmail.com>
2021-05-25 15:11:33 -0300

    Iniciando o projeto

--- src/challenges.js
+++ src/challenges.js
@@ -1,6 +1,11 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(valor1,valor2) {
+  if (valor1==true && valor2==true){
+    return true;
+
+  } else {
+     return false;
+  }
 }
 
 // Desafio 2
