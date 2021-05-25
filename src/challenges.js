befb73e3bdac0995b654c03b95890fa648eb64d7
rafa-araujo <rafacarlosaraujo@gmail.com>
2021-05-25 16:10:13 -0300

    Resolução: Desafio 1 e 2

--- src/challenges.js
+++ src/challenges.js
@@ -1,11 +1,17 @@
 // Desafio 1
-function compareTrue() {
-  
+function compareTrue(n1, n2) {
+  if (n1 === true && n2 === true ) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  let area = base * height / 2;
+  
+  return area
 }
 
 // Desafio 3
