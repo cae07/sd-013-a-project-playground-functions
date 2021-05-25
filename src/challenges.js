8acdaf6d4671b39f508191d7df51fb73f71f9c3f
Matheus Camillo <matheuscamillo@hotmail.com.br>
2021-05-25 14:55:41 -0300

    realizado o terceiro desafio

--- src/challenges.js
+++ src/challenges.js
@@ -14,8 +14,9 @@ function calcArea(base, height) {
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(string) {
+  let res = string.split(' ');
+  return res;
 }
 
 // Desafio 4
