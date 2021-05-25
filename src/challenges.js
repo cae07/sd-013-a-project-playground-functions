27b9ebdd3945916e50eba168215d69a86de100e7
Rafael Janovicci <rjanovicci@hotmail.com>
2021-05-25 14:49:52 -0300

    exercício 1

--- src/challenges.js
+++ src/challenges.js
@@ -1,7 +1,13 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
-}
+function compareTrue(val1, val2) {
+  
+  if (val1 === true && val2 === true) {
+    return true;
+  } else {
+    return false;
+  };
+
+};
 
 // Desafio 2
 function calcArea() {
