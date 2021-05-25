4a71b633681240e6dad2a1d4ec3e5ff26968dc4a
Gustavo Mauricio <gmauricio2072gmail.com>
2021-05-25 17:04:42 -0300

    Desafio 1 realizado

--- src/challenges.js
+++ src/challenges.js
@@ -1,9 +1,12 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
-  
+function compareTrue(valor1, valor2) {
+  if(valor1 && valor2 == true){
+    return true;
+  } else{
+    return false;
+  }
 }
-
+console.log(compareTrue(true, true))
 // Desafio 2
 function calcArea() {
   // seu código aqui
