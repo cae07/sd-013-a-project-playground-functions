1efc1b60277ac9bbe527634c1d1a1635f6ca6e9e
Mario <mjfneto@gmail.com>
2021-05-25 19:13:02 -0300

    Adiciona corpo a funcao generatePhoneNumber

--- src/challenges2.js
+++ src/challenges2.js
@@ -8,8 +8,53 @@ function techList(techArray, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(digitsArray) {
+  if (digitsArray.length == 11) {
+    if (
+      !anyOverThree(digitsArray) &&
+      !anyNegative(digitsArray) &&
+      !anyHigherThanNine(digitsArray)
+    ) {
+      let template = '(areaCode) exchangeCode-lineNumber';
+      template = template.replace('areaCode', digitsArray.slice(0, 2).join(''));
+      template = template.replace(
+        'exchangeCode',
+        digitsArray.slice(2, 7).join('')
+      );
+      template = template.replace(
+        'lineNumber',
+        digitsArray.slice(7, 11).join('')
+      );
+      return template;
+    }
+
+    return 'não é possível gerar um número de telefone com esses valores';
+  }
+  return 'Array com tamanho incorreto.';
+
+  // ************************************
+
+  function anyNegative(array) {
+    return array.some(negative);
+  }
+
+  function anyHigherThanNine(array) {
+    return array.some(higherThan(9));
+  }
+
+  function anyOverThree(array) {
+    for (let i = 0; i < array.length; i++) {
+      let counter = 1;
+      for (let j = 0; j < array.length; j++) {
+        if (array[j] == array[i]) {
+          counter++;
+          if (counter > 3) return true;
+        }
+      }
+    }
+
+    return false;
+  }
 }
 
 // Desafio 12
@@ -28,3 +73,15 @@ module.exports = {
   hydrate,
   triangleCheck,
 };
+
+// *********************
+
+function higherThan(a) {
+  return function (b) {
+    return b > a;
+  };
+}
+
+function negative(a) {
+  return a < 0;
+}
