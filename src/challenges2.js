e7c26714e108667072e81bae54a15fb27c835441
Matheus Duarte de Freitas <freickup@gmail.com>
2021-05-25 18:05:58 -0300

    resolucao da questao 12, e validacao pelo npm

--- src/challenges2.js
+++ src/challenges2.js
@@ -19,18 +19,14 @@ function techList(techsList, nome) {
 // Desafio 11
 function generatePhoneNumber(phoneNumbersArray) {
   let phoneObject = {
-      
   }
   let contaRepete = 0;
-
   if (phoneNumbersArray.length < 11 || phoneNumbersArray.length > 11) {
       console.log ('Array com tamanho incorreto.');
       return "Array com tamanho incorreto.";
   }
-
   for (let comparaRepetido of phoneNumbersArray) {
       for (let repetidoComparar of phoneNumbersArray) {
-
           if (repetidoComparar == comparaRepetido) {
               contaRepete += 1
           }
@@ -41,26 +37,26 @@ function generatePhoneNumber(phoneNumbersArray) {
       }
       contaRepete = 0;
   }
-  
-  for (let pegaNum of phoneNumbersArray) {
+    for (let pegaNum of phoneNumbersArray) {
       if (pegaNum < 0 || pegaNum > 9) {
           console.log ('não é possível gerar um número de telefone com esses valores');
           return "não é possível gerar um número de telefone com esses valores";
       }
   }
-
   for (let indexArray in phoneNumbersArray) {
           phoneObject[indexArray] = phoneNumbersArray[indexArray];
       }
-      
-      let phoneNumber = "(" + phoneObject[0] + phoneObject[1] + ") " + phoneObject[2] + phoneObject[3] + phoneObject[4] + phoneObject[5] + phoneObject[6] + "-" + phoneObject[7] + phoneObject[8] + phoneObject[9] + phoneObject[10];
-      console.log (phoneNumber);
-      return phoneNumber; 
+  let phoneNumber = "(" + phoneObject[0] + phoneObject[1] + ") " + phoneObject[2] + phoneObject[3] + phoneObject[4] + phoneObject[5] + phoneObject[6] + "-" + phoneObject[7] + phoneObject[8] + phoneObject[9] + phoneObject[10];
+  return phoneNumber; 
 }
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+function triangleCheck(lineA,lineB,lineC) {
+  if (lineA < lineB + lineC && lineB < lineA + lineC && lineC < lineA + lineB && lineA > Math.abs(lineB - lineC) && lineB > Math.abs(lineA - lineC) && lineC > Math.abs(lineA - lineB)) {
+    return 'true'; 
+  } else {
+    return 'false';
+  }
 }
 
 // Desafio 13
