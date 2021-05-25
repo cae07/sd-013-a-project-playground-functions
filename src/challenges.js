83ecbcfe7d8eba74eb7bdd8230208f606d45bb5c
Silvio Fabian <silviofabian334@gmail.com>
2021-05-25 15:34:29 -0300

    c2 - Desafio 3

--- src/challenges.js
+++ src/challenges.js
@@ -13,8 +13,8 @@ function calcArea(base, height) {
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(value) {
+  return value.split(" ");
 }
 
 // Desafio 4
