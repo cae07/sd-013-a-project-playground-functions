12e2f13ff22f2dced9842dc3f71a9dc6bd9dddcd
Murilo Ribeiro <murilorsv14@gmail.com>
2021-05-25 16:23:12 -0300

    fiz muita coisa estou ficando louco

--- src/challenges.js
+++ src/challenges.js
@@ -1,42 +1,74 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
-  sim
+function compareTrue(bollean1,bollean2) {
+  if(bollean1 == true && bollean2 == true){
+    return true
+  } else {
+    return false
+  }
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base,altura) {
+  return ((base * altura)/2)
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(sentenca) {
+  return sentenca.split(" ")
 }
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(array) {
+  return array[array.length -1].concat(", ",array[0])
 }
 
 // Desafio 5
-function footballPoints() {
+function footballPoints(wins,ties) {
+  return ((wins*3) + (ties*1))
+
   // seu código aqui
 }
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+function highestCount(array) {
+  let maiorNumero = -99;
+  let maiorNumeroSeRepete = 0;
+  for(let i = 0 ; i<array.length ; i += 1){
+    if(array[i]>maiorNumero){
+      maiorNumero = array[i];
+    } else {
+    }
+  }
+  let contador = 0;
+  for (let i = 0; i<array.length ; i += 1){
+    if(maiorNumero == array[i]){
+      contador += 1;
+    } else {
+
+    }
+    maiorNumeroSeRepete = contador;
+  }
+
+
+  return(maiorNumeroSeRepete)
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse,cat1,cat2) {
+  if(cat1 - mouse > cat2 - mouse){
+    return "cat2"
+  } else if (cat2 - mouse > mouse - cat1){
+    return "cat1"
+  } else if (cat2 - cat1 == mouse*2){
+    return "os gatos trombam e o rato foge"
+  } else {
+
+  }
 }
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(arrayDeNumeros) {
+  
 }
 
 // Desafio 9
@@ -59,3 +91,5 @@ module.exports = {
   highestCount,
   splitSentence,
 };
+
+splitSentence("go trybe")
