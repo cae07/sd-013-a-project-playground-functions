6d0652a29500321cdc4b2e2c920922b6f0135ac9
Emídio Rebouças <emidioreb@gmail.com>
2021-05-25 15:16:21 -0300

    Realizacao do requisito 2

--- src/challenges.js
+++ src/challenges.js
@@ -9,7 +9,9 @@ function compareTrue(value1,value2) {
 
 // Desafio 2
 function calcArea(base,height) {
-  
+  return (base*height)/2;
+}
+
 // Desafio 3
 function splitSentence() {
   // seu código aqui
