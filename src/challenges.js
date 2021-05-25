9630330891b63babfb7b2b71a1fc785fe6c8767a
Filipe <filipecandido123@hotmail.com>
2021-05-25 17:27:41 -0300

    Desafio 1 completo

--- src/challenges.js
+++ src/challenges.js
@@ -1,9 +1,12 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
-
+function compareTrue(valor1,valor2) {
+  if(valor1 === true && valor2 === true){
+    return true;
+  }else {
+    return false;
+  }
 }
-
+console.log(compareTrue(false,false))
 // Desafio 2
 function calcArea() {
   // seu código aqui
