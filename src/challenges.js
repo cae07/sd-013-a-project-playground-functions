50f7cecee4f8694c9a0291a3a726a27b24f3ae72
Pedro Verano <pedroverano2009@yahoo.com.br>
2021-05-25 16:34:37 -0300

    Adiciona o requisito 7

--- src/challenges.js
+++ src/challenges.js
@@ -47,7 +47,7 @@ function footballPoints(wins, ties) {
 // Desafio 6
 function highestCount(arrayDeNumeros) {
   // seu código aqui
-  let maiorNumero = 0 ;
+  let maiorNumero = 0;
   let contador = 0;
   for (let i = 0; i < arrayDeNumeros.length; i += 1) {
     if (i === 0) {
@@ -66,8 +66,28 @@ function highestCount(arrayDeNumeros) {
 }
 
 // Desafio 7
-function catAndMouse() {
+function catAndMouse(mouse, cat1, cat2) {
   // seu código aqui
+  let distancat1 = cat1 - mouse;
+  let distancat2 = cat2 - mouse;
+  let resultado = '';
+  if (distancat1 < 0) {
+    distancat1 *= -1;
+  }
+  if (distancat2 < 0) {
+    distancat2 *= -1;
+  }
+  if (distancat1 === distancat2) {
+    resultado = 'os gatos trombam e o rato foge';
+    return resultado;
+  }
+  if (distancat1 < distancat2) {
+    resultado = 'cat1';
+    return resultado;
+  } else if (distancat2 < distancat1) {
+    resultado = 'cat2';
+    return resultado;
+  }
 }
 
 // Desafio 8
