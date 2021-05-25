35cae0b8cae2f8da61ffb7b22247ec81078a4cbe
Pedro Delicoli <pedrodelicoli@hotmail.com>
2021-05-25 19:14:04 -0300

    Requisito 9

--- src/challenges.js
+++ src/challenges.js
@@ -58,41 +58,52 @@ function catAndMouse(mouse, cat1, cat2) {
   
   if (cat1position < cat2position) {
     return 'cat1';
-  }
+   }
   else if (cat1position === cat2position) {
     return 'os gatos trombam e o rato foge';
-  }
+   }
   else {
     return 'cat2';
-  }
+   }
 }
 
 // Desafio 8
 function fizzBuzz(string) {
-  for (let index in string){
-    if (string [index] % 5 === 0 && string[index] % 3 === 0){
-      string[index] = "fizzbuzz";
-    }  
-    else if (string [index] % 5 === 0){
-      string[index] = "buzz";
-    }
-    else if (string[index] % 3 === 0){
-      string[index] = "fizz";
-    }  
-    else { 
-      string[index] = "bug!"
-    }
+  for (let index in string) {
+    if (string[index] % 5 === 0 && string[index] % 3 === 0) {
+      string[index] = 'fizzBuzz';
+     }
+    else if (string [index] % 5 === 0) {
+      string[index] = 'buzz';
+     }
+    else if (string[index] % 3 === 0) {
+      string[index] = 'fizz';
+     }
+    else {
+      string[index] = 'bug!';
+     }
   }
   return string;
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(stringcod) {
+  stringcod = stringcod.replace(/a/g,"1");
+  stringcod = stringcod.replace(/e/g,"2");
+  stringcod = stringcod.replace(/i/g,"3");
+  stringcod = stringcod.replace(/o/g,"4");
+  stringcod = stringcod.replace(/u/g,"5");
+  return stringcod;
 }
-function decode() {
-  // seu código aqui
+function decode(stringcod) {
+  stringcod = stringcod.replace(/1/g,"a");
+  stringcod = stringcod.replace(/2/g,"e");
+  stringcod = stringcod.replace(/3/g,"i");
+  stringcod = stringcod.replace(/4/g,"o");
+  stringcod = stringcod.replace(/5/g,"u");
+  return stringcod; 
 }
+console.log(decode('h3 th2r2!'))
 
 module.exports = {
   calcArea,
