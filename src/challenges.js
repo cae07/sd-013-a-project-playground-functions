894f2c6bb72309c8be840cd667486548198dbaf8
suco <francalucas937@gmail.com>
2021-05-25 15:18:10 -0300

    desafios 1 e 2 concluidos

--- src/challenges.js
+++ src/challenges.js
@@ -1,60 +1,66 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui.
+function compareTrue(param , param2) {
+	if (param && param2 === true) {
+		return true;
+	} else {
+		return false
+	}
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base ,height) {
+	// seu código aqui
+	let result = (base * height) /2;
+	return result
 }
 
 // Desafio 3
 function splitSentence() {
-  // seu código aqui
+	// seu código aqui
 }
 
 // Desafio 4
 function concatName() {
-  // seu código aqui
+	// seu código aqui
 }
 
 // Desafio 5
 function footballPoints() {
-  // seu código aqui
+	// seu código aqui
 }
 
 // Desafio 6
 function highestCount() {
-  // seu código aqui
+	// seu código aqui
 }
 
 // Desafio 7
 function catAndMouse() {
-  // seu código aqui
+	// seu código aqui
 }
 
 // Desafio 8
 function fizzBuzz() {
-  // seu código aqui
+	// seu código aqui
 }
 
 // Desafio 9
 function encode() {
-  // seu código aqui
+	// seu código aqui
 }
 function decode() {
-  // seu código aqui
+	// seu código aqui
 }
 
 module.exports = {
-  calcArea,
-  catAndMouse,
-  compareTrue,
-  concatName,
-  decode,
-  encode,
-  fizzBuzz,
-  footballPoints,
-  highestCount,
-  splitSentence,
+	calcArea,
+	catAndMouse,
+	compareTrue,
+	concatName,
+	decode,
+	encode,
+	fizzBuzz,
+	footballPoints,
+	highestCount,
+	splitSentence,
 };
