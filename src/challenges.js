3b7885a5fbc736fb91d55ff014cb7a3b7faa1d64
Ygor <ygormaiaa1@gmail.com>
2021-05-25 15:35:33 -0300

    Iniciando o projeto

--- src/challenges.js
+++ src/challenges.js
@@ -1,6 +1,10 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(x, y) {
+  if(x == true && y == true) {
+    return true; 
+  } else {
+    return false;
+  }
 }
 
 // Desafio 2
