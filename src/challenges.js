8790beca08bf828f998fb4a5e815265776f97511
Leonardo Santos <lcds90@gmail.com>
2021-05-25 15:45:48 -0300

    desafio 1 ate 4

--- src/challenges.js
+++ src/challenges.js
@@ -1,17 +1,26 @@
 // Desafio 1
-function compareTrue() {
+function compareTrue(booleanTypeOne, booleanTypeTwo) {
+  if (booleanTypeOne && booleanTypeTwo) {
+    return true;
+  }
+  return false;
 }
 
 // Desafio 2
-function calcArea() {
+function calcArea(base, height) {
+  return (base * height) / 2;
 }
 
 // Desafio 3
-function splitSentence() {
+function splitSentence(word) {
+  return word.split(' ');
 }
 
 // Desafio 4
-function concatName() {
+function concatName(words) {
+  let first = words[0];
+  let last = words[words.length - 1];
+  return `${last}, ${first}`;
 }
 
 // Desafio 5
