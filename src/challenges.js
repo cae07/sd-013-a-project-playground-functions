ac5418c6df4a0f5ab3e64f46bd569b369690d66a
Fernando <nando_filipe@hotmail.com>
2021-05-25 19:06:22 -0300

    fernnadofilipe 1-3

--- src/challenges.js
+++ src/challenges.js
@@ -15,8 +15,9 @@ function calcArea(base,height) {
 }
 
 // Desafio 3
-function splitSentence() {
+function splitSentence(string) {
   // seu código aqui
+  return string.split(' ');
 }
 
 // Desafio 4
