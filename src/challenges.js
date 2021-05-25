6b15bec4514849df8f6ab64a14a978b588b3e72a
fgosuen <fgosuendacosta@gmail.com>
2021-05-25 14:47:33 -0300

    Adição do Desafio 03 - Status: Done

--- src/challenges.js
+++ src/challenges.js
@@ -13,8 +13,8 @@ function calcArea(base, height) {
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(stringASplitar) {
+  return stringASplitar.split(" ")
 }
 
 // Desafio 4
