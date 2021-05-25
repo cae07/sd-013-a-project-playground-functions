e46631dabd9a0302fbe07f11a728392988ce8428
Rafhael Gomes <rafhael.andrade@hotmail.com.br>
2021-05-25 14:46:49 -0300

    5 - função footballPoints() implementada

--- src/challenges.js
+++ src/challenges.js
@@ -27,8 +27,8 @@ function concatName(stringArray) {
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  return (wins * 3) + ties;
 }
 
 // Desafio 6
