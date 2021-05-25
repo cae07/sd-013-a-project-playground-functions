8a273ffee04a966b42d10724d2734d12baf35e4f
Eduardomak <eduardomak92@gmail.com>
2021-05-25 16:19:12 -0300

    adicionando exercicios 1 ao 8, menos o 3

--- src/challenges.js
+++ src/challenges.js
@@ -1,43 +1,78 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
-  
+function compareTrue(value1, value2) {
+  if(value1 === value2 && value1 === true){
+    return true
+  }
+  else{
+    return false
+  }
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  return base * height/2
 }
 
 // Desafio 3
 function splitSentence() {
-  // seu código aqui
+  
 }
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(array) {
+  return array[array.length - 1] + ", " + array[0];
 }
-
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  return wins * 3 + ties
 }
-
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+function highestCount(numbers) {
+  let count = 0;
+  let number = numbers[0]
+    for(let indexnum = 0; indexnum < numbers.length; indexnum+=1){
+      if(numbers[indexnum] > number){
+        count = 1;
+        number = numbers[indexnum];
+      }
+      else if (numbers[indexnum] === number){
+        count+=1;
+      }
+    }
+  return count
 }
-
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  if(cat1 > cat2){
+    return "cat2"
+  }
+  else if ((mouse-cat1)+(mouse-cat2) === 0){
+    return "os gatos trombam e o rato foge"
+  }
+  else{
+    return "cat1"
+  }
+}
+// Desafio 8
+function fizzBuzz(array) {
+  let phrase = []
+  for(let index = 0; index < array.length; index +=1){
+    if(array[index] % 3 === 0 && array[index] % 5 === 0){
+      phrase[index]="fizzBuzz"
+    }
+    else if(array[index] % 3 === 0 && array[index] % 5 !== 0){
+      phrase[index]="fizz"
+    }
+    else if(array[index] % 5 === 0 && array[index] % 3 !== 0){
+      phrase[index]="buzz"
+    }
+    else{
+      phrase[index]="bug!"
+    }
+  }
+  return phrase
 }
 
-// Desafio 8
-function fizzBuzz() {
-  // seu código aqui
-}
 
 // Desafio 9
 function encode() {
