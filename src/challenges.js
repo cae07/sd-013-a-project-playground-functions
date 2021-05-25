eb6e5287399a7600776e78367687def9c598df05
Ricardo-Sousa <rwmsousa@gmail.com>
2021-05-25 19:54:43 -0300

    desafios 6 a 8

--- src/challenges.js
+++ src/challenges.js
@@ -35,28 +35,71 @@ function concatName(arrayString) {
   return result.join(', ');
 }
 
-// Desafio 5
+// Desafio 5 - calcule a quantidade de pontos no futebol
 function footballPoints(wins, ties) {
   let points = (wins * 3) + ties;
   return points;
 }
 
-// Desafio 6
-function highestCount() {
-  // seu código aqui
+// Desafio 6 - Crie uma função que calcule a repetição do maior número
+function highestCount(array) {
+  let big = 0;
+  let count = 0;
+  for (let i = 0; i < array.length; i += 1) {
+    if (array[i] > big) {
+      big = array[i];
+      count = 0;
+      count += 1;
+    } else if (array[i] === big) {
+      big = array[i];
+      count += 1;
+    }
+  }
+  return count;
 }
 
-// Desafio 7
-function catAndMouse() {
-  // seu código aqui
+// Desafio 7 - função de Caça ao Rato
+function catAndMouse(mouse, cat1, cat2) {
+  let winner = '';
+  let posCat1 = Math.abs(mouse - cat1);
+  let posCat2 = Math.abs(mouse - cat2);
+
+  if (posCat1 > posCat2) {
+    winner = 'cat2';
+  } else if (posCat1 < posCat2) {
+    winner = 'cat1';
+  } else {
+    winner = 'os gatos trombam e o rato foge';
+  }
+  return winner;
 }
 
-// Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+// Desafio 8 - função FizzBuzz
+function fizzBuzzAux(num){
+  let aux2 = '';
+    if ((num % 3 === 0) && (num % 5 != 0)) {
+      aux2 = 'fizz';
+    } else if ((num % 3 != 0) && (num % 5 === 0)) {
+      aux2 = 'buzz';
+    } else if ((num % 3 === 0) && (num % 5 === 0)) {
+      aux2 = 'fizzBuzz';
+    } else {
+      aux2 = 'bug!';
+    }
+  return aux2;
 }
 
-// Desafio 9
+function fizzBuzz(array) {
+  let newArray = [];
+  for (let index = 0; index < array.length; index += 1) {
+    newArray.push(fizzBuzzAux(array[index]));
+  } return newArray;
+}
+
+let test = [2, 15, 7, 9, 45];
+console.log(fizzBuzz(test));
+
+// Desafio 9 - 
 function encode() {
   // seu código aqui
 }
