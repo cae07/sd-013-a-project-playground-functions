c31e2c76933bb5f4f18e2f3e6619539adb2d3f3a
Rogerio P. Da Silva <rogeriop1990cv@gmail.com>
2021-05-25 18:07:57 -0300

    Requisito 9 - Ok

--- src/challenges.js
+++ src/challenges.js
@@ -79,11 +79,42 @@ function fizzBuzz(listaDeNumeros) {
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(texto) {
+  let saida = '';
+  const vogais = {
+    a: 1,
+    e: 2,
+    i: 3,
+    o: 4,
+    u: 5,
+  };
+  for (let letra of texto) {
+    if (vogais[letra]) {
+      saida += vogais[letra];
+    } else {
+      saida += letra;
+    }
+  }
+  return saida;
 }
-function decode() {
-  // seu código aqui
+
+function decode(texto) {
+  let saida = '';
+  const vogais = {
+    1: 'a',
+    2: 'e',
+    3: 'i',
+    4: 'o',
+    5: 'u',
+  };
+  for (let letra of texto) {
+    if (vogais[letra]) {
+      saida += vogais[letra];
+    } else {
+      saida += letra;
+    }
+  }
+  return saida;
 }
 
 module.exports = {
