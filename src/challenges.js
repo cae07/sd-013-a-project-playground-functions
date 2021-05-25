8eb4513d782d5f1b98a7d91d6929c6f0d52febc7
Isabella Antunes <bella.antunes2@gmail.com>
2021-05-25 15:16:06 -0300

    Resolução dos exercícios 1 e 2

--- src/challenges.js
+++ src/challenges.js
@@ -1,11 +1,14 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(valor1, valor2) {
+  if (valor1 && valor2) {
+    return true;
+  }
+  return false;
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  return (base * height) / 2;
 }
 
 // Desafio 3
