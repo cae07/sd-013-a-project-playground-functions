32ec1bbe12eeab61557a7064f1ec35c99445fa82
Ricardo-Sousa <rwmsousa@gmail.com>
2021-05-25 16:04:06 -0300

    desafios de 1 a 5

--- src/challenges.js
+++ src/challenges.js
@@ -1,26 +1,39 @@
-// Desafio 1
-function compareTrue() {
-  // seu código aqui
+// Desafio 1 - Compara se dois valores são true
+function compareTrue(num1,num2) {
+  if (num1 && num2 == true){
+  return true;
+  }else {return false;}
 }
 
-// Desafio 2
-function calcArea() {
-  // seu código aqui
+// Desafio 2 - calcula a área de um triângulo
+function calcArea(base, height) {
+  let result = (base * height) / 2
+  return result;
 }
 
-// Desafio 3
-function splitSentence() {
-  // seu código aqui
+// Desafio 3 - cria um array com as strings separadas de uma frase
+function splitSentence(stringSentence) {
+  let array = stringSentence.split(" ");
+  return array;
 }
 
-// Desafio 4
-function concatName() {
-  // seu código aqui
+// Desafio 4 - concatenação de strings, retornando o primeiro item e o último item de um array
+function concatName(arrayString) {
+  let result = [];
+  for(let i = 0; i < arrayString.length; i+=1){
+    if(i==0){
+      result.push(arrayString[i])}
+    if(i==arrayString.length-1){
+      result.push(arrayString[i])}
+  }
+  result = result.reverse();
+  return result.join(', ');
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  let points = (wins*3) + ties;
+  return points;
 }
 
 // Desafio 6
