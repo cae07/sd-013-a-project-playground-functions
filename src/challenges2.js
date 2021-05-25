801149ab6884f5808ed00c04123005b1a3835a18
Luiza Antiques <luiza.antiques@gmail.com>
2021-05-25 19:32:39 -0300

    Conclui requisito 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,22 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(array, name) {
+  array.sort();
+  let novoArray = [];
+  let result = novoArray;
+
+  if (array.length > 0) {
+    for (let index of array) {
+      let objeto = {
+        tech: index,
+        name,
+      };
+      novoArray.push(objeto);
+    }
+  } else {
+    result = 'Vazio!';
+  }
+
+  return result;
 }
 
 // Desafio 11
