fcbaed958de4b9eec2e8c54db1fc27d07f251509
Silvio Fabian <silviofabian334@gmail.com>
2021-05-25 18:54:24 -0300

    c6 - Desafio 7

--- src/challenges.js
+++ src/challenges.js
@@ -50,8 +50,14 @@ function highestCount(value) {
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  if (Math.abs((mouse - cat1)) > Math.abs((mouse - cat2))) {
+    return 'cat2';
+  } else if (Math.abs((mouse - cat1)) < Math.abs((mouse - cat2))) {
+    return 'cat1';
+  } else {
+    return "os gatos trombam e o rato foge";
+  }
 }
 
 // Desafio 8
