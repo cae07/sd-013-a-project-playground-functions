0430b010dae37c10cede990a338eb572992e7862
Thiago <thiagoc.carboneri@gmail.com>
2021-05-25 16:12:30 -0300

    resolução dos exercicios 1,2 e 3

--- src/challenges.js
+++ src/challenges.js
@@ -1,16 +1,23 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(value1, value2) {
+  if (value1 == true && value2 ==true) {
+    return true;
+  } else {
+    return false;
+  }
+    
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  let area = (base*height)/2;
+  return area;
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(str) {
+  str = str.split(" ");
+  return str
 }
 
 // Desafio 4
