2ff7388e60fd0deb00ccf30c15862ed0d30c5166
Julia Baptista <baptista.julia@yahoo.com.br>
2021-05-25 17:33:32 -0300

    7. Crie uma funcao de Caça ao Rato

--- src/challenges.js
+++ src/challenges.js
@@ -57,9 +57,23 @@ function highestCount(numbers) {
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  let distCat1 = Math.abs(mouse - cat1);
+  let distCat2 = Math.abs(mouse - cat2);
+
+  if(distCat1 < distCat2) {
+    return "cat1";
+  }
+  else if (distCat1 > distCat2) {
+    return "cat2";
+  }
+  else if (distCat1 === distCat2) {
+    return "os gatos trombam e o rato foge"
+  }
 }
+
+
+
 
 // Desafio 8
 function fizzBuzz() {
