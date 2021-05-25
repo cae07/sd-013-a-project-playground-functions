20b37a04bcd8d813054671e5f13b928e7bad7627
Julia Baptista <baptista.julia@yahoo.com.br>
2021-05-25 19:57:17 -0300

    9. Crie uma funcao que Codifique e Decodifique

--- src/challenges.js
+++ src/challenges.js
@@ -97,12 +97,29 @@ function fizzBuzz(numbers) {
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+// Desafio realizado com a ajuda da Luiza Antiques
+function encode(string) {
+  string = string.replace(/a/gi, '1');
+  string = string.replace(/e/gi, '2');
+  string = string.replace(/i/gi, '3');
+  string = string.replace(/o/gi, '4');
+  string = string.replace(/u/gi, '5');
+  return string;
 }
-function decode() {
-  // seu código aqui
+
+// encode('hi there!');
+
+function decode(string) {
+  string = string.replace(/1/gi, 'a');
+  string = string.replace(/2/gi, 'e');
+  string = string.replace(/3/gi, 'i');
+  string = string.replace(/4/gi, 'o');
+  string = string.replace(/5/gi, 'u');
+  return string;
 }
+
+
+
 
 module.exports = {
   calcArea,
