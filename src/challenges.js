1b3ece4b805996977db31d01b923a4a9e3353814
Murilo Ribeiro <murilorsv14@gmail.com>
2021-05-25 17:53:57 -0300

    termino do challenges1.js

--- src/challenges.js
+++ src/challenges.js
@@ -1,30 +1,30 @@
 // Desafio 1
-function compareTrue(bollean1,bollean2) {
-  if(bollean1 == true && bollean2 == true){
-    return true
+function compareTrue(bollean1, bollean2) {
+  if (bollean1 === true && bollean2 === true){
+    return true;
   } else {
-    return false
+    return false;
   }
 }
 
 // Desafio 2
-function calcArea(base,altura) {
-  return ((base * altura)/2)
+function calcArea(base, altura) {
+  return ((base * altura) / 2);
 }
 
 // Desafio 3
 function splitSentence(sentenca) {
-  return sentenca.split(" ")
+  return sentenca.split('');
 }
 
 // Desafio 4
 function concatName(array) {
-  return array[array.length -1].concat(", ",array[0])
+  return array[array.length -1].concat( ', ' ,array[0]);
 }
 
 // Desafio 5
-function footballPoints(wins,ties) {
-  return ((wins*3) + (ties*1))
+function footballPoints(wins, ties) {
+  return ((wins * 3) + (ties * 1));
 
   // seu código aqui
 }
@@ -33,43 +33,39 @@ function footballPoints(wins,ties) {
 function highestCount(array) {
   let maiorNumero = -99;
   let maiorNumeroSeRepete = 0;
-  for(let i = 0 ; i<array.length ; i += 1){
-    if(array[i]>maiorNumero){
+  for (let i = 0 ; i<array.length ; i += 1){
+    if (array[i] > maiorNumero) {
       maiorNumero = array[i];
     } else {
     }
   }
   let contador = 0;
-  for (let i = 0; i<array.length ; i += 1){
-    if(maiorNumero == array[i]){
+  for (let i = 0; i < array.length ; i += 1) {
+    if (maiorNumero === array[i]) {
       contador += 1;
     } else {
 
     }
     maiorNumeroSeRepete = contador;
   }
-
-
   return(maiorNumeroSeRepete)
 }
 
 // Desafio 7
-function catAndMouse(mouse,cat1,cat2) {
+function catAndMouse(mouse, cat1, cat2) {
   if(cat1 - mouse > cat2 - mouse){
-    return "cat2"
+    return ' cat2 ';
   } else if (cat2 - mouse > mouse - cat1){
-    return "cat1"
-  } else if (cat2 - cat1 == mouse*2){
-    return "os gatos trombam e o rato foge"
+    return ' cat1 ';
   } else {
-
+    return ' os gatos trombam e o rato foge ';
   }
 }
 
 // Desafio 8
 function fizzBuzz(arrayDeNumeros) {
   let arrayDeResposta = []
-  for (let index = 0; index < arrayDeNumeros.length ; index += 1){
+  for (let index = 0; index < arrayDeNumeros.length ; index += 1) {
     let numero = arrayDeNumeros[index]
     if (divisivelPor3e5(numero) == true){
       arrayDeResposta.push("fizzBuzz")
@@ -118,26 +114,44 @@ function encode(string) {
     o:4,
     u:5
   }
-  arraySaida = []
+  palavraSaida = ""
   for (let index = 0; index < string.length ; index += 1){
     let letraTeste = string[index]
-  
     for (let key in decifrador){
-      if (letraTeste == key){
-        letraTeste[index] = decifrador[key]
-        arraySaida.push(letraTeste)
-      } else {
-        arraySaida.push(letraTeste)
+      if (letraTeste === key){
+        letraTeste = decifrador[key]
       }
-
     }
+    palavraSaida += letraTeste
 
   }
-  return arraySaida
+
+return palavraSaida
 
 }
-function decode() {
-  // seu código aqui
+
+function decode(string) {
+  let desdecifrador = {
+    1:'a',
+    2:'e',
+    3:'i',
+    4:'o',
+    5:'u'
+  }
+  palavraSaida = ""
+  for (let index = 0; index < string.length ; index += 1){
+    let letraTeste = string[index]
+    for (let key in desdecifrador){
+      if (letraTeste === key){
+        letraTeste = desdecifrador[key]
+      }
+    }
+    palavraSaida += letraTeste
+
+  }
+
+return palavraSaida
+
 }
 
 module.exports = {
