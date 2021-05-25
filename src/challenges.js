02663c4f7d9c3677d1f2afdc81f5c44fbc2319f1
Adriano Monteiro <adrianomonteiroweb@gmail.com>
2021-05-25 20:12:20 -0300

    Correçao: Requisito 7: funçao que retorna qual gato esta mais proximo da posiçao do rato ou se o rato foge

--- src/challenges.js
+++ src/challenges.js
@@ -66,15 +66,25 @@ function highestCount(array) {
 
 // Desafio 7
 function catAndMouse(cat1,cat2,mouse) {
-  if ((cat1 - mouse) < (cat2 - mouse)) {
-    return 'cat1';
-  } else if ((cat1 - mouse) > (cat2 - mouse)) {
-    return 'cat2';
+  if (mouse < cat1 && mouse < cat2) {
+    if ((cat1 - mouse) < (cat2 - mouse)) {
+      return 'cat1';
+    } else if ((cat1 - mouse) > (cat2 - mouse)) {
+      return 'cat2';
+    } else {
+      return 'os gatos trombam e o rato foge';
+    }
   } else {
-    return 'os gatos trombam e o rato foge';
+    if ((mouse - cat1) > (mouse - cat2)) {
+      return 'cat1';
+    } else if ((mouse - cat1) < (mouse - cat2)) {
+      return 'cat2';
+    } else {
+      return 'os gatos trombam e o rato foge';
+    }
   }
 }
-
+console.log(catAndMouse(6,12,10));
 // Desafio 8
 function fizzBuzz() {
   // seu código aqui
