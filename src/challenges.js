ff487c4009ee263760a95a212951ff21903d9945
Silvio Fabian <silviofabian334@gmail.com>
2021-05-25 15:17:33 -0300

    c0.3 - Correçao

--- src/challenges.js
+++ src/challenges.js
@@ -1,18 +1,10 @@
 // Desafio 1
-function compareTrue(value1 , value2) {
-  if (value1 === true && value2 === true) {
-    return true;
-  }
-  return false;
-}
-
 function compareTrue(value1,value2) {
   if (value1 === true && value2 === true) {
     return true;
   } else {
     return false;
   }
-
 }
 
 // Desafio 2
