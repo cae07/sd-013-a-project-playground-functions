5981eb69a1ff49d4023b115999feec49f0553dc1
Diego Vilela <vilelaphp@gmail.com>
2021-05-25 19:47:11 -0300

    challenge 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -20,8 +20,35 @@ function techList(techArray, name) {
 //console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], 'diego'));
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function isValidNumber(int, array) {
+    if (int < 0 || int > 9) return false;
+
+    if ((array.filter(el => el === int).length) >= 3) return false;
+
+    return true;
+}
+
+function applyMask(intArray) {
+    // Elementos adicionados "in-place"
+    // https://en.wikipedia.org/wiki/In-place_algorithm
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/splice
+    intArray.splice(0, 0, ['(']);
+    intArray.splice(3, 0, [')']);
+    intArray.splice(4, 0, [' ']);
+    intArray.splice(10, 0, ['-']);
+}
+
+function generatePhoneNumber(intArray) {
+    if (intArray.length !== 11) return 'Array com tamanho incorreto.';
+
+    let msg = 'não é possível gerar um número de telefone com esses valores';
+    for (let i = 0; i < intArray.length; i++) {
+        if (!isValidNumber(intArray[i], intArray)) return msg;
+    }
+
+    applyMask(intArray); // in-place
+
+    return intArray.join('');
 }
 
 // Desafio 12
