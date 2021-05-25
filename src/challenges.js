25b33282288cfe263ba5227377ad5c139d4f46b4
Ingo Reichert Junior <ingoreichertjr@gmail.com>
2021-05-25 17:37:34 -0300

    Adiciona segundo requisito

--- src/challenges.js
+++ src/challenges.js
@@ -9,9 +9,10 @@ function compareTrue(a,b) {
   console.log(compareTrue(true,true));
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base,height) {
+  return (base * height)/2
 }
+  console.log(calcArea(10,6))
 
 // Desafio 3
 function splitSentence() {
