bfb5fc98cbfc1c975e34ede6b81b5be8e95ca408
Leonardo Santos <lcds90@gmail.com>
2021-05-25 19:09:35 -0300

    reqs obrigatorios

--- src/challenges.js
+++ src/challenges.js
@@ -108,9 +108,23 @@ function fizzBuzz(numbersArray) {
 }
 
 // Desafio 9
-function encode() {
+
+function encode(string) {
+  string = string.replaceAll('a', '1');
+  string = string.replaceAll('e', '2');
+  string = string.replaceAll('i', '3');
+  string = string.replaceAll('o', '4');
+  string = string.replaceAll('u', '5');
+  return string;
 }
-function decode() {
+
+function decode(string) {
+  string = string.replaceAll('1', 'a');
+  string = string.replaceAll('2', 'e');
+  string = string.replaceAll('3', 'i');
+  string = string.replaceAll('4', 'o');
+  string = string.replaceAll('5', 'u');
+  return string;
 }
 
 module.exports = {
