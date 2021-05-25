51a8b8f560fe880f9dd914dd3695fb4b27f00e8a
V. Dionysio <vini.dionysio@gmail.com>
2021-05-25 14:54:48 -0300

    Solving challenges 1,2 and 3

--- src/challenges.js
+++ src/challenges.js
@@ -1,16 +1,16 @@
 // Desafio 1
-function compareTrue() {
-  
+function compareTrue(param1, param2) {
+  return param1 && param2;
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  return (base * height)/2;
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(str) {
+  return str.split(' ');
 }
 
 // Desafio 4
