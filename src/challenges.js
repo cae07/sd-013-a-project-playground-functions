45e284841a475fc987b57a561c62d91372656ce5
Olavio Timoteo <olaviotimoteo@hotmail.com>
2021-05-25 16:40:11 -0300

    Adiciona resolução dos desafios 1 a 6

--- src/challenges.js
+++ src/challenges.js
@@ -1,32 +1,51 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
-
-}
+function compareTrue(value1, value2) {
+  if (value1 === true && value2 === true) {
+    return true;
+  } else {
+    return false;
+  };
+};
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  let calc = (base * height) / 2;
+  return calc;
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(str) {
+  return str.split(" ")
 }
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(array) {
+  for (const key in array) {
+    return array[array.length - 1] + ", " + array[0];
+  }
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  return (wins * 3) + (ties);
 }
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+function highestCount(array) {
+  let hightNumber = array[0];
+  let count = 0;
+  for (let index = 0; index < array.length; index += 1) {
+    if (array[index] >= hightNumber) {
+      hightNumber = array[index];
+    }
+  }
+
+  for (let index = 0; index < array.length; index += 1) {
+    if (array[index] === hightNumber) {
+      count += 1
+    }
+  }
+  return count;
 }
 
 // Desafio 7
