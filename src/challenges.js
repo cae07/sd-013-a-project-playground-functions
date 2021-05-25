5f0829c01d89a158a6728823ff16658b5f2266ae
Caê <eduardokae01@hotmail.com>
2021-05-25 16:05:04 -0300

    exercicio 2 - project playground functions

--- src/challenges.js
+++ src/challenges.js
@@ -2,14 +2,14 @@
 function compareTrue(valueA, valueB) {
   if (valueA === true && valueB === true) {
     return true;
-  } else {
-    return false;
-  }  
+  }
+  return false;
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  let area = (base * height) / 2;
+  return area;
 }
 
 // Desafio 3
