60fa03b6513c681324d47515892e2ebb43faccff
deyvid-dev <deyvidscm@hotmail.com>
2021-05-25 17:18:05 -0300

    Concluindo desafio 3 e 4

--- src/challenges.js
+++ src/challenges.js
@@ -15,16 +15,17 @@ function calcArea(base, heigth) {
 function splitSentence(string) {
   return string.split(" ");
 }
-
-
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(array) {
+  let result = array[array.length-1]+ ", " + array[0]; 
+  return result;
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  let pontsTime = 0;
+  wins = 3;
+  ties = 1;
 }
 
 // Desafio 6
