6c87e45f7dfb9eea7f1a969d82d5bac15020fb48
Diego Vilela <vilelaphp@gmail.com>
2021-05-25 15:22:48 -0300

    challenges 2 to 7

--- src/challenges.js
+++ src/challenges.js
@@ -8,33 +8,49 @@ function compareTrue(a, b) {
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+    return (base * height) / 2;
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(str) {
+    return str.split(' ');
 }
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(strArray) {
+    return `${strArray[strArray.length - 1]}, ${strArray[0]}`
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+    return (wins * 3) + ties;
 }
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+function highestCount(numbersArray) {
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/max
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_syntax
+    let highestNumber = Math.max(...numbersArray);
+
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions#the_arrow_function_expression_%3E
+    return numbersArray.filter(element => element === highestNumber).length;
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/abs
+    let cat1Distance = Math.abs(cat1 - mouse);
+    let cat2Distance = Math.abs(cat2 - mouse);
+
+    if (cat1Distance < cat2Distance) {
+        return 'cat1';
+    } else if (cat2Distance < cat1Distance) {
+        return 'cat2';
+    } else {
+        return 'os gatos trombam e o rato foge';
+    }
 }
 
 // Desafio 8
