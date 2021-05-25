367fdaa76a4d64799ba74a6131d27bb6b8d55abe
Pedro Verano <pedroverano2009@yahoo.com.br>
2021-05-25 15:56:16 -0300

    Adiciona o requisito 6

--- src/challenges.js
+++ src/challenges.js
@@ -18,38 +18,51 @@ function calcArea(base, height) {
 // Desafio 3
 function splitSentence(frase) {
   // seu código aqui
-  let arraySeparado = frase.split(" ");
+  let arraySeparado = frase.split(' ');
   return arraySeparado;
 }
 
 // Desafio 4
 function concatName(arrayX) {
   // seu código aqui
-  let ultimoPrimeiro = "";
-  for(let i = arrayX.length ; i > 0; i-= 1)
-  {
+  let ultimoPrimeiro = '';
+  for (let i = arrayX.length; i > 0; i -= 1) {
     if (i === arrayX.length) {
-      ultimoPrimeiro = arrayX[arrayX.length - 1] + ", ";
-    }
-    else if(i === 1) {
+      ultimoPrimeiro = arrayX[arrayX.length - 1] + ', ';
+    } else if (i === 1) {
       ultimoPrimeiro += arrayX[0];
     }
   }
   return ultimoPrimeiro;
-
 }
 
 // Desafio 5
 function footballPoints(wins, ties) {
   // seu código aqui
   let pontos = 0;
-  pontos = (wins * 3) + ties
+  pontos = (wins * 3) + ties;
   return pontos;
 }
 
 // Desafio 6
-function highestCount() {
+function highestCount(arrayDeNumeros) {
   // seu código aqui
+  let maiorNumero = 0 ;
+  let contador = 0;
+  for (let i = 0; i < arrayDeNumeros.length; i += 1) {
+    if (i === 0) {
+      maiorNumero = arrayDeNumeros[0];
+    }
+    if (maiorNumero <= arrayDeNumeros[i]) {
+      maiorNumero = arrayDeNumeros[i];
+    }
+  }
+  for (let j = 0; j < arrayDeNumeros.length; j += 1) {
+    if (maiorNumero === arrayDeNumeros[j]) {
+      contador += 1;
+    }
+  }
+  return contador;
 }
 
 // Desafio 7
