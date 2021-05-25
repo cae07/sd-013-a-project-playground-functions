416389d9a83186df352993afa6c8039bc2465c9e
Japa <wesleyy_@live.com>
2021-05-25 16:36:24 -0300

    Adicionado exercício 5

--- src/challenges.js
+++ src/challenges.js
@@ -35,9 +35,14 @@ function concatName(array) {
 console.log(concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']));
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  let vitorias = wins * 3;
+  let empates = ties * 1;
+  let results = vitorias + empates;
+  return results;
 }
+
+console.log(footballPoints(0,0));
 
 // Desafio 6
 function highestCount() {
