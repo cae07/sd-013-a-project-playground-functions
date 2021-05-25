c1c3a00620a0e035049a034cdfd00d99e08e484a
Gean Gonçalves <gean.a.l.c@gmail.com>
2021-05-25 14:17:13 -0400

    Requisito 4

--- src/challenges.js
+++ src/challenges.js
@@ -27,11 +27,9 @@ function splitSentence(frase) {
   return palavras;
 }
 
-console.log(splitSentence('foguete'));
-
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(array) {
+  return `${array[array.length - 1]}, ${array[0]}`;
 }
 
 // Desafio 5
