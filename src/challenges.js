6ce524aa1c3b0f84807cc0470149224067a25d42
icaroMelo <exemployt@gmail.com>
2021-05-25 18:36:44 -0300

    teste

--- src/challenges.js
+++ src/challenges.js
@@ -2,14 +2,14 @@
 // corrigir--->
 function compareTrue(value, value1) {
 
-  if(value > 0 && value1 > 0 ){
+  if(value ===true && value1 === true ){
     return true;
   } else {
     return false;
   }
-  
+
 }
-console.log(compareTrue(2, 3));
+console.log(compareTrue());
 // <---corrigir
 
 // Desafio 2
@@ -34,17 +34,22 @@ function concatName(string1) {
   let concat = (string1[string1.length-1] + ", " + string1[0])
   return concat;
 }
-console.log(concatName());
+console.log(concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']));
 
 // Desafio 5
-function footballPoints() {
-  
-  
+function footballPoints(wins, ties) {
+  let winspoints = 3*wins;
+  let tiespoints = 1*ties;
+  let points = winspoints + tiespoints;
+  return points;
 }
+console.log(footballPoints(14,8));
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+function highestCount(array1) {
+  
+
+  
 }
 
 // Desafio 7
