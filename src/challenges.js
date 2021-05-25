1fbe96f3d2b90ae2ca40b5cb29c93586213fc79f
Rodrigo Pova <rodrigopova94@gmail.com>
2021-05-25 16:16:49 -0300

    adiciona requisitos 7, 8 e 9

--- src/challenges.js
+++ src/challenges.js
@@ -60,21 +60,72 @@ function highestCount(array) {
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  if (Math.abs(mouse - cat1) == Math.abs(mouse - cat2)) {
+    return "os gatos trombam e o rato foge"
+  } else if ((Math.abs(mouse - cat1) < Math.abs(mouse - cat2))) {
+    return 'cat1';
+  } else {
+    return 'cat2';
+  }
 }
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(array) {
+  let result = [];
+  for (let i of array) {
+    if (i % 3 == 0 && i % 5 == 0) {
+      result.push("fizzBuzz")
+    } else if (i % 3 != 0 && i % 5 != 0) {
+      result.push("bug!")
+    } else if (i % 3 == 0) {
+      result.push("fizz")
+    } else if (i % 5 == 0) {
+      result.push("buzz")
+    }
+  }
+  return result;
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(string) {
+  child = ""
+  for (let i of string) {
+    if (i == 'a') {
+      child += 1;
+    } else if (i == 'e') {
+      child += 2;
+    } else if (i == 'i') {
+      child += 3;
+    } else if (i == 'o') {
+      child += 4;
+    } else if (i == 'u') {
+      child += 5;
+    } else {
+      child += i;
+    }
+  }
+  return child;
 }
-function decode() {
-  // seu código aqui
+
+function decode(string) {
+  child = ""
+  for (let i of string) {
+    if (i == 1) {
+      child += 'a';
+    } else if (i == 2) {
+      child += 'e';
+    } else if (i == 3) {
+      child += 'i';
+    } else if (i == 4) {
+      child += 'o';
+    } else if (i == 5) {
+      child += 'u';
+    } else {
+      child += i;
+    }
+  }
+  return child;
 }
 
 module.exports = {
