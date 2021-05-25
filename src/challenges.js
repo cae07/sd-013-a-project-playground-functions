5c8ba9782c86ad246718abd4418eabe16c1edd66
vitorgonzaga <vitor.gonzagaf1@gmail.com>
2021-05-25 14:45:04 -0300

    execucao do requisito 1 - funcao compare true

--- src/challenges.js
+++ src/challenges.js
@@ -1,7 +1,10 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
-  
+function compareTrue(a, b) {
+  if (a == true && b == true) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 // Desafio 2
