db9b4e6a3afd9f0df8b14a299b40e448e6558665
Rafael Victor <rafavictorgs@gmail.com>
2021-05-25 16:06:10 -0300

    Resolve desafio 5 do projeto Playground functions

--- src/challenges.js
+++ src/challenges.js
@@ -27,8 +27,9 @@ function concatName(stringsArray) {
 
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  let totalPoints = (wins * 3) + ties;
+  return totalPoints;
 }
 
 // Desafio 6
