0c97961b6fb33c08c6c4a77f699265db3d0665e8
Guilherme Couto da Silva <gui.couto90@yahoo.com.br>
2021-05-25 16:57:34 -0300

    Conclusao dos exercicios 1,2,3,5 e 7

--- src/challenges.js
+++ src/challenges.js
@@ -1,38 +1,58 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
-  
+function compareTrue(a,b) {
+  if(a == true && b == true){
+    return true;
+  } else {
+    return false;
+  }
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  let area = (base*height)/2;
+  return area;
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(frase) {
+  let arrayFrase =[];
+  arrayFrase = frase.split(" ");
+  return arrayFrase;
 }
+
 
 // Desafio 4
 function concatName() {
-  // seu código aqui
+
 }
 
+
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  let points = (wins*3)+(ties*1);
+  return points;
 }
 
 // Desafio 6
 function highestCount() {
-  // seu código aqui
+  
 }
 
+
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  if(cat1 == cat2){
+    return 'os gatos trombam e o rato foge';
+  }else if((cat1 - mouse) == (mouse - cat2)){
+    return 'os gatos trombam e o rato foge';
+  }else if(cat2<cat1){
+    return 'cat2';
+  }else if(cat1<cat2){
+    return 'cat1';
+  }
 }
+
+console.log(catAndMouse(1,22,22));
 
 // Desafio 8
 function fizzBuzz() {
