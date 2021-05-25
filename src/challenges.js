af5146190e08e8edc3e62a4b1bb4cbe159e7305b
Emídio Rebouças <emidioreb@gmail.com>
2021-05-25 15:24:21 -0300

    Realizacao do requisito 3

--- src/challenges.js
+++ src/challenges.js
@@ -13,9 +13,10 @@ function calcArea(base,height) {
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(string) {
+  return string.split(" ");
 }
+
 
 // Desafio 4
 function concatName() {
