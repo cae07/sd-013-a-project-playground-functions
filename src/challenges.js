3de114ed64a3909f74f72f918093fd1c05b6d913
fgosuen <fgosuendacosta@gmail.com>
2021-05-25 16:50:37 -0300

    Adição do Desafio 09 - Status: Done

--- src/challenges.js
+++ src/challenges.js
@@ -55,43 +55,56 @@ function catAndMouse(mouse, cat1, cat2) {
   distanciaMouseCat2 = mouse - cat2;
   if (distanciaMouseCat1 < 0) { distanciaMouseCat1 *= -1; }
   if (distanciaMouseCat2 < 0) { distanciaMouseCat2 *= -1; }
+  let cat;
   if (distanciaMouseCat1 < distanciaMouseCat2) {
-    return "cat1";
+    cat = 'cat1';
   } else if (distanciaMouseCat2 < distanciaMouseCat1) {
-    return "cat2";
+    cat = 'cat2';
   } else {
-    return "os gatos trombam e o rato foge";
+    cat = 'os gatos trombam e o rato foge';
   }
+  return cat;
 }
 
 // Desafio 8
 function fizzBuzz(arrayDeNumeros) {
   let novoArray = [];
-  let adiciona = "";
+  let adiciona = '';
   for (let indexD8 = 0; indexD8 < arrayDeNumeros.length; indexD8 += 1) {
     if ((arrayDeNumeros[indexD8] % 3) === 0) {
-      adiciona = "fizz";
+      adiciona = 'fizz';
     }
     if ((arrayDeNumeros[indexD8] % 5) === 0) {
-      adiciona = "buzz";
+      adiciona = 'buzz';
     }
     if (((arrayDeNumeros[indexD8] % 3) === 0) && ((arrayDeNumeros[indexD8] % 5) === 0)) {
-      adiciona = "fizzBuzz";
+      adiciona = 'fizzBuzz';
     } else if (((arrayDeNumeros[indexD8] % 3) !== 0) && ((arrayDeNumeros[indexD8] % 5) !== 0)) {
-      adiciona = "bug!";
+      adiciona = 'bug!';
     }
     novoArray.push(adiciona);
-    adiciona = "";
+    adiciona = '';
   }
   return novoArray;
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(string) {
+  string = string.replace(/a/g, '1');
+  string = string.replace(/e/g, '2');
+  string = string.replace(/i/g, '3');
+  string = string.replace(/o/g, '4');
+  string = string.replace(/u/g, '5');
+  return string;
 }
-function decode() {
-  // seu código aqui
+
+function decode(string) {
+  string = string.replace(/1/g, 'a');
+  string = string.replace(/2/g, 'e');
+  string = string.replace(/3/g, 'i');
+  string = string.replace(/4/g, 'o');
+  string = string.replace(/5/g, 'u');
+  return string;
 }
 
 module.exports = {
