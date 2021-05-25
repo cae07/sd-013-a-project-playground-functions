611c4401f0fec14426f1dc0bf58a03fd01f91700
Bruno Pinho <brunopinho@outlook.com>
2021-05-25 18:32:32 -0300

    desafio 07 feito

--- src/challenges.js
+++ src/challenges.js
@@ -84,8 +84,18 @@ function highestCount(numbers) {
 console.log(highestCount([9, 1, 2, 3, 9, 5, 7]));
 
 // Desafio 7
-function catAndMouse() {
+function catAndMouse(mouse, cat1, cat2) {
   // seu código aqui
+  let distance1 = Math.abs(cat1 - mouse);
+  let distance2 = Math.abs(cat2 - mouse);
+
+  if (distance1 < distance2) {
+    return 'cat1';
+  } else if (distance1 > distance2) {
+    return 'cat2';
+  } else if (distance1 === distance2) {
+    return 'os gatos trombam e o rato foge'
+  }
 }
 
 // Desafio 8
