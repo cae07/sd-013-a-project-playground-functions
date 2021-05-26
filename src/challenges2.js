9b3310e3882e02de074ccfa5ccabd4122c7c185f
Felipe Dias <felipeferreiradias1@gmail.com>
2021-05-25 21:54:09 -0300

    adiciona requisito 12

--- src/challenges2.js
+++ src/challenges2.js
@@ -22,9 +22,42 @@ function techList(myArray, nome) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+vetor = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1];
+
+function generatePhoneNumber(myArray) {
+  let msg = "Array com tamanho incorreto."
+  let msgDois = "não é possível gerar um número de telefone com esses valores";
+  let numAtual = 0;
+  let cont = 0;
+  let res;
+
+  if (myArray.length != 11) {
+    return msg;   
+  }
+  for (let index = 0; index < myArray.length; index += 1) {
+    if (myArray[index] < 0 || myArray[index] > 9) {
+      return msgDois; 
+    }    
+  }
+  for (let j = 0; j < myArray.length; j++) {
+    numAtual = myArray[j];
+    for (let k = 0; k < myArray.length; k++) {
+      if (numAtual == myArray[k]) {
+        cont += 1;
+      } 
+    }
+    if (cont >= 3){
+      return msgDois;
+    }
+    cont = 0;
+  }
+  
+  res = `(${myArray[0]}${myArray[1]}) ${myArray[2]}${myArray[3]}${myArray[4]}${myArray[5]}${myArray[6]}-${myArray[7]}${myArray[8]}${myArray[9]}${myArray[10]}`
+
+  return res;  
 }
+
+console.log(generatePhoneNumber(vetor));
 
 // Desafio 12
 function triangleCheck() {
