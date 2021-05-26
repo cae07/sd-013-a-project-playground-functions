b9aca4278949e1a172a61042e364847fdcc1e99b
Luiz Casimiro <luizcasimi@gmail.com>
2021-05-26 05:22:33 -0300

    reduce lint errors

--- src/challenges.js
+++ src/challenges.js
@@ -1,11 +1,10 @@
 // Desafio 1
 function compareTrue(p1, p2) {
+  let result = false;
   if (p1 === true && p2 === true) {
-    return true;
+    result = true;
   }
-  else {
-    return false;
-  }
+  return result;
 }
 
 // Desafio 2
@@ -20,7 +19,7 @@ function splitSentence(string) {
 
 // Desafio 4
 function concatName(array) {
-  return array[array.length - 1] + ', ' + array[0];
+  return `${array[array.length - 1]}${', '}${array[0]}`;
 }
 
 // Desafio 5
@@ -38,12 +37,11 @@ function highestCount(array) {
   }
 
   let times = 0;
-  for (index = 0; index < array.length; index += 1) {
+  for (let index = 0; index < array.length; index += 1) {
     if (highestNumber === array[index]) {
       times += 1;
     }
   }
-
   return times;
 }
 
@@ -51,92 +49,68 @@ function highestCount(array) {
 function catAndMouse(mouse, cat1, cat2) {
   let distCat1 = Math.abs(cat1 - mouse);
   let distCat2 = Math.abs(cat2 - mouse);
-
+  let result = '';
   if (distCat1 < distCat2) {
-    return 'cat1';
+    result = 'cat1';
+  } else if (distCat1 > distCat2) {
+    result = 'cat2';
+  } else {
+    result = 'os gatos trombam e o rato foge';
   }
-  else if (distCat1 > distCat2) {
-    return 'cat2';
-  }
-  else {
-    return 'os gatos trombam e o rato foge';
-  }
+  return result;
 }
 
 // Desafio 8
 function fizzBuzz(array) {
   let result = [];
-
   for (let index = 0; index < array.length; index += 1) {
-
-    if (array[index] % 3 === 0 && array[index] % 5 !== 0) {
-      result.push('fizz');
-    }
-    else if (array[index] % 3 !== 0 && array[index] % 5 === 0) {
-      result.push('buzz');
-    }
-    else if (array[index] % 3 === 0 && array[index] % 5 === 0) {
+    if (array[index] % 3 === 0 && array[index] % 5 === 0) {
       result.push('fizzBuzz');
-    }
-    else {
+    } else if (array[index] % 3 === 0) {
+      result.push('fizz');
+    } else if (array[index] % 5 === 0) {
+      result.push('buzz');
+    } else {
       result.push('bug!');
     }
-
   }
-
   return result;
 }
 
 // Desafio 9
 function encode(string) {
   let separate = string.split('');
-
   for (let index = 0; index < separate.length; index += 1) {
-
     if (separate[index] === 'a') {
       separate[index] = '1';
-    }
-    else if (separate[index] === 'e') {
+    } else if (separate[index] === 'e') {
       separate[index] = '2';
-    }
-    else if (separate[index] === 'i') {
+    } else if (separate[index] === 'i') {
       separate[index] = '3';
-    }
-    else if (separate[index] === 'o') {
+    } else if (separate[index] === 'o') {
       separate[index] = '4';
-    }
-    else if (separate[index] === 'u') {
+    } else if (separate[index] === 'u') {
       separate[index] = '5';
     }
-
   }
-
   return separate.join('');
 }
 
 function decode(string) {
   let separate = string.split('');
-
   for (let index = 0; index < separate.length; index += 1) {
-
     if (separate[index] === '1') {
       separate[index] = 'a';
-    }
-    else if (separate[index] === '2') {
+    } else if (separate[index] === '2') {
       separate[index] = 'e';
-    }
-    else if (separate[index] === '3') {
+    } else if (separate[index] === '3') {
       separate[index] = 'i';
-    }
-    else if (separate[index] === '4') {
+    } else if (separate[index] === '4') {
       separate[index] = 'o';
-    }
-    else if (separate[index] === '5') {
+    } else if (separate[index] === '5') {
       separate[index] = 'u';
     }
-
   }
-
   return separate.join('');
 }
 
