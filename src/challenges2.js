80cce5fdf895c7cc5b0d784b4600e59930e6d24c
Matheus Duarte de Freitas <freickup@gmail.com>
2021-05-26 09:03:34 -0300

    refatoracao da questao 12

--- src/challenges2.js
+++ src/challenges2.js
@@ -15,7 +15,48 @@ function techList(techsList, nome) {
 }
 
 // Desafio 11
-function generatePhoneNumber(phoneNumbersArray) {
+function mountPhoneNumber (phoneNumbersArray) {
+  let phoneObject = {};
+  for (let indexArray in phoneNumbersArray) {
+    phoneObject[indexArray] = phoneNumbersArray[indexArray];
+  }
+  let phoneNumber = '(' + phoneObject[0] + phoneObject[1] + ') ' + phoneObject[2] + phoneObject[3] + phoneObject[4] + phoneObject[5] + phoneObject[6] + '-' + phoneObject[7] + phoneObject[8] + phoneObject[9] + phoneObject[10];
+  return phoneNumber;
+}
+
+function impossiblePhoneNumber (phoneNumbersArray) {
+  for (let pegaNum of phoneNumbersArray) {
+    if (pegaNum < 0 || pegaNum > 9) {
+      return true;
+    }
+    return false;
+  }
+}
+
+function phoneNumberSize (phoneNumbersArray) {
+  if (phoneNumbersArray.length < 11 || phoneNumbersArray.length > 11) {
+    return true;
+  }
+  return false;
+}
+
+
+function repetePhoneNumber (phoneNumbersArray) {
+  let contaRepete = 0;
+  for (let comparaRepetido of phoneNumbersArray) {
+    for (let repetidoComparar of phoneNumbersArray) {
+      if (repetidoComparar == comparaRepetido) {
+        contaRepete += 1;
+      }
+      if (contaRepete >= 3) {
+        return 'não é possível gerar um número de telefone com esses valores';
+      }
+    }
+    contaRepete = 0;
+  }
+}
+
+function generatePhoneNumber (phoneNumbersArray) {
   let phoneObject = {};
   let contaRepete = 0;
   if (phoneNumbersArray.length < 11 || phoneNumbersArray.length > 11) {
@@ -40,38 +81,17 @@ function generatePhoneNumber(phoneNumbersArray) {
   for (let indexArray in phoneNumbersArray) {
     phoneObject[indexArray] = phoneNumbersArray[indexArray];
   }
-  let phoneNumber =
-    '(' +
-    phoneObject[0] +
-    phoneObject[1] +
-    ') ' +
-    phoneObject[2] +
-    phoneObject[3] +
-    phoneObject[4] +
-    phoneObject[5] +
-    phoneObject[6] +
-    '-' +
-    phoneObject[7] +
-    phoneObject[8] +
-    phoneObject[9] +
-    phoneObject[10];
+  let phoneNumber = '(' + phoneObject[0] + phoneObject[1] + ') ' + phoneObject[2] + phoneObject[3] + phoneObject[4] + phoneObject[5] + phoneObject[6] + '-' + phoneObject[7] + phoneObject[8] + phoneObject[9] + phoneObject[10];
   return phoneNumber;
 }
 
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
   if (
-    lineA < lineB + lineC &&
-    lineB < lineA + lineC &&
-    lineC < lineA + lineB &&
-    lineA > Math.abs(lineB - lineC) &&
-    lineB > Math.abs(lineA - lineC) &&
-    lineC > Math.abs(lineA - lineB)
-  ) {
+    lineA < lineB + lineC && lineB < lineA + lineC && lineC < lineA + lineB && lineA > Math.abs(lineB - lineC) && lineB > Math.abs(lineA - lineC) && lineC > Math.abs(lineA - lineB)) {
     return true;
-  } else {
-    return false;
   }
+  return false;
 }
 
 // Desafio 13
@@ -79,25 +99,18 @@ function hydrate(barString) {
   let regularExp = /\d+/g;
   let stringBebedeira = 0;
   let somatorio = 0;
-  // console.log(barString);
-
   for (let drinks of barString) {
     if (drinks.match(regularExp) !== null) {
       stringBebedeira += drinks.match(regularExp);
     }
   }
-
   for (let somador of stringBebedeira) {
-    somatorio += parseInt(somador);
+    somatorio += parseInt(somador, 10);
   }
-  // console.log(somatorio);
   if (somatorio > 1) {
-    // console.log(`${somatorio} copos de água`);
     return `${somatorio} copos de água`;
-  } else {
-    // console.log(`${somatorio} copo de água`);
-    return `${somatorio} copo de água`;
   }
+  return `${somatorio} copo de água`;
 }
 module.exports = {
   generatePhoneNumber,
