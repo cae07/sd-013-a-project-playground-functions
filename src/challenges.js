65610ee115fdb44727b5a7ef4484902fc918dbf5
Leme <andresleme@gmail.com>
2021-05-25 22:43:15 -0300

    Desafio 6

--- src/challenges.js
+++ src/challenges.js
@@ -33,18 +33,50 @@ function footballPoints(wins, ties) {
 };
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
-}
+// Referencia para Math: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/max
+
+function highestCount(numeros) {
+  let maior = numeros.reduce(function(a, b) {
+    return Math.max(a, b);
+  });
+  let quantidade = 0;
+  
+  for (let valor in numeros) {
+    if (numeros[valor] == maior) {
+      quantidade += 1;    
+    }; 
+  };
+  return quantidade;
+};
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
-}
+// consultar math.abs
+function catAndMouse(mouse, cat1, cat2) {
+  if ((mouse - cat1) < (mouse - cat2)) {
+    return `cat1`;
+  } else if ((mouse - cat1) > (mouse - cat2)) {
+    return `cat2`;
+  } else if ((mouse - cat1) == (mouse - cat2)) {
+    return `os gatos trombam e o rato foge`
+  };
+};
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(numeros) {
+  let array = [];
+
+  for (let numero in numeros) {
+    if ((numeros[numero] % 3 == 0) && (numeros[numero] % 5 == 0)) {
+      array += 'fizzBuzz';
+    } else if (numeros[numero] % 3 == 0) {
+      array += 'fizz';
+    } else if (numeros[numero] % 5 == 0) {
+      array += `buzz`;
+    } else {
+      array += 'bug!';
+    }
+  }
+  return array;
 }
 
 // Desafio 9
