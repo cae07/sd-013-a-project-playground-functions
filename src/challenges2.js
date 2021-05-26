b5b2c39cbfecc35acf9bbb1ae5a6c82b8019ef83
Tatiane Chiarello <tatichiarello@gmail.com>
2021-05-26 18:32:07 +0200

    resolve questao 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -18,8 +18,33 @@ function techList(arr, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(arr) {
+  if (arr.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  }
+
+  for (let i = 0; i < arr.length; i += 1) {
+    const value = arr[i];
+    let count = 0;
+
+    for (let j = 0; j < arr.length; j += 1) {
+      if (value === arr[j]) {
+        count += 1;
+      }
+    }
+
+    if (value < 0 || value > 9 || count >= 3) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+  }
+
+  let telefonNumber = '(xx) xxxxx-xxxx';
+
+  for (let i = 0; i < arr.length; i += 1) {
+    telefonNumber = telefonNumber.replace('x', arr[i]);
+  }
+
+  return telefonNumber;
 }
 
 // Desafio 12
