d9f31bc390f8cf1a11144637641a3af79e03e2bf
Gustavo Mauricio <gmauricio2072gmail.com>
2021-05-26 11:02:34 -0300

    Exercício 9 Realizado

--- src/challenges.js
+++ src/challenges.js
@@ -58,24 +58,68 @@ if(Math.abs(cat1 - mouse) > Math.abs(cat2 - mouse)){
   vencedor = "cat2";
 } else if (Math.abs(cat1 - mouse) < Math.abs(cat2 - mouse)){
   vencedor ="cat1";
+} else {
+    vencedor = "os gatos trombam e o rato foge";
 }
- else {
-  vencedor = "os gatos trombam e o rato foge";
+  return vencedor;
 }
-return vencedor
-}
-console.log(catAndMouse(0,3,3))
+console.log(catAndMouse(0, 3, 3));
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(rogerio) {
+  let myString = [] ;
+  for( let index = 0; index < rogerio.length; index += 1){
+  if( rogerio[index] % 3 == 0 && rogerio[index] % 5 == 0){
+    myString.push("fizzBuzz");  
+  }else if(rogerio[index] % 3 == 0){    
+    myString.push("fizz");
+  } else if (rogerio[index] % 5 == 0){    
+    myString.push("buzz");
+  }else{
+    myString.push("bug!");    
+   }
+ }
+  return myString
 }
-
+console.log(fizzBuzz ([2, 15, 7, 9, 45]))
+// Ajuda do Josue Lobo e Rogério P. da Silva
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(value) {
+  let value2 =""
+
+  for (let i in value)
+  if(value[i] == "a"){
+    value2 += 1;
+  }else if (value[i] == "e"){
+    value2 += 2;
+  } else if (value[i] == "i"){
+    value2 += 3;
+  } else if (value[i] == "o"){
+    value2 += 4;
+  }else if (value[i] == "u") {
+    value2 += 5;
+  }else {
+    value2 += value[i];
+  }
+  return value2;
 }
-function decode() {
-  // seu código aqui
+function decode(value) {
+  let value2 =""
+
+  for (let i in value)
+  if(value[i] == 1){
+    value2 += "a";
+  }else if (value[i] == 2){
+    value2 += "e";
+  } else if (value[i] == 3){
+    value2 += "i";
+  } else if (value[i] == 4){
+    value2 += "o";
+  }else if (value[i] == 5) {
+    value2 += "u";
+  }else {
+    value2 += value[i];
+  }
+  return value2;
 }
 
 module.exports = {
