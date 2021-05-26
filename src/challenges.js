e71c617fd375ae94d2e6a0524592ec094d350b52
Wesley Maia <maia.weol@gmail.com>
2021-05-26 12:57:50 -0300

    Requisito 9- função que Codifique e Decodifique

--- src/challenges.js
+++ src/challenges.js
@@ -29,7 +29,7 @@ function concatName(arrayString) {
   // seu código aqui
   let primeiroItem = arrayString[arrayString.length - 1];
   let ultimoItem = arrayString[0];
-  let result = primeiroItem + ', ' + ultimoItem;
+  let result = `${primeiroItem}, ${ultimoItem}`;
   return result;
 }
 
@@ -90,11 +90,46 @@ function fizzBuzz(arrayNum) {
 }
 
 // Desafio 9
-function encode() {
+function encode(frase) {
   // seu código aqui
+  let novaString = '';
+  let array = frase.split('');
+  for (let index = 0; index < array.length; index += 1) {
+    if (array[index] === 'a') {
+      array[index] = '1';
+    } else if (array[index] === 'e') {
+      array[index] = '2';
+    } else if (array[index] === 'i') {
+      array[index] = '3';
+    } else if (array[index] === 'o') {
+      array[index] = '4';
+    } else if (array[index] === 'u') {
+      array[index] = '5';
+    }
+    novaString += array[index];
+  }
+  return novaString;
 }
-function decode() {
+
+function decode(frase) {
   // seu código aqui
+  let novaString = '';
+  let array = frase.split('');
+  for (let index = 0; index < array.length; index += 1) {
+    if (array[index] === '1') {
+      array[index] = 'a';
+    } else if (array[index] === '2') {
+      array[index] = 'e';
+    } else if (array[index] === '3') {
+      array[index] = 'i';
+    } else if (array[index] === '4') {
+      array[index] = 'o';
+    } else if (array[index] === '5') {
+      array[index] = 'u';
+    }
+    novaString += array[index];
+  }
+  return novaString;
 }
 
 module.exports = {
