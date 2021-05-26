384a47d7c8d917becdc2d73a23f1e9619606dd5c
Filipe <filipecandido123@hotmail.com>
2021-05-26 10:59:52 -0300

    desafio 6 completed

--- src/challenges.js
+++ src/challenges.js
@@ -2,14 +2,14 @@
 function compareTrue(valor1, valor2) {
   if (valor1 === true && valor2 === true) {
     return true;
-  }else {
+  } else {
     return false;
   }
 }
-console.log(compareTrue(false,false))
+console.log(compareTrue(false, false))
 // Desafio 2
 function calcArea(base, height) {
-  return (base * height) /2
+  return (base * height) / 2
 }
 let sum = calcArea(10, 50)
 let sum2 = calcArea(5, 2)
@@ -23,7 +23,7 @@ function splitSentence(string) {
 console.log(splitSentence("go trybe vamo que vamo foguete"))
 // Desafio 4
 function concatName(phrase) {
-  let concatenacao = phrase[phrase.length -1] + ", " + phrase[0];
+  let concatenacao = phrase[phrase.length - 1] + ", " + phrase[0];
   return concatenacao
 }
 let name = concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']);
@@ -36,43 +36,62 @@ function footballPoints(wins, ties) {
   let empate = 1 * ties;
   let result = vitory + empate;
   return result
-  }
+}
 let soma = footballPoints(14, 8)
 let soma2 = footballPoints(1, 2)
 let soma3 = footballPoints(0, 0)
-console.log(soma,soma2,soma3)
+console.log(soma, soma2, soma3)
 // Desafio 6
-function highestCount() {
-  // seu código aqui
-}
+let array1 = [9, 1, 2, 3, 9, 5, 7]
+let array2 = [0, 4, 4, 4, 9, 2, 1];
+let array3 = [0, 0, 0]
+function highestCount(array1) {
+  let maiorNumero = array1[0];
+  let counter = 0;
 
-// Desafio 7
-function catAndMouse() {
-  // seu código aqui
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] >= maiorNumero) {
+      maiorNumero = array1[i]
+    }
+  }
+  for(let j = 0; j <array1.length; j++) {
+    if(array1[j] === maiorNumero){
+      counter++
+    }
+  }
+  return counter
 }
+  let totalSoma = highestCount(array1)
+  let totalSoma2 = highestCount(array2)
+  let totalSoma3 = highestCount(array3)
+  console.log(totalSoma, totalSoma2, totalSoma3)
+  // Desafio 7
+  function catAndMouse() {
+    // seu código aqui
+  }
 
-// Desafio 8
-function fizzBuzz() {
-  // seu código aqui
-}
+  // Desafio 8
+  function fizzBuzz() {
+    // seu código aqui
+  }
 
-// Desafio 9
-function encode() {
-  // seu código aqui
-}
-function decode() {
-  // seu código aqui
-}
+  // Desafio 9
+  function encode() {
+    // seu código aqui
+  }
+  function decode() {
+    // seu código aqui
+  }
 
-module.exports = {
-  calcArea,
-  catAndMouse,
-  compareTrue,
-  concatName,
-  decode,
-  encode,
-  fizzBuzz,
-  footballPoints,
-  highestCount,
-  splitSentence,
-};
+  module.exports = {
+    calcArea,
+    catAndMouse,
+    compareTrue,
+    concatName,
+    decode,
+    encode,
+    fizzBuzz,
+    footballPoints,
+    highestCount,
+    splitSentence,
+  };
