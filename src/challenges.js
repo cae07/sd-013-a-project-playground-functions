9c140dea17f3f2603389e54fa326c9ddffd9a243
Samuel Guilherme <samuel.guilherme.l@hotmail.com>
2021-05-26 01:50:44 -0300

    resolvendo alguns exercicios do projeto Playground Functions

--- src/challenges.js
+++ src/challenges.js
@@ -1,44 +1,110 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
-  
+function compareTrue(value1, value2) {
+  if (value1 === true && value2 === true){
+    return true;
+  }
+  else {
+    return false;
+  }
+
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
-}
+function calcArea(base, height) {
+  return (base * height) / 2;
 
+}
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(string) {
+  return string.split([" "]);
+
+  
 }
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(string) {
+  let ultimoItem = 0;
+  let primeiroItem = 0;
+
+  primeiroItem = string[primeiroItem]
+  ultimoItem = string[string.lenght-1]
+
+  return primeiroItem + ultimoItem;
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  pontos = (wins*3) + ties*1;
+  return pontos;
 }
-
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+function highestCount(param1) {
+    let contN = 0;
+    let contRepetidoMaior = 0;
+    let contRepetidoMaior2 = 0;
+    for (let indice in param1) {
+    if (param1[indice] >= param1[contN+1]){
+    contRepetidoMaior ++;
+    
+    }
+    if (param1[contN+1]>=param1[indice]) {
+      contRepetidoMaior2 ++;
+    }
+    if (contRepetidoMaior2 >= contRepetidoMaior ){
+      return contRepetidoMaior2;
+    }
+    else {
+      return contRepetidoMaior;
+    }
+  }
 }
+    
+    
+  
+  
+console.log(highestCount([9, 1, 2, 3, 9, 5, 7]));
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  let string = "os gatos trombam e o rato foge";
+ if (mouse + cat1 < mouse + cat2) {
+   return 'cat1';   
+ }
+ else if (mouse + cat2 < mouse + cat1) {
+   return 'cat2'; 
+ }
+else if((mouse + cat2 === mouse + cat1) || (mouse + cat1 === mouse + cat2) ) {
+  return string;
+}
 }
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(value1) {
+  let fizz = "fizz";
+  let buzz = "buzz";
+  let bug = "bug!";
+  let fizzbuz1 = "fizzBuzz"
+  let caixa;;
+  for (let index in value1){
+    if (value1[index]% 3 == 0) {
+      caixa += fizz.splitSentence();
+      
+    }
+    else if (value1[index]% 5 == 0){
+      caixa += buzz.splitSentence();
+      
+    }
+    else if ((value1[index]% 3 == 0) && (value1[index]% 5 == 0)) {
+      caixa += fizzbuz1.splitSentence();
+      
+    }
+    else if ((value1[index]% 3 ==! 0) && (value1[index]% 5 ==! 0)) {
+      caixa += bug.splitSentence();
+      
+  }
+  return value1=caixa;
 }
-
+}
 // Desafio 9
 function encode() {
   // seu código aqui
@@ -46,6 +112,8 @@ function encode() {
 function decode() {
   // seu código aqui
 }
+
+
 
 module.exports = {
   calcArea,
@@ -58,4 +126,4 @@ module.exports = {
   footballPoints,
   highestCount,
   splitSentence,
-};
+}
--- src/challenges2.js
+++ src/challenges2.js
@@ -1,17 +1,63 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
-}
+function techList(tech, name) {
+  let vazio = "Vazio!"
+  if (tech == 0) {
+    return vazio;
+    
+  }
+    for (let key in tech){
+  tech[key] = {
+    tecnologia: tech[key],
+    nome: name,   
+  
+  
+  }
+  
 
+}
+  return tech.sort();
+  }
+  let array = ["React", "Jest", "HTML", "CSS", "JavaScript"]
+
+  
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(value) {
+  let incorretTam = "Array com tamanho incorreto";
+  let quantidadeNum = 0;
+  let incorrectValor = "não é possível gerar um número de telefone com esses valores";
+  for (let key in value){
+    quantidadeNum +=1 ;
+  }
+  if (quantidadeNum ==! 10){
+  return incorretTam;
+  }
+  for (let key1 in value){
+    let repetNumber = 1;
+    if (value[key1]===value[repetNumber]){
+      repetNumber += 1;
+      
+    }
+    if ((repetNumber>3)||(value[key1]>9)||(value[key1]<0)){
+      return incorrectValor;
+    }
+
+  }
 }
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
-}
+function triangleCheck(lineA, lineB, lineC) {
+  if (Math.abs(lineA+lineB < lineC) || (Math.abs(lineC+lineB < lineA)) || (Math.abs(lineC+lineA > lineB)) ){
+    return false;
+  }
+  if (lineA || lineB || lineC < Math.abs(lineA-lineB)||Math.abs(lineA-lineC)||Math.abs(lineC-lineB)){
+    return false;
+  }
+  if (lineA || lineB || lineC < (lineA+lineB)||(lineA+lineC)||(lineC+lineB) && lineA || lineB || lineC > Math.abs(lineA-lineB)|| Math.abs(lineA-lineC)||Math.abs(lineC-lineB)) {
+    return true;
+    
+  }
+
+  }
 
 // Desafio 13
 function hydrate() {
