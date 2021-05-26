97289fc02257368a3e074157c54022f8520e16a5
Bruno Pinho <brunopinho@outlook.com>
2021-05-25 21:17:12 -0300

    desafio 10 iniciado. falta resolver alguns detalhes

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,39 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(tech, name) {
+  /* Documentação sobre .push consultada: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/push
+  Referência sobre como checar se um array está vazio: https://flexiple.com/check-if-array-empty-javascript/#:~:text=If%20the%20length%20of%20the,the%20function%20will%20return%20False
+  Metódo de sort retirado de: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort 
+  */
+  let allTechs = [];
+
+  for (let index in tech){
+    if (tech.length == 0) {
+      return 'Vazio!';
+    } else {
+      let objeto = {
+        tech: tech[index],
+        name: name,
+      }
+    }
+
+    allTechs.push(objeto);
+  }
+
+  return allTechs.sort(function(a, b) {
+    let techA = a.tech.toLowerCase();
+    let techB = b.tech.toLowerCase();
+
+    if (techA < techB) {
+      return -1;
+    }
+    if (techA > techB) {
+      return 1;
+    }
+
+    return 0;
+  });
 }
+console.log(techList([], 'Lucas'));
 
 // Desafio 11
 function generatePhoneNumber() {
