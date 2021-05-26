ac75e1aa5f1ee9f5fb9389869b4c7745f708a11b
Julia Baptista <baptista.julia@yahoo.com.br>
2021-05-26 11:34:53 -0300

    Crie uma funcao de Numero de Telefone

--- src/challenges2.js
+++ src/challenges2.js
@@ -24,10 +24,83 @@ function techList(array, name) {
 // techList(tecnologias, name);
 
 
-// Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+let phoneNumber = [1, 1, 3, 4, 5, 6, 7, 8, 9, 0, 1, 1];
+
+// Verifica se numero é menor que zero ou maior que nove
+function verificaNumero(array) {
+  let verificacao = true;
+
+  for(let i = 0; i < array.length; i += 1 ) {
+    if(array[i] > 9 || array[i] < 0) {
+      verificacao = false;
+    }
+  }
+  return verificacao;
 }
+
+// Verifica quantidade de repeticoes
+function verificaRepeticoes(array) {
+  let maxRepeticoes = 0;
+  let repeticoes = 0;
+  for (let index in array) {
+    let numeroAtual = array[index];
+
+    for (let index2 in array) {
+      if(numeroAtual === array[index2]) {
+        repeticoes += 1;
+      }
+    }
+
+    if(repeticoes > maxRepeticoes) {
+      maxRepeticoes = repeticoes;
+    }
+    repeticoes = 0;
+  }
+
+  return maxRepeticoes;
+}
+
+
+function formatNumber(array) {
+  let region = `(${array[0]}${array[1]})`;
+  let firstHalf = "";
+  let secondHalf = "";
+  let phoneNumber = "";
+
+  for (let index = 2; index <= 6; index += 1) {
+    firstHalf += array[index];
+  }
+
+  for (let index = 7; index <= 10; index += 1) {
+    secondHalf += array[index];
+  }
+
+  phoneNumber = `${region} ${firstHalf}-${secondHalf}`;
+
+  return phoneNumber;
+}
+
+// formatNumber(phoneNumber);
+
+
+
+// Desafio 11
+function generatePhoneNumber(numbers) {
+   
+  if (numbers.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  }
+  else if (verificaNumero(numbers) === false || verificaRepeticoes(numbers)>= 3) {
+    return 'não é possível gerar um número de telefone com esses valores';
+  }
+  else {
+    return formatNumber(numbers);
+  }
+}
+
+// generatePhoneNumber(phoneNumber);
+
+
 
 // Desafio 12
 function triangleCheck() {
