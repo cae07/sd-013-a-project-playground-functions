fd1f42d962a14aa9d961b620fcb9c47befc2e0fb
Ricardo-Sousa <rwmsousa@gmail.com>
2021-05-26 13:17:21 -0300

    desafio 9 completo

--- src/challenges.js
+++ src/challenges.js
@@ -96,15 +96,43 @@ function fizzBuzz(array) {
   } return newArray;
 }
 
-let test = [2, 15, 7, 9, 45];
-console.log(fizzBuzz(test));
-
-// Desafio 9 - 
-function encode() {
-  // seu código aqui
+// Desafio 9 - função que Codifique e Decodifique
+function encode(string) {
+  let array = string.split('');
+  for(let i = 0; i < array.length; i += 1){
+    if (array[i] === 'a'){
+      array[i] = 1;
+    }else if (array[i] === 'e'){
+      array[i] = 2;
+    }else if (array[i] === 'i'){
+      array[i] = 3;
+    }else if (array[i] === 'o'){
+      array[i] = 4;
+    }else if (array[i] === 'u'){
+      array[i] = 5;
+    }
+  }
+  let newString = array.join('');
+  return newString;
 }
-function decode() {
-  // seu código aqui
+
+function decode(string) {
+ let array = string.split('');
+  for(let i = 0; i < array.length; i += 1){
+    if (array[i] === '1'){
+      array[i] = 'a';
+    }else if (array[i] === '2'){
+      array[i] = 'e';
+    }else if (array[i] === '3'){
+      array[i] = 'i';
+    }else if (array[i] === '4'){
+      array[i] = 'o';
+    }else if (array[i] === '5'){
+      array[i] = 'u';
+    }
+  }
+  let newString = array.join('');
+  return newString;
 }
 
 module.exports = {
@@ -119,3 +147,4 @@ module.exports = {
   highestCount,
   splitSentence,
 };
+
