d21303fffa9e3abd0d350cf5ae62e0e564ffa1f3
Felipe Dias <felipeferreiradias1@gmail.com>
2021-05-25 21:26:14 -0300

    corrige requisito 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -2,7 +2,7 @@
 function techList() {
   msg = "Vazio!"
 
-  if (myArray.length == 0) {
+  if (techList(myArray.length) == 0) {
     return msg;    
   } else {
 
@@ -20,3 +20,25 @@ function techList() {
     return newArray;
   }
 }
+
+// Desafio 11
+function generatePhoneNumber() {
+  // seu código aqui
+}
+
+// Desafio 12
+function triangleCheck() {
+  // seu código aqui
+}
+
+// Desafio 13
+function hydrate() {
+  // seu código aqui
+}
+
+module.exports = {
+  generatePhoneNumber,
+  techList,
+  hydrate,
+  triangleCheck,
+};
