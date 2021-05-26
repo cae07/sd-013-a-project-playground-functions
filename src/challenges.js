75ad46ffe49ef05e499cdbb7088c766f6f47a7ae
LuizFJP <luizfjportela@gmail.com>
2021-05-26 09:24:11 -0300

    add challenges 1 and 2

--- src/challenges.js
+++ src/challenges.js
@@ -1,11 +1,16 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(val1, val2) {
+  if(val1 == true && val2 == true){
+    return true
+  }else{
+    return false
+  }
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  let area = (base * height)/2;
+  return area
 }
 
 // Desafio 3
