df3323174036992c4ddbee477c934fa0ab497be6
fgosuen <fgosuendacosta@gmail.com>
2021-05-25 21:22:28 -0300

    Corrigindo erros de Lint

--- src/challenges2.js
+++ src/challenges2.js
@@ -25,24 +25,24 @@ function generatePhoneNumber(arrayDeNumeros) {
   let repetiu = 0;
   const erroTelefone01 = 'não é possível gerar um número de telefone com esses valores';
   const erroTelefone02 = 'Array com tamanho incorreto.';
-  if (arrayDeNumeros.length == '') {
+  if (arrayDeNumeros.length === '') {
     numeroTelefone = erroTelefone02;
   } else {
     for (let indexD111 = 0; indexD111 < arrayDeNumeros.length; indexD111 += 1) {
       repetiu = analiseRepeticao(arrayDeNumeros, indexD111, repetiu);
       if (repetiu >= 3) {
-        numeroTelefone = erroTelefone01
+        numeroTelefone = erroTelefone01;
       }
       if (arrayDeNumeros.length === 11) {
         for (let indexD113 = 0; indexD113 < arrayDeNumeros.length; indexD113 += 1) {
           if ((arrayDeNumeros[indexD113] < 0) || (arrayDeNumeros[indexD113] > 9)
           || (repetiu >= 3)) {
-            numeroTelefone = erroTelefone01
+            numeroTelefone = erroTelefone01;
           }
         }
       } else if (arrayDeNumeros.length !== 11) { numeroTelefone = erroTelefone02; }
       repetiu = 0;
-      stringDoArray = stringDoArray + arrayDeNumeros[indexD111];
+      stringDoArray += arrayDeNumeros[indexD111];
     }
     if ((numeroTelefone !== erroTelefone01) && (numeroTelefone !== erroTelefone02)) {
       numeroTelefone = '(' + stringDoArray.slice(0, 2) + ') ' + stringDoArray.slice(2, 7)
@@ -53,21 +53,19 @@ function generatePhoneNumber(arrayDeNumeros) {
 }
 function analiseRepeticao(arrayDeNumeros, indexD111, repetiunumeros) {
   for (let indexD112 = 0; indexD112 < arrayDeNumeros.length; indexD112 += 1) {
-    console.log(arrayDeNumeros[indexD111] + " com " + arrayDeNumeros[indexD112])
-    if (arrayDeNumeros[indexD111] == arrayDeNumeros[indexD112]) {
+    if (arrayDeNumeros[indexD111] === arrayDeNumeros[indexD112]) {
       repetiunumeros += 1;
     }
   }
   return repetiunumeros;
 }
 
-
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
   let ehtriangulo = false;
   if (((lineA < (lineB + lineC)) && (lineA > (lineB - lineC))) && ((lineB < (lineC + lineA))
    && (lineB > (lineC - lineA))) && ((lineC < (lineA + lineB)) && (lineC > (lineA - lineB)))) {
-        ehtriangulo = true;
+    ehtriangulo = true;
   }
   return ehtriangulo;
 }
