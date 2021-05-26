255a09ab2dd45202a928f977e7dc8981ff617722
Patrik Batista <pcr_batista@homail.com>
2021-05-26 12:53:35 -0300

    finalizaçao dos exercícios 1 2 3 5 6 e 7 do projeto

--- src/challenges.js
+++ src/challenges.js
@@ -1,17 +1,19 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
-  
+function compareTrue(value1,value2) {
+  if (value1 == true && value2 == true) {
+    return true;
+  }else{
+    return false;
+  }
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
-}
+function calcArea(base,height) {
+  return(base * height)/2; }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(value) {
+  return value.split(' ');
 }
 
 // Desafio 4
@@ -20,18 +22,40 @@ function concatName() {
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  return (wins * 3) + (ties * 1);
 }
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+function highestCount(value) {
+  let biggerNumber = value[0];
+  let cont = 0;
+
+  for (let i = 0; i < value.length; i++) {
+    if (biggerNumber < value[i]) {
+      biggerNumber = value[i];
+      cont = 1;
+    } else if (value[i] === biggerNumber) {
+      cont = cont + 1;
+    }
+  }
+  return cont;
+
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  let dist1 = Math.abs(cat1 - mouse);
+  let dist2 = Math.abs(cat2 - mouse);
+  let result;
+
+  if(dist1 < dist2) {
+    return 'cat1';
+  }else if(dist1 > dist2){
+    return 'cat2';
+  }else{
+    return 'os gatos trombam e o rato foge';
+  }
 }
 
 // Desafio 8
