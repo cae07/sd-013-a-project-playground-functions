ca5865cf9306c36c09cf9db93ffd464684032bb4
IgorFellows <igor.fpp96@gmail.com>
2021-05-25 22:52:39 -0300

    Adiciona a funçao 7 e corrige alguns erros do lint

--- src/challenges.js
+++ src/challenges.js
@@ -6,66 +6,53 @@ function compareTrue(valor1, valor2) {
     return false;
   }
 }
-
 // Desafio 2
 function calcArea(base, height) {
-  return (base*height)/2;
+  return (base * height) / 2;
 }
-
 // Desafio 3
 function splitSentence(goTrybe) {
-  return goTrybe.split(' ')
-  
+  return goTrybe.split(' '); 
 }
-
 // Desafio 4
 function concatName(stringArrays) {
   return `${stringArrays[stringArrays.length-1]}, ${stringArrays[0]}`
 }
-
-
 // Desafio 5
 function footballPoints(wins, ties) {
-  return (wins*3) + ties
+  return (wins * 3) + ties
 }
-
 // Desafio 6
 function highestCount(numberArray) {
-  let highestNumber = numberArray [0];
+  let highestNumber = numberArray[0];
   let contador = 0;
 for(let index = 0; index < numberArray.length; index +=1) {
   if(numberArray[index] > highestNumber) {
     highestNumber = numberArray[index];
     contador = 1;
-
-  } else if(numberArray[index] === maiorNumber) {
+  } else if (numberArray[index] === highestNumber) {
     contador += 1;
-  }
-  
+  }  
 }
 return contador
-
 }
-
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
   let gato1 = Math.abs(cat1 - mouse);
   let gato2 = Math.abs(cat2 - mouse);
-
   if (gato1 > gato2) {
-    return 'cat2'
+    return 'cat2';
   } else if (gato2 > gato1) {
-    return 'cat1'
+    return 'cat1';
   } else {
     return 'os gatos trombam e o rato foge'
   }
 }
-
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(numeroArray) {
+  let saidaArray = [];
+   
 }
-
 // Desafio 9
 function encode() {
   // seu código aqui
