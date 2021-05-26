f24c621d1cbd25042a85a72e73cc70e1b19ca842
Pedro Trasfereti <pedrotrasfereti@gmail.com>
2021-05-25 21:43:12 -0300

    resolve desafios 2-7

--- src/challenges.js
+++ src/challenges.js
@@ -9,33 +9,98 @@ function compareTrue(boolean1, boolean2) {
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  area = (base * height) / 2;
+  return area;
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(string) {
+  let array = string.split(" ");
+  return array;
 }
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(array) {
+  let result = array[array.length - 1] + ', ' + array[0];
+  return result;
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  let points = (wins * 3) + (ties);
+  return points;
 }
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+function highestCount(numbers) {
+  let highestNumber = Math.max(...numbers);
+  let highestRepeat = 0;
+
+  for (let number of numbers) {
+    if (number == highestNumber) {
+      highestRepeat += 1;
+    }
+  }
+  return highestRepeat;
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  let distCat1 = 0;
+  let distCat2 = 0;
+  if (mouse > cat1 && mouse > cat2) {
+    distCat1 = mouse - cat1;
+    distCat2 = mouse - cat2;
+    if (distCat1 < distCat2) {
+      return "cat1";
+    }
+    else if (distCat1 > distCat2) {
+      return "cat2";
+    }
+    else if (distCat1 == distCat2) {
+      return "os gatos trombam e o rato foge";
+    }
+  }
+  else if (mouse < cat1 && mouse < cat2) {
+    distCat1 = cat1 - mouse;
+    distCat2 = cat2 - mouse;
+    if (distCat1 < distCat2) {
+      return "cat1";
+    }
+    else if (distCat1 > distCat2) {
+      return "cat2";
+    }
+    else if (distCat1 == distCat2) {
+      return "os gatos trombam e o rato foge";
+    }
+  }
+  else if (mouse > cat1 && mouse < cat2) {
+    distCat1 = mouse - cat1;
+    distCat2 = cat2 - mouse;
+    if (distCat1 < distCat2) {
+      return "cat1";
+    }
+    else if (distCat1 > distCat2) {
+      return "cat2";
+    }
+    else if (distCat1 == distCat2) {
+      return "os gatos trombam e o rato foge";
+    }
+  }
+  else if (mouse < cat1 && mouse > cat2) {
+    distCat1 = cat1 - mouse;
+    distCat2 = mouse - cat2;
+    if (distCat1 < distCat2) {
+      return "cat1";
+    }
+    else if (distCat1 > distCat2) {
+      return "cat2";
+    }
+    else if (distCat1 == distCat2) {
+      return "os gatos trombam e o rato foge";
+    }
+  }
 }
 
 // Desafio 8
