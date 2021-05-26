c853e8d697b2075942c8ea07dd2cf6a075765835
Cássio Pereira <cassiorodp@gmail.com>
2021-05-26 11:08:36 -0300

    Desafio 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,23 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(techNames, name) {
+  let techObject = {};
+  let techArray = [];
+  if (techNames[0] === undefined) {
+    return 'Vazio!';
+  }
+  for (let index of techNames) {
+    techObject['tech'] = index;
+    techObject['name'] = name;
+    techArray.push(techObject);
+    techObject = {};
+  }
+  return techArray.sort(function (a, b) {
+    if (a.tech.toLowerCase() < b.tech.toLowerCase()
+    ) return -1;
+    if (a.tech.toLowerCase() > b.tech.toLowerCase()
+    ) return 1;
+    return 0;
+  });
 }
 
 // Desafio 11
