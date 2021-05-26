8ec72c39dc5b66fb80eab3f4e901f529331bea58
Pedro Delicoli <pedrodelicoli@hotmail.com>
2021-05-25 21:25:13 -0300

    Requisito 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,12 +1,70 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(tech, nome) {
+
+  let techlist1 = {
+    tech1: tech[0],
+    name: nome,  
+  };
+  let techlist2 = {
+    tech2: tech[1],
+    name: nome,
+  };
+  let techlist3 = {
+    tech2: tech[2],
+    name: nome,
+  };
+  let techlist4 = {
+    tech2: tech[3],
+    name: nome,
+  };
+  let techlist5 = {
+    tech2: tech[4],
+    name: nome,
+  };
+  return [techlist1, techlist2, techlist3, techlist3, techlist5];
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(numeros) {
+  if (numeros.length != 11) {
+    return 'Array com tamanho incorreto';
+  }
+  for (let index in numeros) {
+    if (numeros[index] < 0 || numeros[index] > 9){
+      return 'não é possivel gerar um número de telefone com esses valores';
+    }
+  }
+  let contNumero = 0;
+  let numeroRepetido = 0;
+  for (let index2 in numeros) {
+    numeroRepetido = numeros[index2];
+  
+    for (let index3 = 0; index3 < numeros.length; index3 += 1) {
+      if (numeros[index3] === numeroRepetido) {
+        contNumero += 1;
+      }
+    }
+    if (contNumero >= 3) {
+      return 'não é possivel gerar um número de telefone com esses valores';
+    }  
+    let mask = '(##)#####-####'
+    let novoNum = '';
+    let posicao = 0;
+    for (let i = 0; i < mask.length; i+= 1) {
+      if(mask[i] == '#') {
+         novoNum += numeros[posicao];
+         posicao += 1;
+      }
+      else {
+        {
+          novoNum += mask[i]
+        }
+      }
+    }
+    return novoNum;
+  }
 }
+
 
 // Desafio 12
 function triangleCheck() {
