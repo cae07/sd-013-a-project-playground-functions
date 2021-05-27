99b75a0cc0f14e1833046736e034784d23ea9245
Ricardo-Sousa <rwmsousa@gmail.com>
2021-05-27 10:44:37 -0300

    Resolução do desafio 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,29 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+
+// função comparar para ordenar array
+function comparar(a, b) {
+  if(a.tech < b.tech) {
+    return -1;
+  } else {
+    return true;
+}
+}
+
+function techList(array, name) {
+  let objectList = [];
+
+    for (let index = 0; index < array.length; index++) {
+      objectList.push({
+      tech: array[index],
+      name: name,
+      }) 
+    }
+    // return objectList.sort(comparar);} //ordena os itens do array
+  
+    if(objectList.length !== 0){
+    return objectList.sort(comparar);
+    } else if (objectList.length === 0){
+    return 'Vazio!'}
 }
 
 // Desafio 11
