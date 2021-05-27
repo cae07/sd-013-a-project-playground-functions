6ba80dffbb57b23e6c7e22dab3f9775b25d23cdf
Matheus Duarte de Freitas <freickup@gmail.com>
2021-05-27 11:11:14 -0300

    identacao do codigo com Preetier

--- src/challenges2.js
+++ src/challenges2.js
@@ -16,7 +16,8 @@ function techList(techsList, nome) {
 
 // Desafio 11
 
-let mensagemImpossivel = 'não é possível gerar um número de telefone com esses valores';
+let mensagemImpossivel =
+  'não é possível gerar um número de telefone com esses valores';
 let mensagemTamanho = 'Array com tamanho incorreto.';
 
 function checaTamanho(phoneNumbersArray) {
@@ -33,27 +34,32 @@ function checaNumeros(phoneNumbersArray) {
   }
 }
 
-function contaRepeticao (phoneNumbersArray, numeroDoPhoneNumbersArray) {
+function contaRepeticao(phoneNumbersArray, numeroDoPhoneNumbersArray) {
+  //Criei esta função depois de um amigo me explicar que 2 for dentro de uma função, aumentam a complexidade e em 1 nível,
+  //Ela é chamada pela função logo abaixo, explicação na função de baixo
   let repetiu = 0;
- 
+
   for (let comparaRepetido in phoneNumbersArray) {
-    if (numeroDoPhoneNumbersArray === phoneNumbersArray[comparaRepetido]) {
-        repetiu += 1;
-        }
-      }
-      return repetiu;
+    if (honeNumbersArray[comparaRepetido] === numeroDoPhoneNumbersArray) {
+      repetiu += 1;
+    }
+  }
+  return repetiu;
 }
 
-function verificaRepeticao (phoneNumbersArray) {
+function verificaRepeticao(phoneNumbersArray) {
   for (let index in phoneNumbersArray) {
+    //A cada vez que este for roda, ele chama a função de cima com o array do telefone (que o robô insere) e o index deste array como parametros
+    //Como a função contaRepeticao retorna o número de vezes que o phoneNumbersArray[index] repetiu no phoneNumbersArray, entao ''repete'' = a cada iteracao da contaRepeticao
+    //Se em alguma iteracao da funcao conta Repeticao, a variavel repetiu for maior ou igual a 3, então repete também será maior ou igual a 3 retornando verdadeiro para a verificacao
     let repete = contaRepeticao(phoneNumbersArray, phoneNumbersArray[index]);
-    if (repete >=3) {
+    if (repete >= 3) {
       return true;
     }
   }
 }
 
-function mountPhone (phoneNumbersArray) {
+function mountPhone(phoneNumbersArray) {
   phoneNumberString = phoneNumbersArray.join('');
   let ddd = phoneNumberString.slice(0, 2);
   let prefixo = phoneNumberString.slice(2, 7);
@@ -62,21 +68,24 @@ function mountPhone (phoneNumbersArray) {
 }
 
 function generatePhoneNumber(phoneNumbersArray) {
- if (checaTamanho(phoneNumbersArray)) {
-  return mensagemTamanho;
- } if (checaNumeros(phoneNumbersArray)) {
-  return mensagemImpossivel;
- } if (verificaRepeticao(phoneNumbersArray)) {
-  return mensagemImpossivel;
- } 
+  if (checaTamanho(phoneNumbersArray)) {
+    return mensagemTamanho;
+  }
+  if (checaNumeros(phoneNumbersArray)) {
+    return mensagemImpossivel;
+  }
+  if (verificaRepeticao(phoneNumbersArray)) {
+    return mensagemImpossivel;
+  }
   return mountPhone(phoneNumbersArray);
 }
 
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
   if (
-    lineA > Math.abs(lineB - lineC)
-    && lineB > Math.abs(lineA - lineC) && lineC > Math.abs(lineA - lineB)
+    lineA > Math.abs(lineB - lineC) &&
+    lineB > Math.abs(lineA - lineC) &&
+    lineC > Math.abs(lineA - lineB)
   ) {
     return true;
   }
