1a7e2e622f8959e2a85d6db38481c923650afd7e
gr3g1987@gmail.com <gabriel.gregorio1987@gmail.com>
2021-05-27 09:59:11 -0300

    Desafio 4

--- src/challenges.js
+++ src/challenges.js
@@ -28,11 +28,17 @@ function splitSentence(digite) {
 
 console.log(splitSentence("Vamo que vamo go Trybe"));
 
-// Desafio 
-
-function concatName() {
-
+// Desafio 4
+function concatName(...nomes) {
+  let first = nomes[0];
+  let last = nomes[nomes.length - 1]
+  let join = last + ", "+ first;
+  return join;
 }
+
+console.log(concatName('Lucas', 'Cassiano', 'Ferraz', 'Paolillo'));
+console.log(concatName('foguete', 'não', 'tem', 'ré'));
+console.log(concatName('captain', 'my', 'captain','captain', 'my', 'captain'));
 
 function footballPoints(wins,ties) {
   vitoria = wins * 3;
@@ -45,8 +51,9 @@ console.log(footballPoints(0,0));
 
 // Desafio 6
 function highestCount() {
-  // seu código aqui
+  
 }
+
 
 // Desafio 7
 function catAndMouse() {
