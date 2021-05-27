92aaff8a5a4b41b233c08d056a80a1106f9015ff
gr3g1987@gmail.com <gabriel.gregorio1987@gmail.com>
2021-05-27 06:31:17 -0300

    Desafio 2

--- src/challenges.js
+++ src/challenges.js
@@ -8,12 +8,17 @@ function compareTrue(x,y) {
   
 }
 
-console.log(compareTrue(true,false))
+console.log(compareTrue(true,false));
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base,heigh) {
+  let area = (base * heigh) / 2;
+  return area;
 }
+
+console.log(calcArea(10,50));
+console.log(calcArea(5,2));
+console.log(calcArea(51,1));
 
 // Desafio 3
 function splitSentence() {
