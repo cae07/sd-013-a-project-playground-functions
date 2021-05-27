46b2886add2f58b2f0d843e1c28af1ca09d83567
Jackson Santana <onjacksm@gmail.com>
2021-05-27 16:46:19 -0300

    Desafios 1 e 2

--- src/challenges.js
+++ src/challenges.js
@@ -1,12 +1,16 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
-  // Commit inicial
+function compareTrue(value1, value2) {
+  if (value1 === true && value2 === true) {
+    return true;
+  } {
+    return false;
+  }
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  let area = (base * height) / 2;
+  return area;
 }
 
 // Desafio 3
