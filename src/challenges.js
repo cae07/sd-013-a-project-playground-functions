72b10696980a18c2495e06d24c22f6ce3b3ff003
Sethano <vitorcard1720@gmail.com>
2021-05-27 19:57:42 -0300

    terceira parte

--- src/challenges.js
+++ src/challenges.js
@@ -21,18 +21,35 @@ function splitSentence(inputToSplit) {
 // Desafio 4
 function concatName(inputConcat) {
   let concated = [inputConcat.slice(-1)] + [', '] + [inputConcat.slice(0, 1)];
+
   return concated;
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  let points = (wins * 3) + ties;
+
+  return points;
 }
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+function highestCount(numberArray) {
+  let hNum = numberArray[0];
+
+  let count = 0;
+
+  for (let i = 0; i < numberArray.length; i += 1) {
+    if (hNum < numberArray[i]) {
+      hNum = numberArray[i];
+      count = 0;
+    }
+    if (hNum === numberArray[i]) {
+      count += 1;
+    }
+  }
+  return count;
 }
+console.log(highestCount([0, 5, 4, 3, 2, 1]));
 
 // Desafio 7
 function catAndMouse() {
