fe829eaca98db19d78fd2d0fa836827f4b8e03fb
Lucassauro <lucassauro.lt@gmail.com>
2021-05-27 14:51:23 -0300

    Adiciona exercicios 4, 6 e 7 completos. Incompletos 8, 9; e todos os exercicios bonus.

--- src/challenges.js
+++ src/challenges.js
@@ -18,11 +18,9 @@ function splitSentence(str) {
   return str.split(" ");
 }
 
-// Desafio 4 A TERMINAR
+// Desafio 4
 function concatName(array) {
-  for (i=0;i<=array.length; i++) {
-    
-  }
+  return palavra = array[array.length -1] + ", " + array[0];
 }
 
 // Desafio 5
@@ -34,14 +32,53 @@ function footballPoints(wins, ties) {
 }
 
 // Desafio 6
-highestCount();
 function highestCount(array) {
-
+  let mN = array[0];
+  let counter = 0;
+  for(i = 0; i < array.length; i++) {
+    if (mN < array[i]) {
+      mN = array[i];
+    }
+  }
+  for (i = 0; i < array.length; i++) {
+    if (mN === array[i]) {
+      counter++;
+    }
+  }
+  return counter;
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  var distanciaCat1 = (cat1 - mouse);
+  var distanciaCat2 = (cat2 - mouse);
+  console.log("dcat1:", distanciaCat1)
+  console.log("dcat2:", distanciaCat2)
+  var string = "";
+  if (distanciaCat1 < 0) {
+    distanciaCat1 = (distanciaCat1 * -1);
+    console.log("dcat1 que era negativo, agora é:", distanciaCat1)
+  } else if (distanciaCat2 < 0) {
+    distanciaCat2 = (distanciaCat2 * -1)
+    console.log("dcat2 que era negativo, agora é:", distanciaCat2)
+  }
+
+  if (distanciaCat1 < distanciaCat2) {
+    console.log("dcat1:", distanciaCat1)
+    console.log("dcat2:", distanciaCat2)
+    console.log("cat 1 pegou")
+    return string = "cat1";
+  } else if (distanciaCat1 > distanciaCat2) {
+    console.log("dcat1:", distanciaCat1)
+    console.log("dcat2:", distanciaCat2)
+    console.log("cat 2 pegou")
+    return string = "cat2";
+  } else {
+    console.log("dcat1:", distanciaCat1)
+    console.log("dcat2:", distanciaCat2)
+    console.log("os gatos trombam e o rato foge")
+    return string = "os gatos trombam e o rato foge";
+  }
 }
 
 // Desafio 8
