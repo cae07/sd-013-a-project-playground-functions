b568604e639c56ea5b374d02380dd153c7b3e33d
Wesley de Melo Pedroso <wesleyxd@hotmail.com>
2021-05-27 03:18:20 -0300

    13 - Crie uma função de Bem vindo ao Bar da Trybe

--- src/challenges2.js
+++ src/challenges2.js
@@ -34,8 +34,25 @@ function triangleCheck(lineA, lineB, lineC) {
 }
 
 // Desafio 13
-function hydrate() {
-  // seu código aqui
+function hydrate(frase) {
+  let numerosString = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  let contaAgua = 0;
+
+  for (let index = 0; index < frase.length; index += 1) {
+    for (let index2 = 0; index2 < frase.length; index2 += 1) {
+      if (frase[index] === numerosString[index2]) {
+        contaAgua += numeros[index2];
+      }
+    }
+  }
+
+  if (contaAgua > 1) {
+    return contaAgua + " copos de água";
+  } else {
+    return contaAgua + " copo de água";
+  }
+
 }
 
 module.exports = {
