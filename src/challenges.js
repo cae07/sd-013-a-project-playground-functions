6aa9ea9c70699add9eb10e716822bdef1d8cbfbb
gr3g1987@gmail.com <gabriel.gregorio1987@gmail.com>
2021-05-27 19:17:18 -0300

    Desafio 7

--- src/challenges.js
+++ src/challenges.js
@@ -68,11 +68,11 @@ function catAndMouse(mouse,cat1,cat2) {
 
   if( (cat1 - mouse) > (cat2 - mouse)){
     return "cat2";
+  }  else if ( (mouse - cat1) == (cat2 - mouse) ){
+      return "os gatos trombam e o rato foge";
   } else  if ( (cat1 - mouse) < (cat2 - mouse)) {
     return "cat1";
-  } else if ( (mouse - cat1) == (cat2 - mouse) ){
-    return "os gatos trombam e o rato foge"
-  }
+  } 
   
 }
 console.log(catAndMouse(0, 3, 2));
