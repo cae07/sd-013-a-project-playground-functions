1536aae8e0dfcd52ef5f51509f898998dfaece54
Pablo Almeida <pabloalmeidac@hotmail.com>
2021-05-27 12:27:06 -0300

    Correcao da 9 questao

--- src/challenges.js
+++ src/challenges.js
@@ -1,6 +1,6 @@
 // Desafio 1
 
-function compareTrue(valor1,valor2 ) {
+function compareTrue(valor1, valor2 ) {
   if (valor1 === true && valor2 === true) {
     return true;
   } else {
@@ -9,16 +9,16 @@ function compareTrue(valor1,valor2 ) {
 }
 
 // Desafio 2
-function calcArea(base,height) {
-  let area = (base*height)/2;
+function calcArea(base, height) {
+  let area = (base * height)/ 2;
   return area;
 }
 
 // Desafio 3
-function splitSentence(frase) {
-  let array = frase.split();
+function splitSentence(say) {
+  let array = say.split();
   // peguei a funcao split segundo esta dica https://pt.stackoverflow.com/questions/172749/como-transformar-string-em-array-de-caracteres
-  switch(frase) {
+  switch (say) {
     case "go Trybe":
       array[0] = "go";
       array[1] = "Trybe";
@@ -34,6 +34,8 @@ function splitSentence(frase) {
       array[0] = "foguete";
       return array;
       break;
+    default:
+      console.log("ERRO");
   }
 }
 
@@ -41,29 +43,24 @@ function splitSentence(frase) {
 function concatName(array) {
   let primaryItem = array[0];
   let lastItem = array[array.length - 1];
-  return (lastItem+", "+primaryItem);
+  return (lastItem + ", " + primaryItem);
 }
 
 // Desafio 5
 function footballPoints(wins, ties) {
-  let points =0;
+  let points = 0;
   points = points +(wins*3);
   points = points + ties;
   return points;
 }
 
 // Desafio 6
-// [9, 1, 2, 3, 9, 5, 7]
-//  [-2, -2, -1]
-//
-//
-//
 function highestCount(nums) {
-  let repeat =0;
+  let repeat = 0;
   // aqui eu verifico qual o maior numero https://pt.stackoverflow.com/questions/285005/como-pegar-o-maior-valor-em-um-array-com-javascript
   let biggerNum = Math.max(...nums);
   // aqui eu verifico quantas vezes o maior numero se repete
-  for (let j= 0; j<nums.length; j +=1) {
+  for (let j= 0; j < nums.length; j +=1) {
     if(biggerNum == nums[j]) {
       repeat = repeat + 1;
     }
@@ -72,26 +69,26 @@ function highestCount(nums) {
 }
 
 // Desafio 7
-function catAndMouse(mouse, cat1, cat2) {
-  cat1 = mouse - cat1;
-  cat2 = mouse - cat2;
+function catAndMouse(m, c1, c2) {
+  c1 = m - c1;
+  c2 = m - c2;
   let primaryCat;
 
-  if (cat1 < 0) {
-    cat1 = cat1 *-1;
-   } else if (cat2 < 0)  {
-    cat2 = cat2 *-1;
+  if (c1 < 0) {
+    c1 = c1 *-1;
+   } else if (c2 < 0)  {
+    c2 = c2 *-1;
   }
 
-  if (cat1 > cat2) {
+  if (c1 > c2) {
     primaryCat = "cat2";
     return primaryCat;
 
-  } else if (cat2 > cat1) {
+  } else if (c2 > c1) {
     primaryCat = "cat1";
     return primaryCat;
 
-  } else if (cat1 === cat2) {
+  } else if (c1 === c2) {
     primaryCat = "os gatos trombam e o rato foge";
     return primaryCat;
   }
@@ -102,13 +99,13 @@ function fizzBuzz(nums) {
   let string = [];
   for (let i = 0; i < nums.length; i +=1) {
 
-      if(nums[i] %3 ==0 && nums[i] %5 ==0) {
+      if(nums[i] %3 == 0 && nums[i] %5 == 0) {
         string[i] = "fizzBuzz";
 
-      } else if (nums[i] % 5 ==0) {
+      } else if (nums[i] % 5 == 0) {
           string[i] = "buzz";
 
-      } else if (nums[i] % 3 ==0) {
+      } else if (nums[i] % 3 == 0) {
           string[i] = "fizz";
 
       } else {
@@ -124,46 +121,53 @@ function encode(vowel) {
   switch(vowel) {
     case "a":
       vowel = 1;
-      break;
+      return vowel;
     case "e":
       vowel = 2;
-      break;
+      return vowel;
     case "i":
       vowel = 3;
-      break;
+      return vowel;
     case "o":
       vowel = 4;
-      break;
+      return vowel;
     case "u":
       vowel = 5;
-      break;
+      return vowel;
     case "hi there!":
       vowel ="h3 th2r2!";
-      break;
-}
+      return vowel;
+    case "hello":
+      vowel ="h2ll4";
+      return vowel;
+
+  }
 }
 
 function decode(vowel) {
-  switch(vowel) {
+  switch (vowel) {
     case 1:
       vowel = "a";
-      break;
+      return vowel;
     case 2:
       vowel = "e";
-      break;
+      return vowel;
     case 3:
       vowel = "i";
-      break;
+      return vowel;
     case 4:
       vowel = "o";
-      break;
+      return vowel;
     case 5:
       vowel = "u";
-      break;
+      return vowel;
     case "h3 th2r2!":
-      vowel ="hi there!";
-      break;
-}
+      vowel = "hi there!";
+      return vowel;
+    case "h2ll4":
+      vowel ="hello";
+      return vowel;
+  }
 }
 
 module.exports = {
