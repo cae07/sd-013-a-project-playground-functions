0fd4436513394efe7d1dbc215eb62aa8f3966479
rene-t13-trybe <cleytonacompanha@gmail.com>
2021-05-27 21:38:15 -0300

    Desafio 5 Projeto Playground Functions

--- src/challenges.js
+++ src/challenges.js
@@ -26,8 +26,9 @@ function concatName(nomes) {
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  let points = (wins * 3) + ties;
+  return points;
 }
 
 // Desafio 6
