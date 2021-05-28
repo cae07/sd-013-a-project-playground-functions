9484647763a4b8e929e24bbc2f44ff4a888c8e5c
Wallacy Francis <wallacy_francis@hotmail.com>
2021-05-28 12:11:05 -0300

    Desafio 11 concluido, generatePhoneNumber realizado

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,23 +1,52 @@
 // Desafio 10
+/* 
+  Dúvida! Retorne uma lista de objetos ordenados quando é passada uma lista com 5 tecnologias
+*/
 function techList(tech, name) {
   let person = {};
-  let arrTech = tech.split(',');
 
-  for (let i = 0 ; i < arrTech.length ; i++) {
-    person['tech'] = arrTech[i];
-    person['name'] = name;
-    console.log(person)
+  if (tech.length === 0) {
+    person = 'Vazio!';
+  } else {
+    for (let i = 0 ; i < tech.length ; i++) {
+      person['tech'] = tech[i]
+      person['name'] = name;
+      console.log(person);
+    }
   }
-  
-  return 
+ 
+  return person;
 }
 
-
-// Desafio 11
+// Desafio 11 - ok
 function generatePhoneNumber(num) {
-  console.log(num);
+  let numTel;
+  if (num.length != 11) {
+    numTel = 'Array com tamanho incorreto.';
+  } else {
+    for (let i = 0 ; i < num.length ; i++ ) {
+      if ( (num[i] > 9 ) || (num[i] < 0) ) {
+        numTel = 'não é possível gerar um número de telefone com esses valores';
+        break;
+      } else {
+        let numAtual = num[i];
+        let repeatNum = 0;
+        for (let j = 0 ; j < num.length ; j++) {
+          if (numAtual == num[j]) {
+            repeatNum += 1;
+          }
+        }
+        if(repeatNum >= 3) {
+          numTel = 'não é possível gerar um número de telefone com esses valores';
+          break;
+        } else {
+          numTel = '('+num[0]+num[1]+')'+' '+num[2]+num[3]+num[4]+num[5]+num[6]+'-'+num[7]+num[8]+num[9]+num[10];
+        }
+      }
+    }  
+  }  
+  return numTel
 }
-generatePhoneNumber(1,2,3,4,5,6,);
 
 // Desafio 12
 function triangleCheck() {
