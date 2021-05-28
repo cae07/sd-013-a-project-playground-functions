32fb687a73798252f383bb6400a4cd693c9d4eaf
Breno Lopes <brenokos4@gmail.com>
2021-05-28 10:09:18 -0300

    teste de bug

--- src/challenges2.js
+++ src/challenges2.js
@@ -2,20 +2,23 @@
 function techList(techArray = Array, nome) {
   let itens = [];
   // eslint-disable-next-line guard-for-in
-  if (techArray.length === 0) {
+  if (techArray.length <= 3) {
     return 'vazio!';
   }
   // eslint-disable-next-line guard-for-in
-  for (let i in techArray) {
-    let tech = { name: nome,
-      tech: techArray[i] };
-    itens.push(tech);
+  if (techArray.length >= 5) {
+    // eslint-disable-next-line guard-for-in
+    for (let i in techArray) {
+      let tech = { name: nome,
+        tech: techArray[i] };
+      itens.push(tech);
+    }
+    // else if (techArray === 0) {
+    // return 'Vazio!';
+    return itens;
   }
-  // else if (techArray === 0) {
-  // return 'Vazio!';
-  return itens;
 }
-console.log(techList([]));
+console.log(techList(['java', 'python'], 'breno'));
 // Desafio 11
 function generatePhoneNumber() {
   
