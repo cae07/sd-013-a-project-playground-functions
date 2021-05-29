db16c0c6e6444d9e63d38c9753abb56303544ab7
Danilo Uehara <daniloyuno@hotmail.com>
2021-05-29 20:16:56 -0300

    add alguns itens

--- src/challenges.js
+++ src/challenges.js
@@ -77,22 +77,59 @@ console.log(highestCount(array));
 
 
 // Desafio 7
-function catAndMouse() {
+function catAndMouse(cat1,cat2) {
+  if (cat1 > cat2){
+    return console.log("cat2");
+  }
+  else if (cat1 == cat2){
+    return console.log("os gatos trombam e o rato foge");
+  }
+  else {
+    return console.log("cat1");
+  }
+}
 
+
+// Desafio 8
+function fizzBuzz(num) {
+  let string=[];
+  for (let index = 0; index < num.length; index++) {
+    if (num%3 == 0){
+      string[index]= "fizz";
+    }
+    else if(num%5 ==0){
+      string[index] == "Buzz";
+    }
+    else if (num%5==0 && num%3 == 0){
+      string[index] == "fizzBuzz";
+    }
+    else{
+      string[index] == "bug!";
+    }    
+  }
+  return string;
   
 }
 
-// Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+// Desafio 9
+function encode(string) {
+  let novaString;    
+  string = string.replace(/a/g, "1");
+  string = string.replace(/e/g, "2");
+  string = string.replace(/i/g, "3");
+  string = string.replace(/o/g, "4");
+  string = string.replace(/u/g, "5");    
+  return string;
 }
 
-// Desafio 9
-function encode() {
-  // seu código aqui
-}
 function decode() {
-  // seu código aqui
+  let novaString;    
+  string = string.replace(/1/g, "a");
+  string = string.replace(/2/g, "e");
+  string = string.replace(/3/g, "i");
+  string = string.replace(/4/g, "o");
+  string = string.replace(/5/g, "u");    
+  return string;
 }
 
 module.exports = {
--- src/challenges2.js
+++ src/challenges2.js
@@ -1,12 +1,76 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(list=[],name) {
+  let newLis=[];
+  let vazio="Vazio!";
+  if (list.length==0){
+    return vazio;
+  }
+  else{
+    for (let index = 0; index < list.length; index+=1) {
+      let objetic = {
+        tec : list[index],
+        nome : name,
+      }
+    for (let a in newLis){
+
+    }
+      newLis[index]= objetic;    
+    }
+    
+    return newLis;
+  }
+        
 }
 
+
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(tel=[]) {
+  let msg="";
+  let aux=[];  
+  
+  if (tel.length != 11){
+    msg="Array com tamanho incorreto.";
+    return msg;
+  }
+  else{
+    for (let index1 = 0; index1 < 10; index1+=1) {
+      aux[index1]= index1;
+      let cont = 0;
+      for (let index = 0; index < tel.length; index+=1) {
+        if (tel[index] < 0 || tel[index] >9){
+          msg = "não é possível gerar um número de telefone com esses valores";
+          return msg;
+          break;
+        }
+        if (aux[index1] == tel[index]){
+          cont+=1;   
+        }
+        if (cont == 3){
+          msg = "não é possível gerar um número de telefone com esses valores";
+          return msg;
+          break;
+        }          
+      }     
+    }  
+    for (let index = 0; index < tel.length; index+=1) {
+      if (index == 0){
+      msg=msg + "(" + tel[index] ;
+      }
+      else if (index == 1){
+        msg=msg + tel[index] + ")" + " ";
+      }
+      else if(index == 7){
+        msg=msg + "-" + tel[index];
+      }
+      else{
+        msg=msg + tel[index];
+      }           
+    }     
+    return msg;   
+  }   
 }
+
+
 
 // Desafio 12
 function triangleCheck() {
