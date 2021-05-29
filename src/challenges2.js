917f96a18d27efd37f924bc1fde2ccd6cd12551f
Caê <eduardokae01@hotmail.com>
2021-05-29 16:01:13 -0300

    arrumando lind

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,5 +1,5 @@
 // Desafio 10
-// Ajudado por
+// Ajudado por Ricardo Souza turma 13 A
 function techList(tech, name) {
   let menor = tech.sort();
   let array = [];
@@ -12,27 +12,28 @@ function techList(tech, name) {
   if (array.length === 0) {
     return 'Vazio!';
   }
-    return array;
+  return array;
 }
 
-// Desafio 11 - Ajudado pela Bianca no plantão da manhã
+// Desafio 11 - Ajudado pela Bianca no plantão da manhã e pelo sumo no plantão da noite
 function generatePhoneNumber(numeros) {
   let contagem = 0;
-  //verifica se o array é igual 11
+  //  verifica se o array é igual 11
   if (numeros.length !== 11) {
-    return 'Array com tamanho incorreto.'
+    return 'Array com tamanho incorreto.';
   }
 
   // verifica se existe numeros < 0 ou > 9
   for (index = 0; index < numeros.length; index += 1) {
     if (numeros[index] < 0 || numeros[index] > 9) {
-      return 'não é possível gerar um número de telefone com esses valores'
+      return 'não é possível gerar um número de telefone com esses valores';
     }
   }
 
+  // Verifica se existem mais de 3 numeros repetidos
   for (index = 0; index < numeros.length; index += 1) {
     for (counter = 0; counter < numeros.length; counter += 1) {
-      if(numeros[index] === numeros[counter]) {
+      if (numeros[index] === numeros[counter]) {
         contagem += 1;
       }
       if (contagem > 2) {
@@ -42,15 +43,19 @@ function generatePhoneNumber(numeros) {
     contagem = 0;
   }
 
-  for (index = 0; index < numeros.length; index += 1){
+  // retorna o numero pronto
+  for (index = 0; index < numeros.length; index += 1) {
   }
-  let numPronto = '(' + numeros[0] + numeros[1] + ') ' + numeros[2] + numeros[3] + numeros[4] + numeros[5] + numeros[6] + '-' + numeros[7] + numeros[8] + numeros[9] +numeros[10];
+  let numPronto = '(' + numeros[0] + numeros[1] + ') ' + numeros[2] + numeros[3] + numeros[4] + numeros[5] + numeros[6] + '-'
+  + numeros[7] + numeros[8] + numeros[9] + numeros[10];
   return numPronto;
 }
 
-
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
+  if (lineA > lineB + lineC || lineB > lineA + lineC || lineC > lineA + lineB) {
+    return 'false';
+  } else if (lineA < Math.abs(lineB - lineC))
   
 }
 console.log(triangleCheck(10, 14, 8))
