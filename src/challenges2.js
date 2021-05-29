bb4ff063d54801a84d1c1c2d01ee9417a2db44c0
Wesley de Melo Pedroso <wesleyxd@hotmail.com>
2021-05-29 14:04:08 -0300

    11 - Crie uma função de Número de telefone

--- src/challenges2.js
+++ src/challenges2.js
@@ -20,10 +20,48 @@ function techList(listaTecnologia, nome) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
-}
+function generatePhoneNumber(numero) {
+  let armazenaNumero = numero;
+  let contador = 0;
+  let mascaraTelefone = ["(","","",")"," ","","","","","","-","","","",""];
+  let numeroTelefone = "";
 
+  if (numero.length !== 11) {
+    return "Array com tamanho incorreto.";
+  }
+  for (let index = 0; index < numero.length; index += 1) {
+    if (numero[index] < 0 || numero[index] > 9) {
+      numeroTelefone = "não é possível gerar um número de telefone com esses valores"
+    }
+    for (let index2 = 0; index2 < armazenaNumero.length; index2 += 1) {
+      if (armazenaNumero[index2] === numero[index]) {
+        contador += 1;
+      }
+      if (contador >= 3) {
+        numeroTelefone = "não é possível gerar um número de telefone com esses valores";
+      }
+    }
+    contador = 0;
+  }
+
+  if (numeroTelefone === "") {
+    for (let index = 0; index < numero.length; index += 1) {
+      for (let index2 = 0; index2 < mascaraTelefone.length; index2 += 1) {
+        if (mascaraTelefone[index2] === "") {
+          numeroTelefone += numero[index];
+          index += 1;
+        }else {
+          numeroTelefone += mascaraTelefone[index2];
+        }
+      }
+      return numeroTelefone;
+    }
+  } else {
+    return numeroTelefone;
+  }
+
+}
+console.log(generatePhoneNumber([3, -2, 1, 9, -5, 0, 7, 4, 0, 6, 1]));
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
   if (lineC > (lineA + lineB) || lineA > (lineC + lineB) || lineB > (lineA + lineC)) {
