be851202263fefc743837823f66ecb67ef825317
Walace Nascimento <wlacens@hotmail.com>
2021-05-29 10:10:15 -0300

    Refatorando o código dos requistos de 1 a 5

--- src/challenges.js
+++ src/challenges.js
@@ -1,10 +1,11 @@
 // Desafio 1
 function compareTrue(n1, n2) {
-  if (n1 && n2) {
+  if (n1 === true && n2 === true) {
     return true;
   }
   return false;
 }
+console.log(compareTrue(true, false));
 
 // Desafio 2
 function calcArea(base, height) {
@@ -12,24 +13,28 @@ function calcArea(base, height) {
 
   return result;
 }
+console.log(calcArea(50, 3));
 
 // Desafio 3
 function splitSentence(string) {
   return string.split(' ');
 }
+console.log(splitSentence('O bicho tá pegando"'));
 
 // Desafio 4
 function concatName(nome) {
-  let resultado = `${nome[nome.length - 1]}, ${nome[0]}`;
-
+let resultado = `${nome[nome.length - 1]}, ${nome[0]}`;
+//let resultado = nome[nome.length -1] + ' ,' + nome[0];
   return resultado;
 }
+console.log(concatName(['Fulano', 'Beltrano', 'Cicrano', 'De tal']));
 
 // Desafio 5
 function footballPoints(wins, ties) {
   return ((wins * 3) + (ties * 1));
 
 }
+console.log(footballPoints(5, 2));
 
 // Desafio 6
 function highestCount() {
