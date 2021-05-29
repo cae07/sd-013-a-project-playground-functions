966e75b24fc5e2bfb1a0a7ab4aba9500dd3db354
Julio Fallace <julioscoffee@gmail.com>
2021-05-29 18:07:26 -0300

    iniciando requisito 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,26 @@
+// let techs = ["React", "Jest", "HTML", "CSS", "JavaScript"];
+// let name = 'julio';
+// for (let i = 0; i < techs.length; i += 1) {
+//   let obj = techs[i];
+//   obj = {};
+//   obj['tech'] = techs[i];
+//   obj['name'] = name;
+//   console.log(obj);
+// }
+
+
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(techs, name) {
+  let result;
+  for (let i = 0; i < techs.length; i += 1) {
+    obj = {};
+    obj['tech'] = techs[i];
+    obj['name'] = name;
+    result += obj;
+  }
+  return result;
 }
+//console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], 'julio'));
 
 // Desafio 11
 function generatePhoneNumber() {
