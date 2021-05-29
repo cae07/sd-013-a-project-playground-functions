911b501119c189b21390d53e7fb4ff3f8eef4dd5
Tomas Santos <tomas.slvsts@gmail.com>
2021-05-29 19:40:09 -0300

    atualizando

--- src/challenges.js
+++ src/challenges.js
@@ -5,7 +5,6 @@ function compareTrue(value1, value2) {
   }
   return false;
 }
-
 // Desafio 2
 function calcArea(base, heigth) {
   return (base * heigth) / 2;
@@ -21,13 +20,15 @@ function splitSentence(string1) {
 console.log(splitSentence('Go Trybe'));
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(arrayStrings) {
+  return arrayStrings[arrayStrings.length -1] + ' ', + arrayStrings[0];
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins,ties) {
+  let winsPoints = 3;
+  let tiesPoints = 1;
+  return (winsPoints * wins) + (tiesPoints * ties);
 }
 
 // Desafio 6
