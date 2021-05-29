a342b0e1968901d806fa055441f88f511ec2f2ed
thomasravache <thomasravache31@gmail.com>
2021-05-28 21:59:41 -0300

    Requisito 10 - Crie uma função de Lista de Tecnologias --> Finalizado.

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,39 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function isEmptyArray(array) {
+  let verificator = 0;
+  let emptyArray = false;
+
+  for (let value of array) {
+    if (value === '') {
+      verificator += 1;
+    }
+  }
+  if (verificator === array.length) {
+    emptyArray = true;
+  }
+
+  return emptyArray;
+}
+
+function techList(arrayOfItems, name) {
+  arrayOfItems.sort();
+  let arrayOfObjects = [];
+  let emptyArray = isEmptyArray(arrayOfItems);
+  let result;
+  if (emptyArray) {
+    result = 'Vazio!';
+  } else {
+    for (let index = 0; index < arrayOfItems.length; index += 1) {
+      let listTech = {
+        tech: arrayOfItems[index],
+        name,
+      };
+      arrayOfObjects.push(listTech);
+    }
+    result = arrayOfObjects;
+  }
+
+  return result;
 }
 
 // Desafio 11
