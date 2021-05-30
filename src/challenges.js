a74a52dcf2c2d7c6e5e34f81666709b8a4b2cfe5
Seu nomecd <canaldogildosantos@gmail.com>
2021-05-30 17:42:40 -0300

    resolve exercicio 7

--- src/challenges.js
+++ src/challenges.js
@@ -35,8 +35,18 @@ function highestCount() {
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  distancia1 = Math.abs(mouse - cat1);
+  distancia2 = Math.abs(mouse -cat2);
+  if ( distancia1 < distancia2){
+    return "cat1";
+  }
+  if ( distancia1 > distancia2){
+    return "cat2";
+  }
+ else {
+    return "os gatos trombam e o rato foge"
+ }
 }
 
 // Desafio 8
