a966d7fe35916607724a819bde645399f3cb0ff7
Pedro Trasfereti <pedrotrasfereti@gmail.com>
2021-05-30 18:02:52 -0300

    resolve desafios 12 e 13

--- src/challenges2.js
+++ src/challenges2.js
@@ -15,17 +15,33 @@ function techList(list, name) {
 
 // Desafio 11
 function generatePhoneNumber() {
-  // seu código aqui
 }
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+function triangleCheck(lineA, lineB, lineC) {
+  if (lineA > Math.abs(lineB) - Math.abs(lineC) && lineB > Math.abs(lineA) - Math.abs(lineC) && lineC > Math.abs(lineA) - Math.abs(lineB)) {
+    return true;
+  }
+  else {
+    return false;
+  }
 }
 
 // Desafio 13
-function hydrate() {
-  // seu código aqui
+function hydrate(barOrder) {
+  let recommendation = '';
+  let ints = barOrder.match(/\d+/g).map(Number);
+  let glassCount = 0;
+
+  for (let index = 0; index < ints.length; index += 1) {
+    glassCount += ints[index];
+  }
+  if (glassCount == 1) {
+    recommendation = glassCount + ' ' + 'copo de água';
+  } else {
+    recommendation = glassCount + ' ' + 'copos de água';
+  }
+  return recommendation;
 }
 
 module.exports = {
