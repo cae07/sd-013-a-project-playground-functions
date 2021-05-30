86f242a5311ff42fa83eaeea9dd380fcaead394c
joaovitor-B <joao.vitorjv2000@gmail.com>
2021-05-30 13:15:46 -0300

    desafio 7

--- src/challenges.js
+++ src/challenges.js
@@ -1,16 +1,16 @@
 // Desafio 1
 function compareTrue(val1, val2) {
-  if (val1 == true && val2 == true){
+  if (val1 == true && val2 == true) {
     return true;
   } else {
     return false;
   }
 }
-  console.log(compareTrue);
-  
+console.log(compareTrue);
+
 // Desafio 2
 function calcArea(base, height) {
-  return (base * height)/2
+  return (base * height) / 2;
 }
 
 // Desafio 3
@@ -20,7 +20,7 @@ function splitSentence(frase) {
 
 // Desafio 4
 function concatName(cat) {
-  return cat[cat.length-1] + ', ' + cat[0]
+  return cat[cat.length - 1] + ', ' + cat[0];
 }
 
 // Desafio 5
@@ -28,28 +28,34 @@ function footballPoints(wins, ties) {
   let win1 = wins * 3;
   let tie1 = ties * 1;
   let jn = win1 + tie1;
-  return jn
+  return jn;
 }
 
 // Desafio 6
 function highestCount(value) {
   let highnumber = value[0];
   let cont = 0;
- for (let i = 0; i < value.length; i += 1){
-   if (highnumber < value[i]){
-     highnumber = value[i]
-     cont = 1
-   } else if (value[i] === highnumber){
-     cont += 1
-   }
-   }
-   return cont 
- } 
- /*console.log(highestCount([9, 1, 2, 3, 9, 5, 7]))*/
+  for (let i = 0; i < value.length; i += 1) {
+    if (highnumber < value[i]) {
+      highnumber = value[i];
+      cont = 1;
+    } else if (value[i] === highnumber) {
+      cont += 1;
+    }
+  }
+  return cont;
+}
+/*console.log(highestCount([9, 1, 2, 3, 9, 5, 7]))*/
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  if (Math.abs(mouse - cat1) > Math.abs(mouse - cat2)){
+    return 'cat2';
+  }
+  if (Math.abs(mouse - cat1) < Math.abs(mouse - cat2)){
+    return 'cat1';
+  }
+  return 'os gatos trombam e o rato foge';
 }
 
 // Desafio 8
