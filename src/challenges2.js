a87dded9f6e081b5601809d345a133b21a23978b
gr3g1987@gmail.com <gabriel.gregorio1987@gmail.com>
2021-05-29 21:58:04 -0300

    Desafio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -23,9 +23,34 @@ console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"],"Lucas"))
 console.log(techList([],"Lucas"))
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(numbers) {
+  let msg = "não é possível gerar um número de telefone com esses valores";
+  let count = 1;
+  let contato = '';
+  if(numbers.length != 11) {
+    return "Array com tamanho incorreto.";
+
+  } else {
+
+    for(let i = 0; i < numbers.length; i += 1) {
+      if(numbers[i] < 0) {
+        return msg;
+      } else if(numbers[i] > 9) {
+        return msg;
+      } else if( numbers.indexOf(numbers[i]) != i ) {
+        count ++;
+        if(count >= 3) {
+          return msg;
+          break;
+        } 
+      }
+    }
+  }
+  contato = `(${numbers.slice(0,2).join('')})${numbers.slice(2,7).join('')}-${numbers.slice(7,11).join('')}`
+
+  return contato;
 }
+console.log((generatePhoneNumber([1,2,3,4,5,6,7,8,9,0,0])));
 
 // Desafio 12
 function triangleCheck() {
