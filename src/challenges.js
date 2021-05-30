72baf123726875cb8775a72222f9fd8030047a82
JoseEduardognzg <joseeduardognzg@gmail.com>
2021-05-30 09:34:35 -0300

    Requisitos 5 e 6

--- src/challenges.js
+++ src/challenges.js
@@ -30,17 +30,34 @@ function concatName() {
 }
 
 // Desafio 5
-function footballPoints() {
+function footballPoints(wins, ties) {
+  pointsWins = wins * 3;
+  pointsTies = ties * 1;
+  let res = pointsWins + pointsTies
+  return res
   // seu código aqui
 }
 
+// Ajuda atravez do link:https://www.youtube.com/watch?v=pkJXwPvselI
 // Desafio 6
-function highestCount() {
+function highestCount(valor) {
+  let maiorNumero = valor[0];
+  let cont = 0;
+  for (let index = 0; index < valor.length; index += 1) {
+    if (valor[index] > maiorNumero) {
+      maiorNumero = valor[index];
+      cont = 0;
+    }
+    if (valor[index] === maiorNumero) {
+      cont += 1;
+    }
+  }
+  return cont;
   // seu código aqui
-}
+
 
 // Desafio 7
-function catAndMouse() {
+function catAndMouse(mouse, cat1, cat2) {
   // seu código aqui
 }
 
