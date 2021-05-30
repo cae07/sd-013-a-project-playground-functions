baed1f3dd8b20f30d7ee67b3e4e6d9302aae33b5
Marlon Lacerda <marlon.desagitario@hotmail.com>
2021-05-30 20:32:41 -0300

    Completei o desafio 12

--- src/challenges2.js
+++ src/challenges2.js
@@ -9,28 +9,37 @@ function generatePhoneNumber() {
 }
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+function triangleCheck(lineA, lineB, lineC) {
+  if (lineA > (lineB + lineC) || lineA < Math.abs(lineB - lineC)) {
+    return false;
+  }
+  if (lineB > (lineA + lineC) || lineB < Math.abs(lineA - lineC)) {
+    return false;
+  }
+  if (lineC > (lineA + lineB) || lineC < Math.abs(lineA - lineB)) {
+    return false;
+  }
+  return true;
 }
 
 // Desafio 13
 function hydrate(array) {
   let regex = /\d+/g;
-  let numString = array.match(regex);  
+  let numString = array.match(regex);
   let conta = 0;
 
-  let numInteg = numString.map(function (result){
-    let resultado = parseInt(result);
-    return resultado
-  })
+  let numInteg = numString.map(function (result) {
+    let resultado = parseInt(result, 10);
+    return resultado;
+  });
 
-  for (let index = 0; index < numInteg.length; index++) {
-    conta += numInteg[index]
+  for (let index = 0; index < numInteg.length; index += 1) {
+    conta += numInteg[index];
   }
 
-  if (conta > 1){
+  if (conta > 1) {
     return `${conta} copos de água`;
-  } 
+  }
   return `${conta} copo de água`;
 }
 
