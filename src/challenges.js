b64e92e394d57cf46023caa726140425d530ad7c
Sethano <vitorcard1720@gmail.com>
2021-05-30 10:53:56 -0300

    quarta parte

--- src/challenges.js
+++ src/challenges.js
@@ -49,24 +49,53 @@ function highestCount(numberArray) {
   }
   return count;
 }
-console.log(highestCount([0, 5, 4, 3, 2, 1]));
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  let relPosCat1 = Math.abs((cat1 > mouse) ? (cat1 - mouse) : (mouse - cat1));
+  let relPosCat2 = Math.abs((cat2 > mouse) ? (cat2 - mouse) : (mouse - cat2));
+  if (relPosCat1 < relPosCat2) {
+    return 'cat1';
+  }
+  if (relPosCat1 > relPosCat2) {
+    return 'cat2';
+  } return 'os gatos trombam e o rato foge';
 }
 
 // Desafio 8
 function fizzBuzz() {
-  // seu código aqui
+  // let numbers = nArray;
+  // let fizzBuzzString = [];
+  // for (let i = 0; i < numbers.length; i += 1) {
+  //   let calc3 = numbers[i] / 3;
+  //   let calc5 = numbers[i] / 5;
+  //   if (Number.isInteger(calc5) === true && Number.isInteger(calc3) === true) {
+  //     fizzBuzzString.push('fizzbuzz');
+  //   } else if (Number.isInteger(calc3) === true) {
+  //     fizzBuzzString.push('fizz');
+  //   } else if (Number.isInteger(calc5) === true) {
+  //     fizzBuzzString.push('buzz');
+  //   } fizzBuzzString.push('bug!');
+  // }
+  // return fizzBuzzString;
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(toEcode) {
+  let replace = toEcode.replace(/a/g, 1);
+  replace = replace.replace(/e/g, 2);
+  replace = replace.replace(/i/g, 3);
+  replace = replace.replace(/o/g, 4);
+  replace = replace.replace(/u/g, 5);
+  return replace;
 }
-function decode() {
-  // seu código aqui
+function decode(toDecode) {
+  let decoding = toDecode.replace(/1/g, 'a');
+  decoding = decoding.replace(/2/g, 'e');
+  decoding = decoding.replace(/3/g, 'i');
+  decoding = decoding.replace(/4/g, 'o');
+  decoding = decoding.replace(/5/g, 'u');
+  return decoding;
 }
 
 module.exports = {
--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,25 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(tech, name) {
+  let results = [];
+  let techNames = tech;
+  let finalList = [];
+  if (techNames.length === 0) {
+    return ('Vazio!');
+  } for (let i = 0; i < techNames.length; i += 1) {
+    let object = {
+      tech: '',
+      name: name,
+    };
+    object.tech = techNames[i];
+    results.push(object);
+  }  finalList = results.sort(function (a, b) {
+    if (a.tech > b.tech) {
+      return 1;
+    } if (a.tech < b.tech) {
+      return -1;
+    } return 0;
+  });
+  return finalList;
 }
 
 // Desafio 11
