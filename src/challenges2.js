286708e885f177662160a3b2820e5f25657a36c6
Debora Teodorico <debora.steodorico@gmail.com>
2021-05-30 15:57:41 -0300

    Adicionando questao 11, fui auxiliada pelos colegas josué, greg e gabriel

--- src/challenges2.js
+++ src/challenges2.js
@@ -20,8 +20,29 @@ function techList(arrayTecnologia, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(array) {
+  let tamanhoIncorreto = 'Array com tamanho incorreto.';
+  let valoreInvalidos = 'Não é possível gerar um número de telefone com esses valores.';
+  if (array.length > 11) {
+    return tamanhoIncorreto;
+  }
+  // SOBRE CONTADOR E FOR OBTIVE AJUDA DOS COLEGAS JOSUE LOBO, GREG E GABRIEL GASPAR.
+  for (let i = 0; i < array.length; i += 1) {
+    let contator = 0;
+    for (let j = 0; j < array.length; j += 1) {
+      contator = 0;
+      if (array[i] < 0 || array[i] > 9) {
+        return valoreInvalidos;
+      } else if (array[i] === array[j]) {
+        contator += 1;
+      }
+    }
+    if (contator >= 3) {
+      return valoreInvalidos;
+    }
+  }
+  let numeroDeTelefone = `(${array[0]}${array[1]}) ${array[2]}${array[3]}${array[4]}${array[5]}${array[6]}-${array[7]}${array[8]}${array[9]}${array[10]}`;
+  return numeroDeTelefone;
 }
 
 // Desafio 12
