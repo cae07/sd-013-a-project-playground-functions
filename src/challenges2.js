c09840e7ef3b1611d0a6ccf68757b3ed37421c0c
LuizFJP <luizfjportela@gmail.com>
2021-05-29 22:25:15 -0300

    exercise 010

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,8 +1,28 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
-}
+function techList(tech, name) {
 
+  let nameObj = name;
+  let alphabeticNames = [];
+  let arrayAux = [];
+  
+  if(tech.length == 0){
+    return "Vazio!"
+  }
+
+  tech.sort();
+  alphabeticNames = tech;
+
+  for (let names of alphabeticNames){
+
+    let techObject = {
+      name: nameObj,
+      tech: names     
+  }
+  arrayAux.push(techObject)
+  }
+  return arrayAux
+}
+techList(["React", "Jest", "HTML", "CSS", "JavaScript"], "Lucas")
 // Desafio 11
 function generatePhoneNumber() {
   // seu código aqui
