72d1de759b2e9f778adc9e84974c78668526f51c
Matheus Camillo <matheuscamillo@hotmail.com.br>
2021-05-29 22:59:00 -0300

    feito desafio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -17,9 +17,66 @@ function techList(tecnologias, name) {
   return tecnologia;
 }
 
-// Desafio 11
-function generatePhoneNumber() {
+function checarComprimeto(numero) {
+  if (numero.length !== 11) {
+    return true;
+  }
+}
 
+function checarNumeroValido(numero) {
+  for (let i = 0; i < numero.length; i += 1) {
+    if (numero[i] < 0 || numero[i] > 9) {
+      return true;
+    }
+  }
+}
+
+function contaRepeticao(numero, elemento) {
+  let contador = 0;
+  for (let index2 = 0; index2 < numero.length; index2 += 1) {
+    if (numero[index2] === elemento) {
+      contador += 1;
+    }
+  }
+  return contador;
+}
+
+function validaRepeticao(numero) {
+  for (let index = 0; index < numero.length; index += 1) {
+    let repete = contaRepeticao(numero, numero[index]);
+    if (repete >= 3) {
+      return true;
+    }
+  }
+}
+
+function montaNumero(numero) {
+  let phoneMontado = numero.join('');
+
+  let ddd = phoneMontado.slice(0, 2);
+
+  let prefixNumber = phoneMontado.slice(2, 7);
+
+  let sufixNumber = phoneMontado.slice(7);
+
+  return `(${ddd}) ${prefixNumber}-${sufixNumber}`;
+}
+
+// Desafio 11
+function generatePhoneNumber(numero) {
+  if (checarComprimeto(numero)) {
+    return 'Array com tamanho incorreto.';
+  }
+
+  if (checarNumeroValido(numero)) {
+    return 'não é possível gerar um número de telefone com esses valores';
+  }
+
+  if (validaRepeticao(numero)) {
+    return 'não é possível gerar um número de telefone com esses valores';
+  }
+
+  return montaNumero(numero);
 }
 
 // Desafio 12
