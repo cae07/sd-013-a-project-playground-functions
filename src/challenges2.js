33e1b68f1b0216c9cf4392d164d2d2a51a99d081
gr3g1987@gmail.com <gabriel.gregorio1987@gmail.com>
2021-05-30 00:06:57 -0300

    corrigindo o contador

--- src/challenges2.js
+++ src/challenges2.js
@@ -25,35 +25,27 @@ console.log(techList([],"Lucas"))
 // Desafio 11
 function generatePhoneNumber(numbers) {
   let msg = "não é possível gerar um número de telefone com esses valores";
-  let count = 1;
   let contato = `(${numbers.slice(0,2).join('')}) ${numbers.slice(2,7).join('')}-${numbers.slice(7,11).join('')}`;
+  
   if(numbers.length != 11) {
     return "Array com tamanho incorreto.";
-
   } else {
-
     for(let i = 0; i < numbers.length; i += 1) {
+      let count = 0;
       if(numbers[i] < 0) {
         return msg;
       } else if(numbers[i] > 9) {
         return msg;
-      } else if( numbers.indexOf(numbers[i]) != i ) {
+      } else if((numbers.indexOf(numbers[i]) != i))  {
         count ++;
-      } 
-    }
-    if(count >= 3) {
-      return msg;
+      } else  if(count >= 3) {
+        return msg;
+      }
     }
   }
-
-  
-
-
-
-
   return contato;
 }
-console.log(generatePhoneNumber([1, 2, 8, 0, 5, 3, 7, 8, 9, 1, 8]));
+console.log(generatePhoneNumber([5, 2, 8, 1, 5, 3, 7, 2, 8, 9, 0]));
 
 // Desafio 12
 function triangleCheck() {
@@ -71,3 +63,11 @@ module.exports = {
   hydrate,
   triangleCheck,
 };
+
+function test() {
+  let test = [1,2,3,4,5,4,5,7,5]
+  let aux = 0;
+  for(let i = 0; i < test.length; i += 1) {
+    
+  }
+}
