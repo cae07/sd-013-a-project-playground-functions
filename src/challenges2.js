770485b28ba887a37d75c93ed558597b8df4dc52
Tomas Santos <tomas.slvsts@gmail.com>
2021-05-30 16:40:06 -0300

    agora tem que ir

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,21 +1,25 @@
 // Desafio 10
-function techList(tecNames, n4me) {
-  let tecName0rdenado = tecName.sort();
-  let tecListReturn = [];
-  if (tecName.length !== 0) {
-    for (let key = 0; techName0rdenado.length > key; key +=1) {
-      let tecList = {
-        tech: techName0rdenado[key],
-        name: n4me,
-      };
-      techListReturn.push(tecList);
-    }
-  } else {
-    return 'vazio';
-  }
-  return tecListReturn;
-} 
+function techList(tech, name) {
+  if(tech.length > 0) {
+    order = tech.sort();
 
+    for (let indexTechList = 0; indexTechList < tech.length; indexTechList +=1) {
+
+      object = {
+        tech: order[indexTechList],
+        name: name,
+      }
+
+      resultTechList[indexTechList] = object;
+    };
+  } else {
+    resultTechList = 'Vazio!';
+
+  };
+
+  return resultTechList;
+};
+  
 // Desafio 12
 function triangleCheck() {
   // seu código aqui
