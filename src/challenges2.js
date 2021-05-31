70508def0179205322b1c5c41fb634b044231cac
Wesley Maia <maia.weol@gmail.com>
2021-05-31 14:13:03 -0300

    Requisito 12- função de Condição de existência de um triângulo

--- src/challenges2.js
+++ src/challenges2.js
@@ -3,23 +3,22 @@ function techList(tech, name) {
   // seu código aqui
   let msg = '';
   let arrayObj = [];
-  
+  let ordena = [];
   if (tech.length < 1) {
     msg = 'Vazio!';
-  } else{
-    var ordena = tech.sort();
-    for(let index = 0; index < ordena.length; index +=1) {
+  } else {
+    ordena = tech.sort();
+    for (let index = 0; index < ordena.length; index += 1) {
       let objeto = {
         tech: ordena[index],
-        name: name
-      }
+        name: name,
+      };
       arrayObj.push(objeto);     
     }
     msg = arrayObj;
   }
   return msg;
 }
-//console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], "Lucas"));
 
 // Desafio 11
 function generatePhoneNumber() {
@@ -27,9 +26,25 @@ function generatePhoneNumber() {
 }
 
 // Desafio 12
-function triangleCheck() {
+function triangleCheck(lineA, lineB, lineC) {
   // seu código aqui
+  if (lineA < (lineB + lineC) && (lineA > Math.abs(lineB - lineC))) {
+    if (lineB < (lineA + lineC) && (lineB > Math.abs(lineA - lineC))) {
+      if(lineC < (lineB + lineA) && (lineC > Math.abs(lineB - lineA))) {
+        return true;
+      }
+      else {
+        return false
+      }
+    } else {
+      return false
+    }
+  }
+  else {
+    return false;
+  }
 }
+console.log(triangleCheck(10,14,50))
 
 // Desafio 13
 function hydrate() {
