6798509dc3b5c6aab9d9be2814c52fb46b5ca048
João Victor Paduam <joao.paduam@hotmail.com>
2021-05-31 17:56:48 -0300

    Primeiro requisito finalizado

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-function compare(a, b) {
-  if (a + b == 3 && a * b == 2){
-    return true;
-  }else{
-   return false;
-  }
+function compareTrue(a,b) {
+  if (a && b) {
+    return true
+  } 
+
+  return false
 }
-compare(1,2);
-console.log(compare(1,3));
+
