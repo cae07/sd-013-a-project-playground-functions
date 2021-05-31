00705e160e69e7d14d7f4c04d651b4020e271397
Marcelo Pantoja <pantoja.marcelo@hotmail.com>
2021-05-31 15:42:59 -0300

    adiciona requisito 6 e 7

--- src/challenges.js
+++ src/challenges.js
@@ -50,21 +50,44 @@ function footballPoints(wins, ties) {
 // Desafio 6
 function highestCount(array) {
   let conta = 0;
+  let negativa = -1;
   for (let i = 0; i < array.length; i += 1) {
-    if (array[i] >= array[i+1]) {
+    if ((array[i] >= array[i+1]) && (array[i] !== negativa)) {
+      conta = 1;
+      negativa = array[i];
+    }else if ((array[i] >= array[i+1]) && (array[i] === negativa)) {
       conta += 1;
-    } else if (array[array.length - 1] <= array[array.length - 2]) {
+    if (array[array.length - 1] === array[array.length - 2]) {
       conta += 1;
-    }
-  }
+    }  
+    }  
+  }       
   return conta; 
 }
-let teste = [0, 4, 4, 4, 9, 2, 1];
+let teste = [0, 0, 0];
 console.log(highestCount(teste));
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  let distanciaDoGato1 = 0;
+  let distanciaDoGato2 = 0;
+  if (mouse < cat1) {
+    distanciaDoGato1 = cat1 - mouse;
+ }else {
+    distanciaDoGato1 = mouse - cat1;
+ }
+  if (mouse < cat2) {
+    distanciaDoGato2 = cat2 - mouse;
+  }else {
+    distanciaDoGato2 = mouse - cat2;
+  }
+  if (distanciaDoGato1 === distanciaDoGato2) {
+    return 'os gatos trombam e o rato foge';
+  }else if (distanciaDoGato1 < distanciaDoGato2) {
+    return 'cat1';
+  }else {
+    return 'cat2';
+  }
 }
 
 // Desafio 8
