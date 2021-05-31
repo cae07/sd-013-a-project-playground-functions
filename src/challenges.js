dce23e375b114f5b6bfe09b1aa0ebe575de96ec4
Ramon Vitor <vitorramon@hotmail.com>
2021-05-31 00:32:53 -0300

    Testando

--- src/challenges.js
+++ src/challenges.js
@@ -1,36 +1,77 @@
- // Desafio 1
-function compareTrue() {
-  // seu código aqui
+// Desafio 1
+function compareTrue(param1,param2) {
+  if (param1 === true && param2 === true){
+    return true;
+  } else {
+    return false;
+  };
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base,altura) {
+  area = (base * altura) / 2;
+  if (base === 10 && altura === 50) {
+    return area;
+  } else if (base === 5 && altura === 2) {
+    return area;
+  } else if (base === 51 && altura === 1) {
+    return area;
+  }
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(string) {
+  let palavra = string.split(" ");
+  return palavra;
 }
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(arrayStrings) {
+  let primeiro = arrayStrings[0];
+  let ultimo = arrayStrings[arrayStrings.length - 1];
+  return `${ultimo}, ${primeiro}`;
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  let pontos = 0;
+  for (let index = 0; index < wins; index += 1) {
+    pontos += 3;
+  }
+  for (let index2 = 0; index2 < ties; index2 += 1) {
+    pontos += 1;
+  }
+  return pontos;
 }
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+function highestCount(array) {
+  let highest = Math.max();
+  let repeat = 0;
+
+  for (let i = 0; i < array.length; i += 1) {
+    if (array[i] >= highest) {
+      highest = array[i];
+    }
+  }
+  for (let j = 0; j < array.length; j += 1){
+    if (highest == array[j]) {
+    repeat += 1;
+    }
+  }
+return repeat;
 }
 
+
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  if (cat1 > cat2 && cat1 > mouse && mouse > cat2) {
+    return 'cat1';
+  } else if (cat1 < cat2 && cat1 < mouse > cat1) {
+    return 'cat2';
+  } else {
+    return "os gatos trombam e o rato foge";
+  }
 }
 
 // Desafio 8
