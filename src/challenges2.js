b17df1a0a0f9340b040fe4bdfb593af9019cb293
Bruno Pinho <brunopinho@outlook.com>
2021-05-31 00:03:28 -0300

    ex011 feito parcialmente

--- src/challenges2.js
+++ src/challenges2.js
@@ -34,9 +34,30 @@ function techList(tech, name) {
 console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], 'Lucas'));
 
 // Desafio 11
-function generatePhoneNumber() {
+function generatePhoneNumber(numbers) {
   // seu código aqui
+  let invalidNumbers = numbers.some(checkNumbers);
+
+  if (numbers.length !== 11) {
+    return "Array com tamanho incorreto.";
+  }
+  if (invalidNumbers) {
+    return "não é possível gerar um número de telefone com esses valores";
+  } 
+
+
+  return `(${numbers[0]}${numbers[1]}) ${numbers[2]}${numbers[3]}${numbers[4]}${numbers[5]}${numbers[6]}-${numbers[7]}${numbers[8]}${numbers[9]}${numbers[10]}`;
+  
 }
+
+//solução baseada em: https://www.w3schools.com/jsref/jsref_some.asp, https://desenvolvimentoparaweb.com/javascript/every-some-find-includes-javascript/
+function checkNumbers(check) {
+  if (check < 0 || check > 9) {
+    return true;
+  }
+}
+
+generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1])
 
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
