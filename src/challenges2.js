1c7165ddafe286d6a66e1ecd4babe9cd8836f4a0
Wesley Maia <maia.weol@gmail.com>
2021-05-31 13:36:22 -0300

    Requisito 10- função de Lista de Tecnologias

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,25 @@
 // Desafio 10
-function techList() {
+function techList(tech, name) {
   // seu código aqui
+  let msg = '';
+  let arrayObj = [];
+  
+  if (tech.length < 1) {
+    msg = 'Vazio!';
+  } else{
+    var ordena = tech.sort();
+    for(let index = 0; index < ordena.length; index +=1) {
+      let objeto = {
+        tech: ordena[index],
+        name: name
+      }
+      arrayObj.push(objeto);     
+    }
+    msg = arrayObj;
+  }
+  return msg;
 }
+//console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], "Lucas"));
 
 // Desafio 11
 function generatePhoneNumber() {
