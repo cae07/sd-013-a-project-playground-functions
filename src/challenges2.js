d374eec499fe2ff590fb8e857432c3983ddbf14b
suco <francalucas937@gmail.com>
2021-05-31 00:26:24 -0300

    desafio 10 concluido

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,20 @@
 // Desafio 10
-function techList() {
+// /* eslint-disable */ 
+let arrayObjects = [];
+function techList(tech, name) {
   // seu código aqui
+  if (tech.length === 0) {return "Vazio!"}
+	for (const key in tech.sort()) {
+		let nomeTech = {
+			name: name,
+			tech: tech[key],
+		};
+		arrayObjects.concat(arrayObjects.push(nomeTech));
+	}return arrayObjects;
 }
+let techArray = [];
+let nome = "lucas";
+console.log(techList(techArray ,nome));
 
 // Desafio 11
 function generatePhoneNumber() {
