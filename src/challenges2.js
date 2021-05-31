4b7c68bba075beb3eacb490f70a4e6f1c4c34cf9
Thiago <thiagoc.carboneri@gmail.com>
2021-05-31 11:52:38 -0300

    exercicio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -17,7 +17,25 @@ function techList(techs, nome) {
 }
 // Desafio 11
 function generatePhoneNumber(arrayNumber) {
+  let number = '(' + arrayNumber[0] + arrayNumber[1] + ')' + " " + arrayNumber[2] + arrayNumber[3] + arrayNumber[4] + arrayNumber[5] + arrayNumber[6] + '-' + arrayNumber[7] + arrayNumber[8] + arrayNumber[9] + arrayNumber[10];
+  if (arrayNumber.length !== 11) { // tamanho diferente de 11
+    return 'Array com tamanho incorreto.';
+  }
+  let countNumbers = {};
+  for (let index = 0; index < arrayNumber.length; index++) {
+    const n = arrayNumber[index];
+    if (countNumbers[n]) {
+      countNumbers[n] += 1;
+    } else {
+      countNumbers[n] = 1;
+    }
+    if (countNumbers[n] > 2 || (n < 0 || n > 9)) {
+      return 'não é possível gerar um número de telefone com esses valores'; // repete por mais de 3 vezes
+    }
+  }
+  return number;
 }
+console.log(generatePhoneNumber([10, 2, 3, 1, 5, 6, 7, 8, 9, 0, 1]));
 // Desafio 12
 function triangleCheck() {
   // seu código aqui
