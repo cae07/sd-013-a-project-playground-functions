535c70a270a7d664cc4cb6ac5626fb68aae0bb65
Eduardo Rossi <eduardo.g.rossi@hotmail.com>
2021-05-31 22:13:33 -0300

    Metade do exercício

--- src/challenges.js
+++ src/challenges.js
@@ -1,6 +1,6 @@
 // Desafio 1
-function compareTrue(parametro_1,parametro_2) {
- if (parametro_1 === true && parametro_2 === true){
+function compareTrue(param1_1,param1_2) {
+ if (param1_1 === true && param1_2 === true){
    return true;
  } else {
    return false;
@@ -8,29 +8,50 @@ function compareTrue(parametro_1,parametro_2) {
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base,height) {
+  let area = (base*height)/2; 
+  return area
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(sentence) {
+  let y =0;
+  for (let i=0; i < sentence.length; i++){
+    
+      
+    }
+  }
 }
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(param4_1) {
+  return param4_1[param4_1.length-1]+", "+param4_1[0]
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins,ties) {
+ return wins*3+ties*1
 }
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
-}
+function highestCount(param6_1) {
+  let max_number =0;
+  let sumMaxNumber=0;
+  
+    for (i = 0; i < param6_1.length; i++){
+      if (param6_1[i]>max_number){
+        max_number = param6_1 [i];
+      }
+    }
+
+
+    for (y = 0; y < param6_1.length; y++){
+      if (param6_1[y] === max_number){
+        sumMaxNumber = sumMaxNumber +1;
+      }
+    }
+    return sumMaxNumber
+  }
 
 // Desafio 7
 function catAndMouse() {
