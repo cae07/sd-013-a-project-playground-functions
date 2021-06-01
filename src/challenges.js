01175fde3806bad7b6ae719c683f436a91bef6c3
Jean Marcel <jeanvanzella91@gmail.com>
2021-06-01 04:53:36 -0300

    em busca do sonho2

--- src/challenges.js
+++ src/challenges.js
@@ -19,9 +19,7 @@ function splitSentence(frase) {
 
 // Desafio 4
 function concatName(parametro) {
-  let param1 = parametro.shift();
-  let param2 = parametro.pop();
-  return (`${param1}, ${param2}`);
+  return (parametro[parametro.lenght - 1] + ', ' + parametro[0]);
 }
 
 // Desafio 5
@@ -34,7 +32,7 @@ function highestCount(numbers) {
   let maior = Math.max(...numbers);
   let contador = 0;
   for (let i = 0; i < numbers.lenght; i += 1) {
-    if (maior == numbers[i]) {
+    if (maior === numbers[i]) {
       contador += 1;
     }
   }
@@ -43,12 +41,13 @@ function highestCount(numbers) {
 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
-  let dist1 = cat1 - mouse;
-  let dist2 = cat2 - mouse;
-  if (dist1 > dist2) {
+  let dist1 = Math.abs(mouse - cat1);
+  let dist2 = Math.abs(mouse - cat2);
+
+  if (dist1 < dist2) {
+    return cat1;
+  } else if (dist2 < dist1) {
     return cat2;
-  } else if (dist2 > dist1) {
-    return cat1; 
   } else if (dist2 === dist1) {
     return ('os gatos trombam e o rato foge');
   }
@@ -57,15 +56,18 @@ function catAndMouse(mouse, cat1, cat2) {
 // Desafio 8
 function fizzBuzz(numbers) {
   let novoArray = [];
+
   for (let i = 0; i < numbers.lenght; i += 1) {
-    if ((numbers[i] % 3) === 0) {
-      novoArray.push('fizz');
+
+    if ((numbers[i] % 3) === 0 && (numbers[i] % 5) === 0) {
+      novoArray.push('fizzBuzz');
     } else if ((numbers[i] % 5) === 0) {
       novoArray.push('buzz');
-    } else if ((numbers[i] % 3) === 0 && (numbers[i] % 5) === 0) {
-      novoArray.push('fizzBuzz');
+    } else if ((numbers[i] % 3) === 0) {
+      novoArray.push('fizz');
     } else {
-      novoArray.push('bug!'); }
+      novoArray.push('bug!');
+    }
   }
   return novoArray;
 }
