0e0934721116e13ba11aa8f8305bd0e5bb0fe058
Jean Marcel <jeanvanzella91@gmail.com>
2021-06-01 05:05:30 -0300

    sonho 3

--- src/challenges.js
+++ src/challenges.js
@@ -19,7 +19,8 @@ function splitSentence(frase) {
 
 // Desafio 4
 function concatName(parametro) {
-  return (parametro[parametro.lenght - 1] + ', ' + parametro[0]);
+  let arrayA = parametro
+  return (arrayA[arrayA.lenght - 1] + ', ' + arrayA[0]);
 }
 
 // Desafio 5
@@ -29,26 +30,32 @@ function footballPoints(wins, ties) {
 
 // Desafio 6
 function highestCount(numbers) {
-  let maior = Math.max(...numbers);
   let contador = 0;
-  for (let i = 0; i < numbers.lenght; i += 1) {
-    if (maior === numbers[i]) {
-      contador += 1;
+  let maior = numbers[0];
+
+  for (let i = 1; i < numbers.length; i += 1) {
+    if (numbers[i] > maior) {
+      maior = numbers[i];
     }
   }
-  return contador;
+  for (let i2 = 0; i2 < numbers.length; i2 += 1) {
+    if (maior === numbers[i2]) {
+      count = count + 1;
+    }
+  }
+  return count;
 }
 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
-  let dist1 = Math.abs(mouse - cat1);
-  let dist2 = Math.abs(mouse - cat2);
+  let dist1 = (mouse - cat1);
+  let dist2 = (mouse - cat2);
 
   if (dist1 < dist2) {
-    return cat1;
+    return 'cat1';
   } else if (dist2 < dist1) {
-    return cat2;
-  } else if (dist2 === dist1) {
+    return 'cat2';
+  } else {
     return ('os gatos trombam e o rato foge');
   }
 }
@@ -58,7 +65,6 @@ function fizzBuzz(numbers) {
   let novoArray = [];
 
   for (let i = 0; i < numbers.lenght; i += 1) {
-
     if ((numbers[i] % 3) === 0 && (numbers[i] % 5) === 0) {
       novoArray.push('fizzBuzz');
     } else if ((numbers[i] % 5) === 0) {
