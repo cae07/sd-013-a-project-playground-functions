92c853af3376417e4d3f160e14898847c03b48ae
RuanCunha <ruanvcunha@hotmail.com>
2021-06-01 13:19:55 -0300

    Finalizados desafios 10, 13 e iniciado 11

--- src/challenges.js
+++ src/challenges.js
@@ -1,5 +1,5 @@
 // Desafio 1
-function compareTrue(number1, number2) {
+function compareTrue (number1, number2) {
   if (number1 && number2){
     return true;
   }
@@ -9,43 +9,43 @@ console.log(compareTrue(1, 1));
 
 // Desafio 2
 function calcArea(base, height) {
-  return (base*height)/2;
+  return (base * height) / 2;
 }
 
 // Desafio 3
 function splitSentence(str) {
-  let res = str.split(" ");
+  let res = str.split(' ');
   return res;
 }
 
 // Desafio 4
 function concatName(array) {
-  let res = (array[array.length-1] + ', ' + array[0]);
+  let res = (array[array.length - 1] + ', ' + array[0]);
   return res;
 }
 
 // Desafio 5
 function footballPoints(wins, ties) {
-  let res = (wins*3)+(ties);
+  let res = (wins * 3) + (ties);
   return res;
 }
 
 // Desafio 6
 function highestCount(array) {
   let compare;
-  if (array[0]>array[1]){
+  if (array[0] > array[1]) {
     compare = array[0];
   } else {
     compare = array[1];
   }
-  for (let index = 2; index < array.length; index +=1){
-    if (array[index] > compare){
+  for (let index = 2; index < array.length; index += 1) {
+    if (array[index] > compare) {
       compare = array[index];
     }
   }
   let count = 0;
-  for (let index = 0; index < array.length; index +=1){
-    if (array[index]==compare){
+  for (let index = 0; index < array.length; index += 1) {
+    if (array[index] === compare) {
       count += 1;
     }
   }
@@ -57,14 +57,14 @@ function catAndMouse(mouse, cat1, cat2) {
   mouse = Math.abs(mouse);
   cat1 = Math.abs(cat1);
   cat2 = Math.abs(cat2);
-  let dist1 = Math.max(mouse, cat1) - Math.min(mouse,cat1);
-  let dist2 = Math.max(mouse, cat2) - Math.min(mouse,cat2);
-  if (dist1 == dist2){
-    return ("os gatos trombam e o rato foge");
+  let dist1 = Math.max(mouse, cat1) - Math.min(mouse, cat1);
+  let dist2 = Math.max(mouse, cat2) - Math.min(mouse, cat2);
+  if (dist1 === dist2) {
+    return ('os gatos trombam e o rato foge');
   } else if (dist1 < dist2){
-    return ("cat1");
+    return ('cat1');
   } else {
-    return ("cat2");
+    return ('cat2');
   }
 }
 
--- src/challenges2.js
+++ src/challenges2.js
@@ -1,11 +1,59 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(tecArray, name) {
+  tecArray.sort();
+  let arrayFinal = [];
+  if (tecArray.length != 0) {
+    for (let index = 0; index < tecArray.length; index += 1){
+      arrayFinal[index] = {
+        tech: tecArray[index],
+        name: name
+      }
+    }
+  } else {
+    arrayFinal = 'Vazio!';
+  }
+  return arrayFinal;
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(number) {
+  if (testQuantity(number) || checkZero(number)) {
+    res = '(' + number[0] + number[1] + ') ' + number[2] + number[3] + number[4] + number[5] + number[6] + '-' + number[7] + number[8] + number[9] + number[10]
+  } else {
+    res = "não é possível gerar um número de telefone com esses valores"
+  }
+  return res;
+}
+
+function testQuantity(number) {
+  let count = 0;
+  let compare = 0;
+  if (number[0] > number[1]) {
+    compare = number[0];
+  } else {
+    compare = number[1];
+  }
+  for (let index = 2; index < number.length; index += 1) {
+    if (number[index] > compare) {
+      compare = number[index];
+    }
+  }
+  for (let index = 0; index < number.length; index += 1) {
+    if (number[index] === compare) {
+      count += 1;
+    }
+  }
+  if (count > 3) {
+    return false;
+  } return true;
+}
+
+function checkZero(number) {
+  for (let index = 0; index < number.length; index += 1) {
+    if (number[index] < 0 || number[index] > 0) {
+      return false;
+    }
+  }
 }
 
 // Desafio 12
@@ -14,8 +62,17 @@ function triangleCheck() {
 }
 
 // Desafio 13
-function hydrate() {
-  // seu código aqui
+function hydrate(texto) {
+  let array = texto.match(/\d+/g);
+  let sum = 0;
+  for(let index = 0; index < array.length; index += 1) {
+    sum = sum + parseInt(array[index]);
+  }
+  if(sum > 1){
+    return sum + ' copos de água';
+  } else {
+  return sum + ' copo de água';
+  }
 }
 
 module.exports = {
