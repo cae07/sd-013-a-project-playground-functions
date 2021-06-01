f764c5313a4d907c0d8c93942106da5ea04a8a2c
junglejf <jfcchagas@id.uff.br>
2021-06-01 06:21:56 -0300

    resolvidos maioria dos lint revisar challeng2.js complexity problem

--- src/challenges.js
+++ src/challenges.js
@@ -1,133 +1,84 @@
 // Desafio 1
-function compareTrue(key1,key2) {
+function compareTrue(key1, key2) {
   return (key1 && key2);
 }
 // Desafio 2
-function calcArea(base,altura) {
-  let area = (base * altura)/2;
+function calcArea(base, altura) {
+  let area = (base * altura) / 2;
   return area;
 }
 // Desafio 3
 function splitSentence(frase) {
-  let fraseSeparada = frase.split(" ");
+  let fraseSeparada = frase.split(' ');
   return fraseSeparada;
 }
 
 // Desafio 4
 function concatName(arrayString) {
-  return(arrayString[arrayString.length - 1]+", "+arrayString[0]);
+  let primeiro = arrayString[arrayString.length - 1];
+  let ultimo = arrayString[0];
+  return (`${primeiro}, ${ultimo}`);
 }
 
 // Desafio 5
 function footballPoints(wins, ties) {
-  return(wins*3 + ties * 1);
+  return (wins * 3 + ties * 1);
 }
 
 // Desafio 6
 function highestCount(numArray) {
-  if (numArray.length > 1){
-    let count = 1;
-    let maior = numArray[0];
-    for(let i = 1; i < numArray.length;i++){
-      if(maior < numArray[i]){
-        maior = numArray[i];
-        count = 1;
-      } 
-      else if(maior == numArray[i]){
-        count ++;
-      }
+  let count = 1;
+  let maior = numArray[0];
+  for (let i = 1; i < numArray.length; i += 1) {
+    if (maior < numArray[i]) {
+      maior = numArray[i];
+      count = 1;
+    } else if (maior === numArray[i]) {
+      count += 1;
     }
-    return count;
   }
-  return (numArray.length);
+  return count;
 }
 // Desafio 7
-function catAndMouse(mouse,cat1,cat2) {
-  d1 = Math.abs(mouse - cat1);
-  d2 = Math.abs(mouse - cat2);
-  if(d1 > d2){
-    return ("cat2");
+function catAndMouse(mouse, cat1, cat2) {
+  let d1 = Math.abs(mouse - cat1);
+  let d2 = Math.abs(mouse - cat2);
+  if (d1 > d2) {
+    return ('cat2');
   }
-  else if ( d1 == d2){
-    return("os gatos trombam e o rato foge");
+  if (d1 === d2) {
+    return ('os gatos trombam e o rato foge');
   }
-  return ("cat1");
-
+  return ('cat1');
 }
-
 // Desafio 8
 function fizzBuzz(numArray) {
-  var num;
-  var fizzBuzzArray = new Array();
-  for(let i=0; i < numArray.length ; i++){
-    num = numArray[i];
-    if((num%3 == 0) && (num%5 == 0)){
-      fizzBuzzArray.push("fizzBuzz");
+  let fizzBuzzArray = [];
+  for (let i = 0; i < numArray.length; i += 1) {
+    let word = 'bug!';
+    if (numArray[i] % 15 === 0) {
+      word = 'fizzBuzz';
+    } else if (numArray[i] % 3 === 0) {
+      word = 'fizz';
+    } else if (numArray[i] % 5 === 0) {
+      word = 'buzz';
     }
-    else if (num%3 == 0){
-      fizzBuzzArray.push("fizz");
-    }
-    else if (num%5 == 0){
-      fizzBuzzArray.push("buzz");
-    }
-    else {
-      fizzBuzzArray.push("bug!")
-    }
+    fizzBuzzArray.push(word);
   }
   return fizzBuzzArray;
 }
 // Desafio 9
 function encode(fraseToEncode) {
-  var frase = fraseToEncode;
-  for(let i=0;i<frase.length;i++){
-    //console.log(frase[i],i,frase.length);
-      switch(frase[i]){
-
-        case "a":
-          frase = frase.replace('a','1');
-          break;
-        case "e":
-          frase = frase.replace('e','2');
-          break;
-        case "i":
-          frase = frase.replace('i','3');
-          break;
-        case "o":
-          frase = frase.replace('o','4');
-          break;
-        case "u":
-          frase = frase.replace('u','5');
-          break;
-      }
-  }
-  return frase;
+  let dicionario = { a: '1', e: '2', i: '3', o: '4', u: '5' };
+  let f = fraseToEncode.replace(/[aeiou]/g, (lambidaFunction) => dicionario[lambidaFunction]);
+  return f;
 }
 
-//console.log(encode("frase"));
 function decode(fraseToDecode) {
-  var frase = fraseToDecode;
-  for(let i=0;i<frase.length;i++){
-    //console.log(frase[i],i,frase.length);
-      switch(frase[i]){
-
-        case "1":
-          frase = frase.replace('1','a');
-          break;
-        case "2":
-          frase = frase.replace('2','e');
-          break;
-        case "3":
-          frase = frase.replace('3','i');
-          break;
-        case "4":
-          frase = frase.replace('4','o');
-          break;
-        case "5":
-          frase = frase.replace('5','u');
-          break;
-      }
-  }
-  return frase;
+  let dicionario = { 1: 'a', 2: 'e', 3: 'i', 4: 'o', 5: 'u' };
+  let f = fraseToDecode.replace(/[1-5]/g, (lambidaFunction) => dicionario[lambidaFunction]);
+  console.log(f);
+  return f;
 }
 
 module.exports = {
--- src/challenges2.js
+++ src/challenges2.js
@@ -1,95 +1,62 @@
 // Desafio 10
-function techList(techList, name) {
-
-  if(typeof techList != "undefined" && techList != null && techList.length != null && techList.length > 0){
-
+function techList(List, name) {
+  if (List.length) {
     let namedTechList = [];
-
-    for(item of techList){
-      let techItem = {
-        tech:item,
-        name:name,
-      };
+    for (let item of List) {
+      let techItem = { tech: item, name };
       namedTechList.push(techItem);
     }
-    //ordenar a lista se tiver mais de 1 elemento
-    if (namedTechList.length > 1){
-      
-
-      for(let i=0; i < namedTechList.length; i++){
-          
-        let menor = namedTechList[i];
-          
-        for (let j=i+1; j<namedTechList.length; j++){
-
-          if( (menor.tech.localeCompare(namedTechList[j].tech)) == 1 ){
-            menor = namedTechList[j];
-            namedTechList[j] = namedTechList[i];
-            namedTechList[i] = menor;
-
-          }
-         
-
+    for (let i = 0; i < namedTechList.length; i += 1) {
+      let menor = namedTechList[i];
+      for (let j = i + 1; j < namedTechList.length; j += 1) {
+        if ((menor.tech.localeCompare(namedTechList[j].tech)) === 1) {
+          menor = namedTechList[j];
+          namedTechList[j] = namedTechList[i];
+          namedTechList[i] = menor;
         }
-
       }
-      console.log("orderedlist",namedTechList);
-      return namedTechList;
-    }  
-    return namedTechList;
-
-  }
-
-  return "Vazio!";
+    } return namedTechList;
+  } return ('Vazio!');
 }
-
 // Desafio 11
 function generatePhoneNumber(numArray) {
-  if(numArray.length != 11){
-    return "Array com tamanho incorreto.";
+  if (numArray.length !== 11) {
+    return 'Array com tamanho incorreto.';
   }
-  let countArray = [0,0,0,0,0,0,0,0,0,0];
-  let telefone = "";
-  for ( let i=0; i<numArray.length; i++){
+  let countArray = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  let telefone = '';
+  for (let i = 0; i < numArray.length; i += 1) {
     let num = numArray[i];
-    if(num < 0 || num > 9 || countArray[num] == 2){
-      return "não é possível gerar um número de telefone com esses valores";
+    if (num < 0 || num > 9 || countArray[num] === 2) {
+      return 'não é possível gerar um número de telefone com esses valores';
     }
-    countArray[num]++;
+    countArray[num] += 1;
     telefone += num.toString();
-
-    
   }
-  let ddd = "("+(telefone.substring(0,2))+") ";
-  telefone = (telefone.substring(2,7))+"-"+(telefone.substring(7,11))
-  return (ddd+telefone)
-
-
-
+  let ddd = `(${(telefone.substring(0, 2))}) `;
+  telefone = `${(telefone.substring(2, 7))}-${(telefone.substring(7, 11))}`;
+  return (ddd + telefone);
 }
 
 // Desafio 12
 function triangleCheck(a, b, c) {
-  if((a > b + c) || (b > a + c) || (c > b + a) || (a <= Math.abs(b-c)) || (b <= Math.abs(a-c))|| (c<= Math.abs(b-a))  ){
+  if ((a > b + c) || (b > a + c) || (c > b + a)) {
     return false;
   }
- 
   return true;
-
-
 }
 
 // Desafio 13
 function hydrate(pedido) {
-  let numbers = pedido.match(/[0-9]+/g)
+  let numbers = pedido.match(/[0-9]+/g);
   let copos = 0;
-  for(num of numbers){
-    copos += parseInt(num);
+  for (let num of numbers) {
+    copos += parseInt(num, 10);
   }
-  if(copos > 1){
-    return (copos.toString() +" copos de água")
+  if (copos > 1) {
+    return (`${copos.toString()} copos de água`);
   }
-  return (copos.toString()+" copo de água")
+  return (`${copos.toString()} copo de água`);
 }
 
 module.exports = {
