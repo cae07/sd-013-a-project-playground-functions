daad5ff1e008ffba36d9530cc16565c4af84e1dc
Vanessa <vanessamrios@gmail.com>
2021-05-31 22:48:38 -0300

    resolve o desafio 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,23 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(tecnologias, name) {
+
+  if(tecnologias.length === 0){
+    return "Vazio!";
+  }
+
+  tecnologias.sort();
+
+  let novaListaDeTecnologias = [];
+
+  for(let tecnologia of tecnologias){
+    let tecnologiaQueEuQueroAprender = {
+      tech: tecnologia,
+      name: name
+    }
+    novaListaDeTecnologias.push(tecnologiaQueEuQueroAprender);
+  }
+
+  return novaListaDeTecnologias;
 }
 
 // Desafio 11
