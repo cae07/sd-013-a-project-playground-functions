8710c2e425c891e7309b15cafca81f52461a9f60
junglejf <jfcchagas@id.uff.br>
2021-05-31 22:14:17 -0300

    desafio 10 concluído

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,11 +1,57 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(techList, name) {
+
+  if(typeof techList != "undefined" && techList != null && techList.length != null && techList.length > 0){
+
+    let namedTechList = [];
+
+    for(item of techList){
+      let techItem = {
+        tech:item,
+        name:name,
+      };
+      namedTechList.push(techItem);
+    }
+    //ordenar a lista se tiver mais de 1 elemento
+    if (namedTechList.length > 1){
+      
+
+      for(let i=0; i < namedTechList.length; i++){
+          
+        let menor = namedTechList[i];
+          
+        for (let j=i+1; j<namedTechList.length; j++){
+
+          if( (menor.tech.localeCompare(namedTechList[j].tech)) == 1 ){
+            menor = namedTechList[j];
+            namedTechList[j] = namedTechList[i];
+            namedTechList[i] = menor;
+
+          }
+         
+
+        }
+
+      }
+      console.log("orderedlist",namedTechList);
+      return namedTechList;
+    }  
+    return namedTechList;
+
+  }
+
+  return "Vazio!";
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(numArray) {
+  if(numArray.length != 11){
+    return " Array com tamanho incorreto.";
+  }
+  let quadruplicateArrayControl = [0,0,0,0,0,0,0,0,0,0];
+
+
+
 }
 
 // Desafio 12
