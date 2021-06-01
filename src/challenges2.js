dfafdc2f81679c5a0601403dd35ff78c5ae38e2d
rafa-araujo <rafacarlosaraujo@gmail.com>
2021-06-01 13:45:58 -0300

    Resoluçao desafio 12 - Bonus

--- src/challenges2.js
+++ src/challenges2.js
@@ -13,33 +13,29 @@ function techList(array, name) {
         name: name
       };
       arrayB.push(objeto);
-    }
+    };
   } else {
     result = 'Vazio!';
   }
   return result;
-}
-
-
-
-
-
-
-
-
-
-
-
+};
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(array) {
+
 }
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
-}
+function triangleCheck(lineA, lineB, lineC) {
+    let check = false;
+  
+    if (lineA < (lineB + lineC) && lineA > Math.abs(lineB - lineC) &&
+    lineB < (lineA + lineC) && lineB > Math.abs(lineA - lineC) && 
+    lineC < (lineB + lineA) && lineC > Math.abs(lineB - lineA)) {
+      check = true;
+    }
+    return check;
+  };
 
 // Desafio 13
 function hydrate() {
