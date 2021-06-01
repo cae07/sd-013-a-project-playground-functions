54d139d7716790bf9115a81a6e26d28eb1c64840
junglejf <jfcchagas@id.uff.br>
2021-05-31 23:34:07 -0300

    desafio 12 completo

--- src/challenges2.js
+++ src/challenges2.js
@@ -46,17 +46,37 @@ function techList(techList, name) {
 // Desafio 11
 function generatePhoneNumber(numArray) {
   if(numArray.length != 11){
-    return " Array com tamanho incorreto.";
+    return "Array com tamanho incorreto.";
   }
-  let quadruplicateArrayControl = [0,0,0,0,0,0,0,0,0,0];
+  let countArray = [0,0,0,0,0,0,0,0,0,0];
+  let telefone = "";
+  for ( let i=0; i<numArray.length; i++){
+    let num = numArray[i];
+    if(num < 0 || num > 9 || countArray[num] == 2){
+      return "não é possível gerar um número de telefone com esses valores";
+    }
+    countArray[num]++;
+    telefone += num.toString();
+
+    
+  }
+  let ddd = "("+(telefone.substring(0,2))+") ";
+  telefone = (telefone.substring(2,7))+"-"+(telefone.substring(7,11))
+  return (ddd+telefone)
 
 
 
 }
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+function triangleCheck(a, b, c) {
+  if((a > b + c) || (b > a + c) || (c > b + a) || (a <= Math.abs(b-c)) || (b <= Math.abs(a-c))|| (c<= Math.abs(b-a))  ){
+    return false;
+  }
+ 
+  return true;
+
+
 }
 
 // Desafio 13
