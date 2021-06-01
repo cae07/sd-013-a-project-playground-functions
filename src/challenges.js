6d1f418765cbf871a65f978e864e1762c13a62fd
Heleny Flavia <flaviaanahmias@gmail.com>
2021-05-31 22:13:07 -0400

    corrige falhas

--- src/challenges.js
+++ src/challenges.js
@@ -79,15 +79,31 @@ function fizzBuzz(array) {
       retorno = 'fizzBuzz';
     } else if (array[index] % 3 === 0) {
       retorno = 'fizz';
-    } else if (array[index]] % 5 === 0) {
+    } else if (array[index] % 5 === 0) {
       retorno = 'buzz';
     }
   } return newArray;
 }
 
 // Desafio 9
-function encode() {
+function encode(number) {
   // seu código aqui
+  string = string.replace(/a/g, "1");
+  string = string.replace(/e/g, "2");
+  string = string.replace(/i/g, "3");
+  string = string.replace(/o/g, "4");
+  string = string.replace(/u/g, "5");
+  return string;  
+}
+
+function decode(string) {
+  // seu código aqui
+  string = string.replace(/1/g, "a");
+  string = string.replace(/2/g, "e");
+  string = string.replace(/3/g, "i");
+  string = string.replace(/4/g, "o");
+  string = string.replace(/5/g, "u");
+  return string;  
 }
 function decode() {
   // seu código aqui
