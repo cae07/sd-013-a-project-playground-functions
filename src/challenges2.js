725d5bf8abdd5d243e241e2c144a1f27ce51b0b4
Jéssica Queiroz <gnr0034@gmail.com>
2021-06-01 11:52:53 -0300

    Requisito 13

--- src/challenges2.js
+++ src/challenges2.js
@@ -56,17 +56,21 @@ function triangleCheck(lineA, lineB, lineC) {
 // Desafio 13
 function hydrate(string) {
   // seu código aqui
-  let numbersOfString = /d+?/g;
-  let match = numbersOfString.exec(string);
-  let sum;
-  for(let i=0; i < string.length; i+=1){
-    
-
-    
-
-    
-  } return match;
-}  console.log(hydrate("1 cachaça, 5 cervejas e 1 copo de vinho"));
+  let numsStr = string.replace(/[^0-9]/g,'');
+  let array = numsStr.split('');
+  let soma = 0;
+  let newArray = [];
+  for(let index = 0; index < array.length; index+=1){
+    newArray.push(parseInt(array[index]));
+  }
+  for(let i=0; i < newArray.length; i+=1){
+    soma += newArray[i];
+  } 
+  if(soma <= 1){
+    return `${soma} copo de água`
+  } else return `${soma} copos de água`;
+}
+console.log(hydrate("1 cachaça"));
 
 module.exports = {
   generatePhoneNumber,
