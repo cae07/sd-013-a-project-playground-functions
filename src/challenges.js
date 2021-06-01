e215dd2d5b48cc0e62e0e16cc529dafd4455afd4
Tatiane Chiarello <tatichiarello@gmail.com>
2021-06-01 12:25:55 +0200

    arruma desafios 9 e 10

--- src/challenges.js
+++ src/challenges.js
@@ -79,25 +79,22 @@ function fizzBuzz(arr) {
 
 // Desafio 9
 function encode(str) {
-  let newStr = str;
-  newStr
+  return str
     .replace(/a/g, '1')
     .replace(/e/g, '2')
     .replace(/i/g, '3')
     .replace(/o/g, '4')
     .replace(/u/g, '5');
-  return newStr;
 }
+console.log(encode('hello'));
 
 function decode(str) {
-  let newStr = str;
-  newStr
+  return str
     .replace(/1/g, 'a')
     .replace(/2/g, 'e')
     .replace(/3/g, 'i')
     .replace(/4/g, 'o')
     .replace(/5/g, 'u');
-  return newStr;
 }
 
 module.exports = {
