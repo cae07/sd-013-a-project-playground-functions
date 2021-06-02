fc548a708dd10055ac0bf1add792ad55dff5f460
Rodrigo F de Avila <rodrigoferraodeavila@gmail.com>
2021-06-02 14:15:45 -0300

    ate 5 completo problema no 6

--- src/challenges.js
+++ src/challenges.js
@@ -1,33 +1,62 @@
 // Desafio 1
-function compareTrue() {
+function compareTrue(a,b) {
   // seu código aqui
+    if(a && b === true){
+      return true;
+    }else{
+      return false
+    }
 }
 
 // Desafio 2
-function calcArea() {
+function calcArea(base, height) {
   // seu código aqui
+  let area = (base * height)/2;
+   return area;  
+
 }
 
 // Desafio 3
-function splitSentence() {
+function splitSentence(s) {
   // seu código aqui
+  r = s.split(" ", 10);
+  return r;
 }
 
 // Desafio 4
-function concatName() {
+function concatName(n) {
   // seu código aqui
+   f = n[0];
+   l = n[n.length - 1]
+   
+   return l + ", " + f;
 }
-
 // Desafio 5
-function footballPoints() {
+function footballPoints(wins, ties) {
   // seu código aqui
+  return (wins * 3) + ties;
 }
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
-}
+function highestCount(h) {
 
+    // seu código aqui
+    let aux = null;
+    var high = null;
+    // descobre maior valor
+    for(let i = 0; i < h.length; i++){
+      if(high < h[i]){
+        high = h[i];
+      }
+    }
+    //conta quantidade que maior valor aparece
+    for(let j = 0;j < h.length; j++){
+      if(h[j] === high){
+        aux = aux +1;
+      }
+    }
+    return aux;
+  }
 // Desafio 7
 function catAndMouse() {
   // seu código aqui
