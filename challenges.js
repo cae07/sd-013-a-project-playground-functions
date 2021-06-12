37a3046284029d948a480f2bc64ec80ea9841f9f
= <luz.matheus11@gmail.com>
2021-06-12 15:34:03 -0300

    Requisito 1 e 2 desenvolvidos

---
+++ challenges.js
@@ -0,0 +1,14 @@
+//requisito 1
+
+function compareTrue(booleano1, booleano2){
+  return booleano1 && booleano2;
+}
+
+//requisito 2
+
+compareTrue(true, false);
+
+function calcArea() {
+  let calcArea = (10 * 50) / 2;
+  return calcArea;
+}
