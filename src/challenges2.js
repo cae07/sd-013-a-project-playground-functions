72b61c5f45afeeaa902b8b5ce621c10226678782
Marcelo Cunha <marcelo.cunha1@outlook.com>
2021-06-12 00:22:26 -0300

    solving challenge 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -17,8 +17,24 @@ function techList(techs, nome) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(arrayNumber) {
+  let number = '(' + arrayNumber[0] + arrayNumber[1] + ')' + " " + arrayNumber[2] + arrayNumber[3] + arrayNumber[4] + arrayNumber[5] + arrayNumber[6] + '-' + arrayNumber[7] + arrayNumber[8] + arrayNumber[9] + arrayNumber[10];
+  if (arrayNumber.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  }
+  let countNumbers = {};
+  for (i in arrayNumber) {
+    const n = arrayNumber[i];
+    if (countNumbers[n]) {
+      countNumbers[n] += 1;
+    } else {
+      countNumbers[n] = 1;
+    }
+    if (countNumbers[n] > 2 || (n < 0 || n > 9)) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+  }
+  return number;
 }
 
 // Desafio 12
