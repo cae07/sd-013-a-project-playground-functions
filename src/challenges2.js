65dc7266f99fe139c4f1cdff40f28278b82ada59
JoseEduardognzg <joseeduardognzg@gmail.com>
2021-06-15 18:57:32 -0300

    Requisito 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,22 @@
 // Desafio 10
 function techList() {
+  //https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
   // seu código aqui
+  function techList(tecnologias, name) {
+    if (tecnologias.length === 0) {
+      return 'Vazio!';
+    }
+  
+    let tecnologia = [];
+    tecnologias.sort();
+  
+    for (let valor = 0; valor < tecnologias.length; valor += 1) {
+      tecnologia.push({
+        tech: tecnologias[valor],
+        name,
+      });
+    }
+    return tecnologia;
 }
 
 // Desafio 11
