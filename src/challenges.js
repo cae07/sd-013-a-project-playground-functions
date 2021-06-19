e1395cea4642637e5a4968a3063f327d4b6ffac7
= <luz.matheus11@gmail.com>
2021-06-19 15:27:40 -0300

    Requesito 6

--- src/challenges.js
+++ src/challenges.js
@@ -15,7 +15,7 @@ function calcArea(base, height) {
 
 // Desafio 3
 function splitSentence(string) {
-  return string.split[', '];
+  return string.split[' '];
 };
 
 
@@ -38,13 +38,20 @@ let maiorNumero = [9, 1, 2, 3, 9, 5, 7];
 let contador = 0;
 
 // Desafio 6
-  function highestCount(maiorNumero){
-      for(let contador = 0; contador < maiorNumero.length; contador += 0){
-      if(maiorNumero[index] === maiorNumero[contador]){
+  function highestCount(array){
+    let maiorValor = array[0];
+    let contador = 0;
+    for(let index = 0; index < array.length; index += 1){
+      if(array[index] >= maiorValor){
+        maiorValor = array[index];
+      }
+    }
+    for(let index = 0; index < array.length; index += 0){
+      if(array[index] === maiorValor){
         contador += 1;
       }
-      return contador;
-  }
+    }
+    return contador;
 };
 
 // Desafio 7
@@ -52,9 +59,9 @@ function catAndMouse(mouse, cat1, cat2) {
   let dist1 = mouse - cat1;
   let dist2 = mouse - cat2;
 
-  if(dist1 < dist2){
+  if(dist1 <= dist2){
     return cat1;
-  } else if (dist2 < dist1) {
+  } else if (dist2 <= dist1) {
     return cat2;
   } else if (dist1 === dist2) {
     return "os gatos trobam e o rato foge";
@@ -62,9 +69,19 @@ function catAndMouse(mouse, cat1, cat2) {
 };
 
 // Desafio 8
-function fizzBuzz() {
-   // seu código aqui
-}
+function fizzBuzz(array) {
+   let arrayFizzBuzz = [];
+   for(let index = 0; index < array.length; index +=1){
+   let arrayFiiz = array[index];
+  } if(arrayFiiz[index] % 3 === 0 && arrayFiiz % 5) {
+    arrayFizzBuzz.push('fizzBuzz');
+  } else if(arrayFiiz[index] % 5 === 0){
+    arrayFizzBuzz.push('buzz');
+  } else if(arrayFiiz[index] % 3 === 0){
+    arrayFizzBuzz.push('fizz');
+  }
+  return arrayFizzBuzz;
+};
 
 // Desafio 9
 function encode() {
