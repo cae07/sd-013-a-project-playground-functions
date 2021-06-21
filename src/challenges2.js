2872a80f0ca42c4e892b47aab591f031208da9c0
Rodrigo F de Avila <rodrigoferraodeavila@gmail.com>
2021-06-21 17:07:32 -0300

    desafio 11 feito

--- src/challenges2.js
+++ src/challenges2.js
@@ -21,7 +21,31 @@ function techList(t, name) {
   // Desafio 11
 function generatePhoneNumber(f) {
   // seu código aqui
-}
+
+    var rep = 0;
+
+    if(f.length !== 11){
+      return "Array com tamanho incorreto.";
+    }
+
+    for (let i = 0; i < f.length; i++) {
+      for (let j = i+1; j < f.length; j++) {
+      if(f[i] < 0 | f[i] > 9 ){
+        return "não é possível gerar um número de telefone com esses valores";
+      }else if(f[i] == f[j]){
+        rep++;
+      }    
+      
+    }
+      }// fim for
+      if(rep > 3){
+        return "não é possível gerar um número de telefone com esses valores";
+      }else{
+       return "("+f[0]+f[1]+")"+" "+f[2]+f[3]+f[4]+f[5]+f[6]+"-"+f[7]+f[8]+f[9]+f[10];
+      }
+    }
+
+
 // Desafio 12
 function triangleCheck() {
   // seu código aqui
