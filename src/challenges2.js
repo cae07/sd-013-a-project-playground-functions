fb4e65130b76a9c59b383008e9607ebd5b406603
= <luz.matheus11@gmail.com>
2021-06-21 16:41:41 -0300

    Requesito 12

--- src/challenges2.js
+++ src/challenges2.js
@@ -10,21 +10,49 @@ function techList(techList, nome){
   }
    if(techList.length > 0){
     return techListName;
-  } else return 'Vazio!';
+  } else 'Vazio!';
 };
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
-}
+function generatePhoneNumber(){
+   // seu código aqui
+};
+
+
+
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
-}
+function triangleCheck(lineA, lineB, lineC) {
+  let a = checkA(lineA, lineB, lineC);
+  let b = checkB(lineA, lineB, lineC);
+  let c = checkC(lineA, lineB, lineC);
+  return a && b && c;
+
+
+
+  function checkA(lineA, lineB, lineC){
+    let fullTriangle = Math.abs(lineA, lineB, lineC) || Math.abs(lineB, lineC, lineA) || Math.abs(lineC, lineA, lineB)
+  if(fullTriangle){
+    return true;
+  }
+    return false;
+  }
+};
+  function checkB(lineA, lineB, lineC){
+  if(lineB > lineC + lineA || lineB < Math.abs(lineC - lineA)){
+    return false;
+  }
+  return true;
+};
+  function checkC(lineA, lineB, lineC){
+    if(lineC > lineA + lineB || lineC < Math.abs(lineA - lineB)){
+      return false;
+    }
+    return true;
+  }
 
 // Desafio 13
-function hydrate() {
+function hydrate(string) {
   // seu código aqui
 }
 
