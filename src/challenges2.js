6a6fabe2e3c02723c1653a4372391005efad1d5c
Marlon Lacerda <marlon.desagitario@hotmail.com>
2021-11-03 20:20:37 -0300

    req 11 done

--- src/challenges2.js
+++ src/challenges2.js
@@ -21,8 +21,51 @@ function techList(array, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
+const verifyDigits = (numbers) => {
+  for (let index = 0; index < numbers.length; index += 1) {
+    if (numbers[index] < 0 || numbers[index] > 9) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const checkRepeatAux = (array, number) => {
+  let repeat = 0;
+  for (let index = 0; index < array.length; index += 1) {
+    if (number === array[index]) {
+      repeat += 1;
+    }
+  }
+  return repeat;
+};
+
+const checkRepeatNumbers = (array) => {
+  for (let index = 0; index < array.length; index += 1) {
+    let repeat = checkRepeatAux(array, array[index]);
+    if (repeat >= 3) {
+      return false;
+    }
+  }
+  return true;
+};
+
+function generatePhoneNumber(array) {
   // seu código aqui
+  let phonenumber = '';
+
+  if (array.length !== 11) return 'Array com tamanho incorreto.';
+  if (verifyDigits(array) === false) {
+    return 'não é possível gerar um número de telefone com esses valores';
+  }
+  if (checkRepeatNumbers(array) === false) {
+    return 'não é possível gerar um número de telefone com esses valores';
+  }
+
+  phonenumber += `(${array[0]}${array[1]}) `;
+  phonenumber += `${array[2]}${array[3]}${array[4]}${array[5]}${array[6]}-`;
+  phonenumber += `${array[7]}${array[8]}${array[9]}${array[10]}`;
+  return phonenumber;
 }
 
 // Desafio 12
