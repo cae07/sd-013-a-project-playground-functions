8e6d67f06ccf8b9956da61aef8726a3f41aa53b8
RuanCunha <ruanvcunha@hotmail.com>
2021-11-04 18:15:04 -0300

    Requisito 8 corrigido e finalizado

--- src/challenges.js
+++ src/challenges.js
@@ -1,6 +1,6 @@
 // Desafio 1
-function compareTrue (number1, number2) {
-  if (number1 && number2){
+function compareTrue(number1, number2) {
+  if (number1 && number2) {
     return true;
   }
   return false;
@@ -20,7 +20,7 @@ function splitSentence(str) {
 
 // Desafio 4
 function concatName(array) {
-  let res = (array[array.length - 1] + ', ' + array[0]);
+  let res = (`${array[array.length - 1]}, ${array[0]}`);
   return res;
 }
 
@@ -61,24 +61,69 @@ function catAndMouse(mouse, cat1, cat2) {
   let dist2 = Math.max(mouse, cat2) - Math.min(mouse, cat2);
   if (dist1 === dist2) {
     return ('os gatos trombam e o rato foge');
-  } else if (dist1 < dist2){
+  } if (dist1 < dist2) {
     return ('cat1');
-  } else {
-    return ('cat2');
   }
+  return ('cat2');
 }
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function transform(num) {
+  let result = 0;
+  if (num % 15 === 0) {
+    result = 'fizzBuzz';
+  } else if (num % 3 === 0) {
+    result = 'fizz';
+  } else if (num % 5 === 0) {
+    result = 'buzz';
+  } else {
+    result = 'bug!';
+  }
+  return result;
+}
+
+function fizzBuzz(array) {
+  const result = array.map((num) => transform(num));
+  return result;
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(frase) {
+  let res = frase.split('');
+  for (let index = 0; index < res.length; index += 1) {
+    if (res[index] == 'a') {
+      res[index] = '1';
+    } else if (res[index] == 'e') {
+      res[index] = '2';
+    } else if (res[index] == 'i') {
+      res[index] = '3';
+    } else if (res[index] == 'o') {
+      res[index] = '4';
+    } else if (res[index] == 'u') {
+      res[index] = '5';
+    }
+  }
+  frase = res.join('');
+  return frase;
 }
-function decode() {
-  // seu código aqui
+
+function decode(frase) {
+  let res = frase.split('');
+  for (let index = 0; index < res.length; index += 1) {
+    if (res[index] == '1') {
+      res[index] = 'a';
+    } else if (res[index] == '2') {
+      res[index] = 'e';
+    } else if (res[index] == '3') {
+      res[index] = 'i';
+    } else if (res[index] == '4') {
+      res[index] = 'o';
+    } else if (res[index] == '5') {
+      res[index] = 'u';
+    }
+  }
+  frase = res.join('');
+  return frase;
 }
 
 module.exports = {
--- src/challenges2.js
+++ src/challenges2.js
@@ -2,12 +2,12 @@
 function techList(tecArray, name) {
   tecArray.sort();
   let arrayFinal = [];
-  if (tecArray.length != 0) {
-    for (let index = 0; index < tecArray.length; index += 1){
+  if (tecArray.length !== 0) {
+    for (let index = 0; index < tecArray.length; index += 1) {
       arrayFinal[index] = {
         tech: tecArray[index],
-        name: name
-      }
+        name,
+      };
     }
   } else {
     arrayFinal = 'Vazio!';
@@ -17,62 +17,97 @@ function techList(tecArray, name) {
 
 // Desafio 11
 function generatePhoneNumber(number) {
-  if (testQuantity(number) || checkZero(number)) {
-    res = '(' + number[0] + number[1] + ') ' + number[2] + number[3] + number[4] + number[5] + number[6] + '-' + number[7] + number[8] + number[9] + number[10]
-  } else {
-    res = "não é possível gerar um número de telefone com esses valores"
-  }
-  return res;
-}
+//   let res;
+//   if (testQuantity(number) || checkZero(number)) {
+//     return res = '(' + number[0] + number[1] + ') ' + number[2] + number[3] + number[4] + number[5] + number[6] + '-' + number[7] + number[8] + number[9] + number[10];
+//   }
+//   return "não é possível gerar um número de telefone com esses valores"
+// }
 
-function testQuantity(number) {
-  let count = 0;
-  let compare = 0;
-  if (number[0] > number[1]) {
-    compare = number[0];
-  } else {
-    compare = number[1];
-  }
-  for (let index = 2; index < number.length; index += 1) {
-    if (number[index] > compare) {
-      compare = number[index];
-    }
-  }
-  for (let index = 0; index < number.length; index += 1) {
-    if (number[index] === compare) {
-      count += 1;
-    }
-  }
-  if (count > 3) {
-    return false;
-  } return true;
-}
+  // function testSize(number) {
+  //   let teste = 0;
+  //   for (let index = 0; index < number.length; index += 1) {
+  //     if (number[index] > 9 || number[index] < 0) {
+  //     teste += 1;
+  //     }
+  //   }
+  //   if (teste > 0) {
+  //     return false;
+  //   }
+  //   return true
+  // }
+  // function testQuantity(number) {
+  //   let count = 0;
+  //   let compare = 0;
+  //   if (number[0] > number[1]) {
+  //     compare = number[0];
+  //   } else {
+  //     compare = number[1];
+  //   }
+  //   for (let index = 2; index < number.length; index += 1) {
+  //     if (number[index] > compare) {
+  //       compare = number[index];
+  //     }
+  //   }
+  //   for (let index = 0; index < number.length; index += 1) {
+  //     if (number[index] === compare) {
+  //       count += 1;
+  //     }
+  //   }
+  //   if (count > 2) {
+  //     return false;
+  //   } return true;
+  // }
 
-function checkZero(number) {
-  for (let index = 0; index < number.length; index += 1) {
-    if (number[index] < 0 || number[index] > 0) {
-      return false;
-    }
-  }
+// function checkZero(number) {
+//   let teste = 0;
+//   for (let index = 0; index < number.length; index += 1) {
+//     if (number[index] < 0 || number[index] > 0) {
+//       teste += 1;
+//     }
+//   }
+//   if (teste > 0) {
+//     return false;
+//   } return true;
 }
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+let lineA = 10;
+let lineB = 10;
+let lineC = 10;
+// function triangleCheck(lineA, lineB, lineC) {
+//   if (checkSide() && checkAbsolut()) {
+//     return true;
+//   } return false;
+// }
+// function checkAbsolut(lineA, lineB, lineC) {
+//   let ab = Math.abs(lineA-lineB);
+//   let bc = Math.abs(lineB-lineC);
+//   let ca = Math.abs(lineC-lineA);
+//   if (lineA > bc && lineB > ca && lineC > ab) {
+//     return true;
+//   } return false;
+// }
+
+function checkSide(lineA, lineB, lineC) {
+  if ((lineA + lineB) > lineC && (lineC + lineA) > lineB && (lineB + lineC) > lineA) {
+    return true;
+  }
+  return false;
 }
 
+console.log(checkSide());
 // Desafio 13
 function hydrate(texto) {
   let array = texto.match(/\d+/g);
   let sum = 0;
-  for(let index = 0; index < array.length; index += 1) {
-    sum = sum + parseInt(array[index]);
+  for (let index = 0; index < array.length; index += 1) {
+    sum += parseInt(array[index]);
   }
-  if(sum > 1){
-    return sum + ' copos de água';
-  } else {
-  return sum + ' copo de água';
+  if (sum > 1) {
+    return `${sum} copos de água`;
   }
+  return `${sum} copo de água`;
 }
 
 module.exports = {
