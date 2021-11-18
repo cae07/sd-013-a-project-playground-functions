274278841dd7d2b899670a3bf98ea1b18f102fd0
yurigarrido <y.garrido.yuri@gmail.com>
2021-11-18 17:16:58 -0300

    faz parcialmente o desafio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -15,10 +15,27 @@ function techList(arrayNomes, name) {
 console.log(techList([], 'yuri'));
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
-}
+// function generatePhoneNumber(arrayNumbers) {
+//   if (arrayNumbers.length >= 12) return 'Array com tamanho incorreto.';
+//   let parentesis = arrayNumbers.slice(0, 2).join('');
+//   let primeiroBloco = arrayNumbers.slice(2, 7).join('');
+//   let ultimoBloco = arrayNumbers.slice(7, 11).join('');
+//   let number = '('.concat(parentesis, ') ', primeiroBloco, '-', ultimoBloco);
+//   let arrayPadrão = arrayNumbers;
+//   let mensagem = '';
+//   mensagem = number;
+//   let numeroRepete = 0;
+//   if (numeroRepete === 2) mensagem = 'não é possível gerar um número de telefone com esses valores';
+//   arrayPadrão.forEach((cadaNumero) => {
+//     if (cadaNumero < 0) mensagem = 'não é possível gerar um número de telefone com esses valores';
+//     arrayPadrão.forEach((numeros) => {
+//       if (numeros === cadaNumero) numeroRepete += 1;
+//     });
+//   });
+//   return mensagem;
+// }
 
+// console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 1]));
 // Desafio 12
 function triangleCheck() {
   // seu código aqui
