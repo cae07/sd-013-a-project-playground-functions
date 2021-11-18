3ff51ca600fed17e9eaad37f53c0ff3529080e84
yurigarrido <y.garrido.yuri@gmail.com>
2021-11-18 16:22:49 -0300

    finaliza desafio 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,18 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(arrayNomes, name) {
+  let arrayCompleto = [];
+  let arrayCompletoSort = arrayNomes.sort();
+  if (arrayNomes.length === 0) return 'Vazio!';
+  arrayCompletoSort.forEach((nomeCorrente) => {
+    const objeto = {
+      tech: nomeCorrente,
+      name,
+    };
+    arrayCompleto.push(objeto);
+  });
+  return arrayCompleto;
 }
+console.log(techList([], 'yuri'));
 
 // Desafio 11
 function generatePhoneNumber() {
