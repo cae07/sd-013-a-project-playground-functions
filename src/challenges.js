0ecfe763dbfe1b73da452246939ec5e9c4767e3b
yurigarrido <y.garrido.yuri@gmail.com>
2021-11-18 14:43:38 -0300

    refaz execicio 8

--- src/challenges.js
+++ src/challenges.js
@@ -18,89 +18,61 @@ console.log(splitSentence('go trybe'));
 
 // Desafio 4 == OK, passou nos 3 testes
 const concatName = (array) => {
-  let primeiro = array[0];
-  let ultimo = array[array.length - 1];
-  let result = ultimo.concat(' ', primeiro);
+  const primeiro = array[0];
+  const ultimo = array[array.length - 1];
+  const result = ultimo.concat(', ', primeiro);
   return result;
 };
-console.log(concatName(['captain', 'my', 'captain']));
+console.log(concatName(['ultimo', 'my', 'primeiro']));
 
 // Desafio 5 == OK, passou nos 3 testes.
-function footballPoints(wins, ties) {
-  let vitoria = wins * 3;
-  let empate = ties * 1;
-  let result = vitoria + empate;
+const footballPoints = (wins, ties) => {
+  let result = (wins * 3) + (ties * 1);
   return result;
-}
-console.log(footballPoints(0,0));
+};
+console.log(footballPoints(1, 1));
 
 // Desafio 6
-function highestCount(arrayNum) {
- let maiorNum = arrayNum[0];
- let contaVezes = 0;
-    
-  for( index = 0; index < arrayNum.length; index += 1){   
-    if( arrayNum[index] > maiorNum ){
-      maiorNum = arrayNum[index];
-      contaVezes = 1;
-    } else if (arrayNum[index] == maiorNum){
-      contaVezes = contaVezes + 1;
-    }
-  }  return contaVezes; 
-}
-
-console.log(highestCount([-2,-2,-1,-1]));
+const highestCount = (arrayNum) => {
+  let maiorNum = Math.max.apply(null, arrayNum);
+  let contaVezes = 0;
+  arrayNum.forEach((num) => {
+    if (num === maiorNum) contaVezes += 1;
+  });
+  return contaVezes;
+};
+// fonte : https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Function/apply (appy)
+// apply => 1° parametro é o contexto, 2° parametro são os valores que a função recebe
+console.log(highestCount([-2, -2, -1, -1, -1]));
 
 // Desafio 7
-function catAndMouse(mouse, cat1, cat2) {
-  let distGato1 = cat1 - mouse ;
-    if (distGato1 < 0 ){
-      distGato1 = (distGato1 * -1);
-    }
-  let distGato2 = cat2 - mouse ;
-  if (distGato2 < 0) {
-    distGato2 = (distGato2 * -1);
-  }
-
-if( distGato1 < distGato2){
-      return 'cat1';
-    } else if ( distGato2 < distGato1){
-      return "cat2";
-    } else if ( distGato2 == distGato1){
-      return "os gatos trombam e o rato foge"
-    } 
-}
-console.log(catAndMouse(10,4,22));
+const catAndMouse = (mouse, cat1, cat2) => {
+  let distGato1 = cat1 - mouse;
+  let distGato2 = cat2 - mouse;
+  if (distGato1 < 0) distGato1 *= -1;
+  if (distGato2 < 0) distGato2 *= -1;
+  if (distGato1 < distGato2) return 'cat1';
+  if (distGato2 < distGato1) return 'cat2';
+  return 'os gatos trombam e o rato foge';
+};
+console.log(catAndMouse(1, 0, 2));
 
 // Desafio 8
-function fizzBuzz(array) {
-
+const fizzBuzz = ((array) => {
   let resultado = [];
-  
-  for (let index = 0; index < array.length; index += 1) {
-  
-  if (array[index] % 3 === 0 && array[index] % 5 === 0) {
-  resultado.push('fizzBuzz');
-  } 
-  
-  else if (array[index] % 5 === 0) {
-  resultado.push('buzz');
-  }
-  
-  else if (array[index] % 3 == 0){
-  resultado.push('fizz');
-  }
-  
-  else{
-  resultado.push('bug!');
-  }
-  
-  }
-  
+  array.forEach((position) => {
+    if (position % 3 === 0 && position % 5 === 0) {
+      resultado.push('fizzBuzz');
+    } else if (position % 5 === 0) {
+      resultado.push('buzz');
+    } else if (position % 3 === 0) {
+      resultado.push('fizz');
+    } else { resultado.push('bug!'); }
+  });
+
   return resultado;
-  
-  }
-  console.log(fizzBuzz([2, 15, 7, 9, 45]))
+});
+console.log(fizzBuzz([2, 15, 7, 9, 45]));
 
 // Desafio 9
 function encode() {
